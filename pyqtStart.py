5ea07460a30240e6f617229eec5a58c6f26fa17a
pratik-tan10 <pratik.tan10@gmail.com>
2021-09-20 12:50:20 -0500

    Update pyqtStart.py

--- pyqtStart.py
+++ pyqtStart.py
@@ -1,28 +1,38 @@
-import sys 
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel
-
-from PyQt5.QtCore import Qt, QCoreApplication
+import sys
  
-app = QCoreApplication.instance()
-if app is None:
-    app = QApplication(sys.argv)
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QGridLayout, QPushButton, QHBoxLayout 
+from PyQt5.QtCore import Qt 
  
-app.aboutToQuit.connect(app.deleteLater) 
+def buttonClickedHandler(c): 
+     global counter 
+     counter += 1
+     label.setText('Thank you for clicking the button ' + str(counter) + ' times!') 
+ 
+app = QApplication(sys.argv) 
+ 
 window = QWidget() 
 window.resize(400,200) 
 window.setWindowTitle("PyQt5 example 1") 
  
-layout = QGridLayout(window)
-label = QLabel("Just a window with a label!", window) 
-label.setAlignment(Qt.AlignCenter) 
-layout.addWidget(label,0,0)
-
-button = QPushButton("Close me") 
-button.setToolTip('This is a <b>QPushButton</b> widget. Clicking it will close the program!') 
-layout.addWidget(button,1,0) 
+layout = QGridLayout(window) 
  
-button.clicked.connect(app.quit) 
+label = QLabel("Just a window with a label (now perfectly centered!)") 
+label.setAlignment(Qt.AlignCenter) 
+layout.addWidget(label,0,0) 
+ 
+button = QPushButton("Click me") 
+button.setToolTip('This is a <b>QPushButton</b> widget. Click it!') 
+ 
+horLayout = QHBoxLayout() 
+horLayout.addStretch(1) 
+horLayout.addWidget(button) 
+horLayout.addStretch(1) 
+layout.addLayout(horLayout,1,0) 
+ 
+button.clicked.connect(buttonClickedHandler) 
+ 
+counter = 0
  
 window.show() 
  
-sys.exit(app.exec_())
+sys.exit(app.exec_()) 
