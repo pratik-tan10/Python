3cb6f7082bf9c3a87f409f7cdeb8ac457ae5db87
pratik-tan10 <pratik.tan10@gmail.com>
2021-09-23 11:23:04 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -13,3 +13,8 @@ print("Hello " +name+" ". Nice to meet you!")
 #with string format
 print("Hello {} . Nice to meet you!".format(name))
  
+number = int(input("Please enter a whole number."))
+      
+#python ternary ...if...else....
+
+print("That's a large number" if number>100 else "That's a small number")
