55086cfd8c76004934643eb6c6d03e1d714ab5e2
pratik-tan10 <pratik.tan10@gmail.com>
2021-09-23 11:20:25 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -4,3 +4,12 @@ print "Hello world"
 
 #Python 3
 print("Hello world")
+
+name = input("Enter your name")
+
+#with string concatenation
+print("Hello " +name+" ". Nice to meet you!")
+
+#with string format
+print("Hello {} . Nice to meet you!".format(name))
+ 
