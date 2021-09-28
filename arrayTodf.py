b5855aa6420444aed50afc4b715223467144101b
pratik-tan10 <pratik.tan10@gmail.com>
2021-09-28 17:13:20 -0500

    Update arrayTodf.py

--- arrayTodf.py
+++ arrayTodf.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
 
-my_array = np.array([['Jon',25,1995,2016],['Maria',47,1973,2000],['Bill',38,1982,2005]], dtype=object)
+my_array = np.array([['Jon',25,1995,2016],['Maria',47,1973,2000],['Bill',38,1982,2005]])
 
 df = pd.DataFrame(my_array, columns = ['Name','Age','Birth Year','Graduation Year'])
+
+df['Age'] = df['Age'].astype(int)
+df['Birth Year'] = df['Birth Year'].astype(int)
+df['Graduation Year'] = df['Graduation Year'].astype(int)
 
 print(df)
 print(type(df))
