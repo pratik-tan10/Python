092b572c4c08f9a4a78fc0c3380d5bf451d135c9
pratik-tan10 <pratik.tan10@gmail.com>
2021-09-28 17:11:53 -0500

    Update arrayTodf.py

--- arrayTodf.py
+++ arrayTodf.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pandas as pd
 
 my_array = np.array([[11,22,33],[44,55,66]])
 
-print(my_array)
-print(type(my_array))
+df = pd.DataFrame(my_array, columns = ['Column_A','Column_B','Column_C'])
+
+print(df)
+print(type(df))
