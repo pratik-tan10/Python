037c156b1e1237e34ca5bb8ac0a7993dd1e490c4
pratik-tan10 <pratik.tan10@gmail.com>
2021-09-29 14:13:20 -0500

    Update regularExpression.py

--- regularExpression.py
+++ regularExpression.py
@@ -1,14 +1,19 @@
-import re
+import re 
+ 
 personList = [ 'Julia Smith', 'Francis Drake', 'Michael Mason',  
                 'Jennifer Johnson', 'John Williams', 'Susanne Walker',  
                 'Kermit the Frog', 'Dr. Melissa Franklin', 'Papa John', 
                 'Walter John Miller', 'Frank Michael Robertson', 'Richard Robertson', 
                 'Erik D. White', 'Vincent van Gogh', 'Dr. Dr. Matthew Malone', 
-                'Rebecca Clark' ]
-pattern = ".u"
-
-compiledRE = re.compile(pattern)
-
-for person in personList:
-  if compiledRE.match(pattern, person):
-    print(person)
+                'Rebecca Clark' ] 
+ 
+pattern = "John"
+compiledRE = re.compile(pattern) 
+ 
+for person in personList: 
+     match = compiledRE.match(person) 
+     if match: 
+         print(match.group()) 
+         print(match.start()) 
+         print(match.end()) 
+         print(match.span()) 
