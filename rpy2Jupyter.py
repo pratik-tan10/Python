7d9184ff443f50fbffced895109da5c18bd79111
pratik-tan10 <pratik.tan10@gmail.com>
2021-10-01 16:46:54 -0500

    Update rpy2Jupyter.py

--- rpy2Jupyter.py
+++ rpy2Jupyter.py
@@ -39,4 +39,19 @@ for i in range(1,4):
     button.on_click(onButtonClick)
 
 #_______________________________________________________________________
+from ipywidgets import widgets 
+from IPython.display import display 
+ 
+def onButtonClick(b):
+    print("Button " + b.description + " has been clicked")
+ 
+for i in range(1,4):
+    button = widgets.Button(description=str(i))
+    display(button)
+    button.on_click(onButtonClick)
+
+
+#_______________________________________________________________________
+
+
 
