708bde83b5b11cb74d5e746dfaa0ce41186667dc
pratik-tan10 <pratik.tan10@gmail.com>
2021-10-01 16:40:21 -0500

    Update rpy2Jupyter.py

--- rpy2Jupyter.py
+++ rpy2Jupyter.py
@@ -4,3 +4,9 @@ How the code works:
 * package **requests** is used to load web page from URL given in variable *documentURL* 
 * package **BeautifulSoup4 (bs4)** is used to parse content of loaded web page 
 * the call of *soup.get_text()* in the last line provides the content of page as plain text 
+#_______________________________________________________________________
+import os, rpy2
+os.environ['R_HOME'] = r'C:\Users\username\anaconda3\envs\AC37\lib\R' # workaround for R.dll issue occurring on some systems
+%load_ext rpy2.ipython
+
+#_______________________________________________________________________
