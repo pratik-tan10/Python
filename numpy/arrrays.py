b44ec2dccf6649fdc271a75bddbf86edc2748696
pratik-tan10 <pratik.tan10@gmail.com>
2021-10-04 14:13:34 -0500

    Update arrrays.py

--- numpy/arrrays.py
+++ numpy/arrrays.py
@@ -84,4 +84,40 @@ print(a)
 print(b)
 print(np.corrcoef(a,b))
 
+#fibonacci series
+
+import matplotlib.pyplot as plt
+x = range(200)
+
+ # Program to display the Fibonacci sequence up to n-th term
+
+nterms = x[-1]
+
+# first two terms
+n1, n2 = 0, 1
+count = 0
+fib =[]
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+# if there is only one term, return n1
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+
+# generate fibonacci sequence
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       fib.append(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
+
+print(fib)
+
+plt.plot(x[:-1],fib)
+plt.show()
+
 
