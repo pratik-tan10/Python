d609604e65428eb227f2c77b4dec8e161700c004
pratik-tan10 <pratik.tan10@gmail.com>
2021-10-04 10:30:27 -0500

    Update arrrays.py

--- numpy/arrrays.py
+++ numpy/arrrays.py
@@ -23,3 +23,19 @@ def numpySquare(x):
     return list(np.array(x)**2)
 b= numpySquare(np.array([4,8,4,8,8,8]))
 print(b)
+
+#basic numpy
+
+a = numpy.arange(5)
+print(a)
+a.dtype
+
+b= numpy.zeros(5)
+a=numpy.array([1,2,3,4,5])
+a.shape
+a.size
+a.ndim
+newArray = numpy.array(aList)
+
+a = numpy.array([1,2,3],[6,7,8],[11,51,53])
+print(a)
