fced5dc8abace8f2639c4e34388b0f6936d80cee
pratik-tan10 <pratik.tan10@gmail.com>
2021-10-04 10:22:30 -0500

    Update arrrays.py

--- numpy/arrrays.py
+++ numpy/arrrays.py
@@ -1,6 +1,18 @@
+# with numpy
 import numpy
 def numpysum(n):
   a = np.arange(n) ** 2
   b = np.arange(n) ** 3
   c = a + b
+return c
+
+#without numpy
+def pythonsum(n):
+a = range(n)
+b = range(n)
+c = []
+for i in range(len(a)):
+a[i] = i ** 2
+b[i] = i ** 3
+c.append(a[i] + b[i])
 return c
