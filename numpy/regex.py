ebcac294cb412f082b487d7315894ec747698979
pratik-tan10 <pratik.tan10@gmail.com>
2021-10-07 12:22:47 -0500

    Update regex.py

--- numpy/regex.py
+++ numpy/regex.py
@@ -24,3 +24,10 @@ s = 'A message from csev@umich.edu to cwen@iupui.edu about meeting @2PM'
 lst = re.findall('\S+@\S+', s)
 print(lst)
 
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('\S+@\S+', line)
+    if len(x) > 0:
+        print(x)
+
