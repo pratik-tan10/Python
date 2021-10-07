bf1375a629b833a5d2c26b9ce8c301f32a8e5703
pratik-tan10 <pratik.tan10@gmail.com>
2021-10-07 12:27:17 -0500

    Update regex.py

--- numpy/regex.py
+++ numpy/regex.py
@@ -31,3 +31,10 @@ for line in hand:
     if len(x) > 0:
         print(x)
 
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('[a-zA-Z0-9]\S*@\S*[a-zA-Z0-9]', line)
+    if len(x) > 0:
+        print(x)
+
