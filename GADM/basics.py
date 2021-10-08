2b9f26a69c0429dc9f3c5a1538f35b3b4035615e
pratik-tan10 <pratik.tan10@gmail.com>
2021-10-08 13:53:28 -0500

    Update basics.py

--- GADM/basics.py
+++ GADM/basics.py
@@ -5,3 +5,11 @@ from bs4 import BeautifulSoup
 # specify the URL of the archive here
 template_url = "https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_{}_shp.zip"
 sp = input('Enter your country name.\n')
+
+cdict = {}
+with open("countriesShortform.txt",'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        a = line.split(',')
+        if len(a)>1:
+            cdict[a[0]]=a[1].rstrip()
