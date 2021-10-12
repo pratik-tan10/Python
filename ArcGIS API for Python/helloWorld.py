f43de6745c5026a21bb7089a038924a46d414ddd
pratik-tan10 <pratik.tan10@gmail.com>
2021-10-12 12:30:34 -0500

    Update helloWorld.py

--- ArcGIS API for Python/helloWorld.py
+++ ArcGIS API for Python/helloWorld.py
@@ -9,3 +9,16 @@ user
 user['firstName']
 user.lastName
 
+map1 = gis.map("Palm Springs, CA")
+map1
+
+from IPython.display import display
+
+items = gis.content.search('Palm Springs Trails')
+for item in items:
+    display(item)
+    
+
+# Let us filter out the item with title 'Trails' that we want to add
+item_to_add = [temp_item for temp_item in items if temp_item.title == "Trails"]
+map1.add_layer(item_to_add[0])
