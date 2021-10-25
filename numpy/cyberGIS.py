d5cfe1b32e9153b0bcf5cd858ddd93fdb67f4069
Pratik Dhungana <pratik.tan10@gmail.com>
2021-10-25 12:40:53 -0500

    Update cyberGIS.py

--- numpy/cyberGIS.py
+++ numpy/cyberGIS.py
@@ -11,3 +11,8 @@ plt.plot(range(1)[::-1], 'b*:', label = 'dec')
 plt.plot([4.5]*10, 'gx-', label = 'fix')
 plt.plot(range(10), 'ko-', label = 'ver')
 plt.legend();
+
+plt.plot(range(10))
+plt.gca().add_patch(patches.Circle((5,5),2, edgecolor = 'red', facecolor = 'cyan'))
+plt.gca().add_patch(patches.rectangle((0,0),9,9, linewidth = 10, edgecolor = 'cyan'))
+
