7657424a36ceb7feebc19eebf3d9da30fdeed6f7
Pratik Dhungana <pratik.tan10@gmail.com>
2021-10-27 12:30:06 -0500

    Update fionaAndGdp.py

--- numpy/fionaAndGdp.py
+++ numpy/fionaAndGdp.py
@@ -16,3 +16,19 @@ boroDF.values[0]
 boroDF.columns
 boroDF>values[1]
 boroDF.values[1][1]
+
+boroDF.describe()
+#Transform Dataframe
+boroDF.T
+#sort dataframe
+boroDF.sort_values(by = 'BoroCode', inplace = True, ascending = True)
+boroDF.head()
+
+boroDF[:2]
+boroDF.sort_values(by = 'Shape_area', inplace = True, ascending = False)
+boroDF[:2]
+
+boroDF[['Shape_Araea', 'BoroName']]
+boroDF[['Shape_Araea', 'BoroName']][:2]
+boroDF[:2][['Shape_Araea', 'BoroName',  'Shape_Leng']]
+
