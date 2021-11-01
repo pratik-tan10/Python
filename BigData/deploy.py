5b22dd3edc0666cffb639ede4a8798a78665d6a7
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-01 11:47:49 -0500

    Update deploy.py

--- BigData/deploy.py
+++ BigData/deploy.py
@@ -16,3 +16,12 @@ def binomial_distribution_ci(confidence, p_hat, n):
     rhs = z_score * np.sqrt(p_hat*(1-p_hat))/n
     ci = [p_hat - rhs, p_hat + rhs]
     return ci
+# IPython widget controls for confidence intervals of normal and binomial distributions ...
+normal_label = Label("Normal Distribution Confidence Interval Calculator")
+normal_confidence = FloatSlider(description="confidence", value=0.95, min=0.5, max=0.99, step=0.01)
+normal_x_bar_input = FloatText(value=75.7, min=0, max=100000, step=0.01, description='x bar:', disabled=False)
+normal_sigma_input = FloatText(value=7.3, min=0, max=100000, step=1, description='sigma:', disabled=False)
+normal_n_input = BoundedIntText(value=30, min=0, max=100000, step=1, description='n:', disabled=False)
+normal_button = Button(description="Calculate")
+normal_output = Output()
+
