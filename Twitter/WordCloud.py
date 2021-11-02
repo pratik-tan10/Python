16fbc82d25efc686dce3e12669dfe31eec4b802e
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-02 13:42:33 -0500

    Update WordCloud.py

--- Twitter/WordCloud.py
+++ Twitter/WordCloud.py
@@ -11,3 +11,17 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 #make a CSV of keywords
+def keyword_to_csv(keyword,recent):
+    try:
+        tweets = tweepy.Cursor(api.search,q=keyword).items(recent) #creates query method
+        tweets_list = [[tweet.text] for tweet in tweets] 
+#pulls text information from tweets
+        df = pd.DataFrame(tweets_list,columns=['Text']) 
+#creates a pandas dataframe
+        df.to_csv('{}.csv'.format(keyword), sep=',', index = False) 
+ #creates a csv from data frame
+    except BaseException as e:
+        print('failed on_status,',str(e))
+        time.sleep(3)
+
+        
