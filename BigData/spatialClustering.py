c36d02506dfd5530291e4c6db9a91f712133af7b
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-03 12:18:34 -0500

    Create spatialClustering.py

---
+++ BigData/spatialClustering.py
@@ -0,0 +1,14 @@
+#Library Imports
+%matplotlib inline
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pysal as ps
+import geopandas as gpd
+from sklearn import cluster
+from sklearn.preprocessing import scale
+
+sns.set(style="whitegrid")
+
