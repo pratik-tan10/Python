bbbae4067a25622f8463361d3bacb1d4c15076c1
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-03 12:20:17 -0500

    Update spatialClustering.py

--- BigData/spatialClustering.py
+++ BigData/spatialClustering.py
@@ -12,3 +12,12 @@ from sklearn.preprocessing import scale
 
 sns.set(style="whitegrid")
 
+# Adjust this to point to the right file in your computer
+abb_link = '../data/listings.csv.gz'
+zc_link = '../data/Zipcodes.geojson'
+
+lst = pd.read_csv(abb_link)
+varis = ['bedrooms', 'bathrooms', 'beds']
+aves = lst.groupby('zipcode')[varis].mean()
+aves.info()
+
