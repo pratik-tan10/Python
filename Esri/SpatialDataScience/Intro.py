6e50b1d70d3efe8b68777bae601727e3e0e3a368
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-04 12:22:39 -0500

    Update Intro.py

--- Esri/SpatialDataScience/Intro.py
+++ Esri/SpatialDataScience/Intro.py
@@ -3,3 +3,11 @@ gis = GIS()
 from arcgis.gis import GIS
 gis = GIS('https://www.arcgis.com', '<your username>', '<your password>')
 gis?
+
+import os,sys
+os.environ["PATH"] = r"{};{}".format(os.environ["PATH"], r"C:\Program Files\ArcGIS\Pro\bin")
+sys.path.append(r"C:\Program Files\ArcGIS\Pro\Resources\ArcPy")
+import arcgis
+from arcgis.gis import GIS
+gis = GIS('pro')
+gis?
