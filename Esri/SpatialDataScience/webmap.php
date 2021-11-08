37200ddbf826216744f3086ff9e03e5464702488
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-08 12:08:52 -0600

    Create webmap.php

---
+++ Esri/SpatialDataScience/webmap.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Pratik Dhungana - Request/Response Cycle</title>
+</head>
+<body>
+<h1 style = "background-color: smokewhite; border: 1px solid black; display: inline-block;">Pratik Dhungana Request / Response</h1>
+<p>The SHA256 hash of "Pratik Dhungana" is
+<?php
+echo hash('sha256','Pratik Dhungana')
+?>
+<div style ="width: 25%; border: 2px solid green; padding: 2%">
+<style>
+  .embed-container {position: relative; padding-bottom: 80%; height: 0; max-width: 100%;} .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 90%;} small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}
+</style>
+<div class="embed-container">
+  <small
+    ><a
+      href="//www.arcgis.com/apps/Embed/index.html?webmap=f9b17c0a29fb4403b5730f62b820dab1&extent=-79.6271,38.9017,-75.716,42.4376&home=true&zoom=true&scale=true&basemap_gallery=true&disable_scroll=true&theme=light"
+      style="color:#0000FF;text-align:left; font-size:1rem; text-decoration:none; "
+      target="_top"
+      >View larger map</a
+    ></small
+  ><br /><iframe
+    width="25%"
+    height="25%"
+    frameborder="1"
+    scrolling="no"
+    marginheight="0"
+    marginwidth="0"
+    title="GeoG863"
+    src="//www.arcgis.com/apps/Embed/index.html?webmap=f9b17c0a29fb4403b5730f62b820dab1&extent=-79.6271,38.9017,-75.716,42.4376&home=true&zoom=true&previewImage=false&scale=true&basemap_gallery=true&disable_scroll=true&theme=light"
+  ></iframe>
+</div>
+
+</div>
+</p>
+<pre>ASCII ART:
+
+    ***********
+    ***********
+    **       **
+    **       **
+    ***********
+    ***********
+    **
+    **
+    **
+    **
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
