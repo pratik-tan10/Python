fdadbb675a8da8dcc9ce1c4ff03778a9f54e4bfb
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-08 14:57:52 -0600

    Update webmap.php
    Removed php code

--- Esri/SpatialDataScience/webmap.php
+++ Esri/SpatialDataScience/webmap.php
@@ -6,9 +6,6 @@
 <body>
 <h1 style = "background-color: smokewhite; border: 1px solid black; display: inline-block;">Pratik Dhungana Request / Response</h1>
 <p>The SHA256 hash of "Pratik Dhungana" is
-<?php
-echo hash('sha256','Pratik Dhungana')
-?>
 <div style ="width: 50%; border: 2px solid green; padding: 2%">
 <style>
   .embed-container {position: relative; padding-bottom: 80%; height: 0; max-width: 100%;} .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 90%;} small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}
@@ -31,29 +28,17 @@ echo hash('sha256','Pratik Dhungana')
     title="GeoG863"
     src="https://www.arcgis.com/apps/View/index.html?appid=5aa3a3402d694e8c956a848bb12e1df1"
 </div>
-
 </div>
 <div style ="width: 25%;height: 400px; border: 2px solid green; padding: 2%"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16723.783131514898!2d83.95586696316263!3d28.215140413409127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3995951d11d64f5d%3A0x8f0d3f2ed876286d!2sLake%20Side%2C%20Pokhara%2033700%2C%20Nepal!5e0!3m2!1sen!2sus!4v1636396610477!5m2!1sen!2sus" width=100% height=100% style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
 </p>
-<pre>ASCII ART:
 
-    ***********
-    ***********
-    **       **
-    **       **
-    ***********
-    ***********
-    **
-    **
-    **
-    **
 <form action="/action_page.php">
   <label for="birthday">Birthday:</label>
   <input type="date" id="birthday" name="birthday">
   <input type="submit">
 </form>
 <iframe src="https://calendar.google.com/calendar/htmlembed?src=addressbook%23contacts%40group.v.calendar.google.com&ctz=America%2FChicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-</pre>
+
 <a href="check.php">Click here to check the error setting</a>
 <br/>
 <a href="fail.php">Click here to cause a traceback</a>
