0a230e960cc87a56fb85f9009efcdfc885d9b407
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-09 14:17:24 -0600

    Update SQLite.py
    Create user, post, like and comment tables

--- DB/SQLite.py
+++ DB/SQLite.py
@@ -13,3 +13,55 @@ def create_connection(path):
 
 connection = create_connection(r"C:\Users\Research Lab\Desktop\Pratik\WEB\chuck\sqlite\sm_app.sqlite")
 
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+        
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  age INTEGER,
+  gender TEXT,
+  nationality TEXT
+);
+"""       
+execute_query(connection, create_users_table)  
+
+create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts(
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  title TEXT NOT NULL, 
+  description TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+"""
+execute_query(connection, create_posts_table)
+
+create_comments_table = """
+CREATE TABLE IF NOT EXISTS comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  text TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  post_id INTEGER NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+"""
+
+create_likes_table = """
+CREATE TABLE IF NOT EXISTS likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  user_id INTEGER NOT NULL, 
+  post_id integer NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+"""
+
+execute_query(connection, create_comments_table)  
+execute_query(connection, create_likes_table)
