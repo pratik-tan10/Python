a5ff8f23c03b46c821c6e9fc2a4f9b06fe8232d1
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-09 14:13:39 -0600

    Update MySQL.py

--- DB/MySQL.py
+++ DB/MySQL.py
@@ -1,20 +1,21 @@
 import mysql.connector
 from mysql.connector import Error
 
-def create_connection(host_name, user_name, user_password):
+def create_connection(host_name, user_name, user_password, db_name):
   connection =  None
   try:
     connection = mysql.connector.connect(\
                                          host = host_name,
                                          user = user_name,
-                                         passwd = user_password
+                                         passwd = user_password,
+                                         database = db_name
                                         )
     print("Connection to MySQL DB successful.")
   except Error as e:
     print(f"The Error '{e}' occured")
   return connection
 
-connection = create_connection("localhost", "root", "")
+connection = create_connection("localhost", "root", "", "sm_app")
 
 def create_database(connection, query):
   cursor = connection.cursor()
@@ -24,3 +25,5 @@ def create_database(connection, query):
   except Error as e:
     print(f"The error '{e}' occured")
     
+create_database_query = "CREATE DATABASE sm_app"
+create_database(connection, create_database_query)
