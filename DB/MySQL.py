8ea6d582098b19d54bab7070c9c401ed70cb25ff
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-09 14:19:59 -0600

    Update MySQL.py
    create post, like and comment tables

--- DB/MySQL.py
+++ DB/MySQL.py
@@ -50,3 +50,35 @@ CREATE TABLE IF NOT EXISTS users (
 
 execute_query(connection, create_users_table)
 
+create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts (
+  id INT AUTO_INCREMENT, 
+  title TEXT NOT NULL, 
+  description TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  FOREIGN KEY fk_user_id (user_id) REFERENCES users(id), 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+execute_query(connection, create_posts_table)
+create_comments_table = """
+CREATE TABLE IF NOT EXISTS comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  text TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  post_id INTEGER NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+"""
+
+create_likes_table = """
+CREATE TABLE IF NOT EXISTS likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  user_id INTEGER NOT NULL, 
+  post_id integer NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+"""
+
+execute_query(connection, create_comments_table)  
+execute_query(connection, create_likes_table)
