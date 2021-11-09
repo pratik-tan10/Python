1345017ce57a0284765ece3174a10c29aedc4175
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-09 14:22:23 -0600

    Update SQLite.py

--- DB/SQLite.py
+++ DB/SQLite.py
@@ -78,3 +78,44 @@ VALUES
 """
 
 execute_query(connection, create_users)  
+
+create_posts = """
+INSERT INTO
+  posts (title, description, user_id)
+VALUES
+  ("Happy", "I am feeling very happy today", 1),
+  ("Hot Weather", "The weather is very hot today", 2),
+  ("Help", "I need some help with my work", 2),
+  ("Great News", "I am getting married", 1),
+  ("Interesting Game", "It was a fantastic game of tennis", 5),
+  ("Party", "Anyone up for a late-night party today?", 3);
+"""
+create_comments = """
+INSERT INTO
+  comments (text, user_id, post_id)
+VALUES
+  ('Count me in', 1, 6),
+  ('What sort of help?', 5, 3),
+  ('Congrats buddy', 2, 4),
+  ('I was rooting for Nadal though', 4, 5),
+  ('Help with your thesis?', 2, 3),
+  ('Many congratulations', 5, 4);
+"""
+
+create_likes = """
+INSERT INTO
+  likes (user_id, post_id)
+VALUES
+  (1, 6),
+  (2, 3),
+  (1, 5),
+  (5, 4),
+  (2, 4),
+  (4, 2),
+  (3, 6);
+"""
+
+execute_query(connection, create_comments)
+execute_query(connection, create_likes)
+
+execute_query(connection, create_posts)  
