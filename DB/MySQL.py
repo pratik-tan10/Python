cbf5e9a0e7c943a7faf069d070dcc70eb8dada6e
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-10 17:36:04 -0600

    Update MySQL.py

--- DB/MySQL.py
+++ DB/MySQL.py
@@ -98,3 +98,18 @@ users = execute_read_query(connection, select_users)
 for user in users:
     print(user)
 
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+
+select_users = "SELECT * FROM users"
+users = execute_read_query(connection, select_users)
+
+for user in users:
+    print(user)
