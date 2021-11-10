ebcc25b5f51e69b840e0c49a4677e9daecde1c64
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-10 17:37:31 -0600

    Update SQLite.py
    Updating records

--- DB/SQLite.py
+++ DB/SQLite.py
@@ -200,3 +200,20 @@ post_likes = execute_read_query(connection, select_post_likes)
 for post_like in post_likes:
     print(post_like)
 
+select_post_description = "SELECT description FROM posts WHERE id = 2"
+
+post_description = execute_read_query(connection, select_post_description)
+
+for description in post_description:
+    print(description)
+
+update_post_description = """
+UPDATE
+  posts
+SET
+  description = "The weather has become pleasant now"
+WHERE
+  id = 2
+"""
+
+execute_query(connection, update_post_description)
