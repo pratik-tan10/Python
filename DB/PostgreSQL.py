3dddc539815c0c23dbf342c40af4206dd750a804
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-11 11:46:47 -0600

    Create PostgreSQL.py
    Connecting to a database

---
+++ DB/PostgreSQL.py
@@ -0,0 +1,40 @@
+$ pip install psycopg2
+
+import psycopg2
+from psycopg2 import OperationalError
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
+  
+  connection = create_connection(
+    "postgres", "postgres", "abc123", "127.0.0.1", "5432"
+)
+  
+def create_database(connection, query):
+    connection.autocommit = True
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Query executed successfully")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+
+create_database_query = "CREATE DATABASE sm_app"
+create_database(connection, create_database_query)
+
+connection = create_connection(
+    "sm_app", "postgres", "abc123", "127.0.0.1", "5432"
+)
+
