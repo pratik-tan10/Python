0db7192c698b17aeaa370a932d418eee5f31f299
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-16 13:38:44 -0600

    Create ratatePages.py
    Rotate Pages of pdf

---
+++ PDF/ratatePages.py
@@ -0,0 +1,22 @@
+# rotate_pages.py
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+def rotate_pages(pdf_path):
+    pdf_writer = PdfFileWriter()
+    pdf_reader = PdfFileReader(pdf_path)
+    # Rotate page 90 degrees to the right
+    page_1 = pdf_reader.getPage(0).rotateClockwise(90)
+    pdf_writer.addPage(page_1)
+    # Rotate page 90 degrees to the left
+    page_2 = pdf_reader.getPage(1).rotateCounterClockwise(90)
+    pdf_writer.addPage(page_2)
+    # Add a page in normal orientation
+    pdf_writer.addPage(pdf_reader.getPage(2))
+
+    with open('rotate_pages.pdf', 'wb') as fh:
+        pdf_writer.write(fh)
+
+if __name__ == '__main__':
+    path = 'Jupyter_Notebook_An_Introduction.pdf'
+    rotate_pages(path)
