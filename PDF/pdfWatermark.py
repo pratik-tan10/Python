d2b96a789dbe108cb3462f1dc110eeea0001952b
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-16 13:40:31 -0600

    Create pdfWatermark.py
    Adds watermark to pdf

---
+++ PDF/pdfWatermark.py
@@ -0,0 +1,25 @@
+# pdfWatermark.py
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+def create_watermark(input_pdf, output, watermark):
+    watermark_obj = PdfFileReader(watermark)
+    watermark_page = watermark_obj.getPage(0)
+
+    pdf_reader = PdfFileReader(input_pdf)
+    pdf_writer = PdfFileWriter()
+
+    # Watermark all the pages
+    for page in range(pdf_reader.getNumPages()):
+        page = pdf_reader.getPage(page)
+        page.mergePage(watermark_page)
+        pdf_writer.addPage(page)
+
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
+
+if __name__ == '__main__':
+    create_watermark(
+        input_pdf='Jupyter_Notebook_An_Introduction.pdf', 
+        output='watermarked_notebook.pdf',
+        watermark='watermark.pdf')
