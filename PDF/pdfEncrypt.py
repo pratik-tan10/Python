ead089a90344ae99818f933e2ae86dd0a54b4c66
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-16 13:41:06 -0600

    Create pdfEncrypt.py
    Encrypts a pdf

---
+++ PDF/pdfEncrypt.py
@@ -0,0 +1,21 @@
+# pdfEncrypt.py
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+def add_encryption(input_pdf, output_pdf, password):
+    pdf_writer = PdfFileWriter()
+    pdf_reader = PdfFileReader(input_pdf)
+
+    for page in range(pdf_reader.getNumPages()):
+        pdf_writer.addPage(pdf_reader.getPage(page))
+
+    pdf_writer.encrypt(user_pwd=password, owner_pwd=None, 
+                       use_128bit=True)
+
+    with open(output_pdf, 'wb') as fh:
+        pdf_writer.write(fh)
+
+if __name__ == '__main__':
+    add_encryption(input_pdf='reportlab-sample.pdf',
+                   output_pdf='reportlab-encrypted.pdf',
+                   password='twofish')
