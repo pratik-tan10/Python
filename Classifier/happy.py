bfbc831172c7e4d6ec1c99fb5ab75434987935b6
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-17 15:51:06 -0600

    Update happy.py

--- Classifier/happy.py
+++ Classifier/happy.py
@@ -17,3 +17,13 @@ train = ImageDataGenerator(rescale = 1/255)
 validation = ImageDataGenerator(rescale = 1/255)
 train_dataset = train.flow_from_directory('basedata/train/',rarget_size = (200,200), batch_size = 3,\
 class_mode = 'binary')
+
+#Loading validation dataset
+validation_dataset = train.flow_from_directory('basedata/validation/',rarget_size = (200,200), batch_size = 3,\
+class_mode = 'binary')
+
+#Looking at indices of classes
+train_dataset.class_indices
+
+#Looking at class labels of training datasets
+train_dataset.classes
