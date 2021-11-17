0b1df0fc2966b2f8823972f475b3477cab6ccdde
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-17 15:50:02 -0600

    Update happy.py

--- Classifier/happy.py
+++ Classifier/happy.py
@@ -7,3 +7,13 @@ import tensarflow as tf
 import numpy as np
 import cv2
 import os
+
+#Reading Images
+plt.imshow(img)
+
+cv2.imread("basedata/train/happy/1.PNG").shape
+
+train = ImageDataGenerator(rescale = 1/255)
+validation = ImageDataGenerator(rescale = 1/255)
+train_dataset = train.flow_from_directory('basedata/train/',rarget_size = (200,200), batch_size = 3,\
+class_mode = 'binary')
