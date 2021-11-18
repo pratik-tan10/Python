8d80045cfb6f1b5039796470a5b27a5d951963ab
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-18 16:28:26 -0600

    Update imageFusion.py

--- EarthPy/imageFusion.py
+++ EarthPy/imageFusion.py
@@ -29,3 +29,20 @@ I2 = cv2.resize(I2,I1.shape) # I do this just because i used two random images
 wavelet = 'db1'
 cooef1 = pywt.wavedec2(I1[:,:], wavelet)
 cooef2 = pywt.wavedec2(I2[:,:], wavelet)
+# Second: for each level in both image do the fusion according to the desire option
+fusedCooef = []
+for i in range(len(cooef1)-1):
+
+    # The first values in each decomposition is the apprximation values of the top level
+    if(i == 0):
+
+        fusedCooef.append(fuseCoeff(cooef1[0],cooef2[0],FUSION_METHOD))
+
+    else:
+
+        # For the rest of the levels we have tupels with 3 coeeficents
+        c1 = fuseCoeff(cooef1[i][0],cooef2[i][0],FUSION_METHOD)
+        c2 = fuseCoeff(cooef1[i][1], cooef2[i][1], FUSION_METHOD)
+        c3 = fuseCoeff(cooef1[i][2], cooef2[i][2], FUSION_METHOD)
+
+        fusedCooef.append((c1,c2,c3))
