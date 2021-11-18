6a48f19f54d44bcfbef6d0e6eeac8bef0b7b6de3
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-18 16:27:24 -0600

    Create imageFusion.py
    Import and setup

---
+++ EarthPy/imageFusion.py
@@ -0,0 +1,17 @@
+import pywt
+import cv2
+import numpy as np
+
+# This function does the coefficient fusing according to the fusion method
+def fuseCoeff(cooef1, cooef2, method):
+
+    if (method == 'mean'):
+        cooef = (cooef1 + cooef2) / 2
+    elif (method == 'min'):
+        cooef = np.minimum(cooef1,cooef2)
+    elif (method == 'max'):
+        cooef = np.maximum(cooef1,cooef2)
+    else:
+        cooef = []
+
+    return cooef
