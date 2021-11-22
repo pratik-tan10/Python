60f4b8c4bb1d97f6fbdb87247df0bc0b3c65aff8
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-22 11:31:26 -0600

    Create lulc.py

---
+++ Classifier/lulc.py
@@ -0,0 +1,14 @@
+#magic
+%reload_ext autoreload
+%autoreload 2
+%matplotlib inline
+
+#imports
+from fastai.imports import *
+from fastai.conv_learner import *
+from fastai.model import *
+from fastai.plots import *
+
+import rasterio
+from rasterio import plot
+import matplotlib.pyplot as plt
