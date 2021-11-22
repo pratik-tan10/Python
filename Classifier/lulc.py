b56905cf431157bbc21e33dba43e3175b71f3f67
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-22 11:32:43 -0600

    Update lulc.py

--- Classifier/lulc.py
+++ Classifier/lulc.py
@@ -12,3 +12,29 @@ from fastai.plots import *
 import rasterio
 from rasterio import plot
 import matplotlib.pyplot as plt
+
+#path setup
+PATH = Path('/home/appy/project/EuroSat/Bands/')
+train_path = PATH/'train'
+classes = [str(f).split('/')[-1] for f in list(train_path.iterdir())]
+
+#classes and size
+files = []
+
+for i in classes:
+    paths =train_path/i
+    files.append(list(paths.iterdir())[0])
+classes_num = {}
+for i in classes:
+   
+    folders = train_path/i
+    classes_num[i] = len(list(folders.iterdir()))
+    #print(f'{i} class has {len(list(folders.iterdir()))}')
+plt.figure(figsize=(15,6))
+plt.bar(classes_num.keys(), classes_num.values(), color='green')
+plt.title('Land Use Classes & Size', fontsize=16)
+plt.xlabel('Classes', fontsize=14)
+plt.ylabel('Size', fontsize=14)
+plt.tight_layout()
+plt.savefig('classes.jpg')
+
