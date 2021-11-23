192196135273309598d9a97db99e32f9a9ad36da
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-23 13:21:06 -0600

    Update Script.py

--- ClipTool/Script.py
+++ ClipTool/Script.py
@@ -13,3 +13,25 @@ i=0
 arcpy.AddMessage(f"{i} Lets Begin...")
 fields = ['SHAPE@',nameField]
 
+def copyorclip(outl,fc):
+    if fc not in in_anno and fc not in rasters:
+        arcpy.AddMessage("Clipping " + str(fc))
+        out_fc = str(out_dataset) + '/' + str(fc)
+        arcpy.Clip_analysis(str(fc), clipper, out_fc)
+    elif fc in in_anno:
+        arcpy.AddMessage("Copying " + str(fc))
+        out_fc = str(out_dataset) + '/' + str(fc)
+        arcpy.Copy_management(str(fc), out_fc)
+    else:
+        arcpy.AddMessage("Clipping raster " + str(fc))
+        out_fc = str(out_dataset) + '/' + str(fc)
+
+        desc = arcpy.Describe(clipper)
+
+        xmin = desc.extent.XMin
+        xmax = desc.extent.XMax
+        ymin = desc.extent.YMin
+        ymax = desc.extent.YMax
+
+        arcpy.Clip_management(fc, f"{xmin} {ymin} {xmax} {ymax}", out_fc, clipper, "0", "ClippingGeometry", "MAINTAIN_EXTENT")
+
