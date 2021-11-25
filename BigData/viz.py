5e0a7862d137cec14f708c1777f3f99f95c353f9
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-25 13:24:53 -0600

    Update viz.py

--- BigData/viz.py
+++ BigData/viz.py
@@ -160,5 +160,117 @@ city_id = [0, 1, 2, 3, 4, 5, 6]
 plot_map_fill_multiples_ids(“Multiple Shapes”,
 city_id, sf, color = ‘g’)
 
+#highlight multiple shapes
+# plotting the city on the map to be coloured by using the dist_name
+def plot_cities_2(sf, title, cities, color):
+    
+    df = read_shapefile(sf)
+    city_id = []
+    for i in cities:
+        city_id.append(df[df.DIST_NAME == i.upper()]
+                         .index.get_values()[0])
+    plot_map_fill_multiples_ids(title, city_id, sf, 
+                                       x_lim = None, 
+                                       y_lim = None, 
+                                       figsize = (11,9), 
+                                       color = color);
+south = [‘jaipur’,’churu’,’bikaner’]
+plot_cities_2(sf, ‘DIST’, south, ‘c’)
+
+#heatmap
+def calc_color(data, color=None):
+        if color   == 1: 
+            color_sq =  ['#dadaebFF','#bcbddcF0','#9e9ac8F0','#807dbaF0','#6a51a3F0','#54278fF0']; 
+            colors = 'Purples';
+        elif color == 2: 
+            color_sq = ['#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494']; 
+            colors = 'YlGnBu';
+        elif color == 3: 
+            color_sq = ['#f7f7f7','#d9d9d9','#bdbdbd','#969696','#636363','#252525']; 
+            colors = 'Greys';
+        elif color == 9: 
+            color_sq = ['#ff0000','#ff0000','#ff0000','#ff0000','#ff0000','#ff0000'];
+                        
+        else:           
+            color_sq = ['#ffffd4','#fee391','#fec44f','#fe9929','#d95f0e','#993404']; 
+            colors = 'YlOrBr';
+        new_data, bins = pd.qcut(data, 6, retbins=True, 
+        labels=list(range(6)))
+        color_ton = []
+        for val in new_data:
+            color_ton.append(color_sq[val]) 
+        if color != 9:
+            colors = sns.color_palette(colors, n_colors=6)
+            sns.palplot(colors, 0.6);
+            for i in range(6):
+                print ("\n"+str(i+1)+': '+str(int(bins[i]))+
+                       " => "+str(int(bins[i+1])-1))
+            print("\n\n   1   2   3   4   5   6")    
+        return color_ton, bins;
+
+def plot_cities_data(sf, title, cities, data=None,color=None, print_id=False):
+ 
+    color_ton, bins = calc_color(data, color)
+    df = read_shapefile(sf)
+    city_id = []
+    for i in cities:
+        city_id.append(df[df.DIST_NAME == 
+                            i.upper()].index.get_values()[0])
+    plot_map_fill_multiples_ids_tone(sf, title, city_id, 
+                                     print_id, 
+                                     color_ton, 
+                                     bins, 
+                                     x_lim = None, 
+                                     y_lim = None, 
+                                     figsize = (11,9));
+def plot_map_fill_multiples_ids_tone(sf, title, city,  
+                                     print_id, color_ton, 
+                                     bins, 
+                                     x_lim = None, 
+                                     y_lim = None, 
+                                     figsize = (11,9)):
+   
+        
+    plt.figure(figsize = figsize)
+    fig, ax = plt.subplots(figsize = figsize)
+    fig.suptitle(title, fontsize=16)
+    for shape in sf.shapeRecords():
+        x = [i[0] for i in shape.shape.points[:]]
+        y = [i[1] for i in shape.shape.points[:]]
+        ax.plot(x, y, 'k')
+            
+    for id in city:
+        shape_ex = sf.shape(id)
+        x_lon = np.zeros((len(shape_ex.points),1))
+        y_lat = np.zeros((len(shape_ex.points),1))
+        for ip in range(len(shape_ex.points)):
+            x_lon[ip] = shape_ex.points[ip][0]
+            y_lat[ip] = shape_ex.points[ip][1]
+        ax.fill(x_lon,y_lat, color_ton[city.index(id)])
+        if print_id != False:
+            x0 = np.mean(x_lon)
+            y0 = np.mean(y_lat)
+            plt.text(x0, y0, id, fontsize=10)
+    if (x_lim != None) & (y_lim != None):     
+        plt.xlim(x_lim)
+        plt.ylim(y_lim)
+        
+names= [‘jaipur’,’bikaner’,’churu’,’bhilwara’,’udaipur’]
+data = [100, 2000, 300, 400000, 500, 600, 100, 2000, 300, 400, 500, 600, 100, 2000, 300, 400, 500, 600]
+print_id = True # The shape id will be printed
+color_pallete = 1 # ‘Purple’
+plot_cities_data(sf, ‘Heat map of given cities’, names, data, color_pallete, print_id)
+
+#plot real data
+# reading data set
+census_17 = df.POPULATION
+census_17.shape
+#plotting
+title = ‘Population Distrubution on Rajasthan Region’
+data = census_17
+names = df.DIST_NAME
+plot_cities_data(sf, title, names, data, 1, True)
+
+
 
 
