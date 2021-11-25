6863646f2d0dc246110f6f3c9b48873c80b4d0d3
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-25 13:19:57 -0600

    Update viz.py

--- BigData/viz.py
+++ BigData/viz.py
@@ -20,3 +20,20 @@ print(sf.records())
 print(sf.records()[1][0])
 
 Output= Barmer
+
+#shp to pandas dataframe
+def read_shapefile(sf):
+    #fetching the headings from the shape file
+    fields = [x[0] for x in sf.fields][1:]
+#fetching the records from the shape file
+    records = [list(i) for i in sf.records()]
+    shps = [s.points for s in sf.shapes()]
+#converting shapefile data into pandas dataframe
+    df = pd.DataFrame(columns=fields, data=records)
+#assigning the coordinates
+    df = df.assign(coords=shps)
+    return df
+  
+df = read_shapefile(sf)
+df.shape
+
