b27068f324625983ea234e56342449bec3a608c4
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-25 13:18:43 -0600

    Create viz.py

---
+++ BigData/viz.py
@@ -0,0 +1,14 @@
+!conda/pip install pyshp
+import numpy as np
+import pandas as pd
+import shapefile as shp
+import matplotlib.pyplot as plt
+import seaborn as sns
+#initialize visualization set
+sns.set(style=”whitegrid”, palette=”pastel”, color_codes=True) sns.mpl.rc(“figure”, figsize=(10,6))
+
+#open vecotr map
+#opening the vector map
+shp_path = “\\District_Boundary.shp”
+#reading the shape file by using reader function of the shape lib
+sf = shp.Reader(shp_path)
