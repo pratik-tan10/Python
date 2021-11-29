95fdd165eecba04d2aab9822aa97f6d294647d73
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-29 11:47:07 -0600

    Update runPyFileInInteractiveMode.py

--- ClassProjectNov/runPyFileInInteractiveMode.py
+++ ClassProjectNov/runPyFileInInteractiveMode.py
@@ -2,11 +2,16 @@ import runpy
 runpy.run_path(path_name='hello.py')
 
 #Change processing extent
-desc = arcpy.Describe(clipper)
+def setExtent(clipper):
+  desc = arcpy.Describe(clipper)
 
-xmin = desc.extent.XMin
-xmax = desc.extent.XMax
-ymin = desc.extent.YMin
-ymax = desc.extent.YMax
+  xmin = desc.extent.XMin
+  xmax = desc.extent.XMax
+  ymin = desc.extent.YMin
+  ymax = desc.extent.YMax
 
-arcpy.env.extent = arcpy.Extent(xmin, ymin, xmax, ymax)
+  arcpy.env.extent = arcpy.Extent(xmin, ymin, xmax, ymax)
+
+  def setExtentAlternative(clipper):
+    try:arcpy.env.extent = clipper.extent
+    except: arcpy.env.extent = row[0].extent
