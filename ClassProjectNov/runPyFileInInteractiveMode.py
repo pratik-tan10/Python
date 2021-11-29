d078af28032f85df68187c89bd73134317d180cf
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-29 11:45:19 -0600

    Update runPyFileInInteractiveMode.py

--- ClassProjectNov/runPyFileInInteractiveMode.py
+++ ClassProjectNov/runPyFileInInteractiveMode.py
@@ -1,2 +1,12 @@
 import runpy
 runpy.run_path(path_name='hello.py')
+
+#Change processing extent
+desc = arcpy.Describe(clipper)
+
+xmin = desc.extent.XMin
+xmax = desc.extent.XMax
+ymin = desc.extent.YMin
+ymax = desc.extent.YMax
+
+arcpy.env.extent = arcpy.Extent(xmin, ymin, xmax, ymax)
