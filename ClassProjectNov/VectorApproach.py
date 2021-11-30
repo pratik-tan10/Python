40db4748784ad626124c5ea8dc7debaecacda269
Pratik Dhungana <pratik.tan10@gmail.com>
2021-11-30 17:24:45 -0600

    Update VectorApproach.py

--- ClassProjectNov/VectorApproach.py
+++ ClassProjectNov/VectorApproach.py
@@ -4,3 +4,11 @@ import numpy as np
 ds = arcpy.Describe(fc)
 fname = 'CategoryVector'
 fields = ['SHAPE@',fname,'OBJECTID_1']
+
+i = 0
+vectorList = []
+with arcpy.da.SearchCursor(fc, fields) as rows:
+    for row in rows:
+        if i ==2: break
+        arcpy.MakeFeatureLayer_management(fc,'selector')
+
