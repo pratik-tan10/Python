2d3e8ccb1fe5d614b4c80f4109dc2f3283d863c4
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-01 19:17:23 -0600

    Update parallelProcessing.py

--- BigData/parallelProcessing.py
+++ BigData/parallelProcessing.py
@@ -1,3 +1,4 @@
+
 from multiprocessing import Pool
 import time
 import plotly.express as px
@@ -5,7 +6,9 @@ import plotly
 import pandas as pd
 
 def f(x):
+    time.sleep(2)
     return x**2
+
 
 def runner(list_length):
     print(f"Size of List:{list_length}")
@@ -22,14 +25,15 @@ def runner(list_length):
     print(f"With multiprocessing we ran the function in {t1 - t0:0.4f} seconds")
     time_with_multiprocessing = t1-t0
     return time_without_multiprocessing, time_with_multiprocessing
+
 if __name__ ==  '__main__':
     times_taken = []
-    for i in range(1,9):
-        list_length = 10**i
+    for i in range(1,10):
+        list_length = i
         time_without_multiprocessing, time_with_multiprocessing = runner(list_length)
         times_taken.append([list_length, 'No Mutiproc', time_without_multiprocessing])
         times_taken.append([list_length, 'Multiproc', time_with_multiprocessing])
 
     timedf = pd.DataFrame(times_taken,columns = ['list_length', 'type','time_taken'])
-    fig =  px.line(timedf,x = 'list_length',y='time_taken',color='type',log_x=True)
+    fig =  px.line(timedf,x = 'list_length',y='time_taken',color='type')
     plotly.offline.plot(fig, filename='comparison_bw_multiproc.html')
