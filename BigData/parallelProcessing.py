aa713a8ae31b7bfff273a1cbc0ef204afb1c3d6c
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-01 19:20:29 -0600

    Update parallelProcessing.py

--- BigData/parallelProcessing.py
+++ BigData/parallelProcessing.py
@@ -4,6 +4,7 @@ import time
 import plotly.express as px
 import plotly
 import pandas as pd
+from joblib import Parallel, delayed
 
 def f(x):
     time.sleep(2)
@@ -13,9 +14,9 @@ def f(x):
 def runner(list_length):
     print(f"Size of List:{list_length}")
     t0 = time.time()
-    result1 = [f(x) for x in list(range(list_length))]
+    result1 = Parallel(n_jobs=8)(delayed(f)(i) for i in range(list_length))
     t1 = time.time()
-    print(f"Without multiprocessing we ran the function in {t1 - t0:0.4f} seconds")
+    print(f"With joblib we ran the function in {t1 - t0:0.4f} seconds")
     time_without_multiprocessing = t1-t0
     t0 = time.time()
     pool = Pool(8)
@@ -28,7 +29,7 @@ def runner(list_length):
 
 if __name__ ==  '__main__':
     times_taken = []
-    for i in range(1,10):
+    for i in range(1,16):
         list_length = i
         time_without_multiprocessing, time_with_multiprocessing = runner(list_length)
         times_taken.append([list_length, 'No Mutiproc', time_without_multiprocessing])
@@ -63,3 +64,11 @@ pool.close()
 
 
 pool.close()
+
+#Using joblib and single param function
+from joblib import Parallel, delayed
+import time
+def f(x):
+    time.sleep(2)
+    return x**2
+results = Parallel(n_jobs=8)(delayed(f)(i) for i in range(10))
