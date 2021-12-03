3676a0169906447af6be2754845d1ffe6d185b4e
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-03 17:41:46 -0600

    Update gee.py

--- BigData/gee.py
+++ BigData/gee.py
@@ -10,3 +10,19 @@ import subprocess
 import sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+pkgs = ['geemap', 'leafmap', 'geopandas']
+if "google.colab" in sys.modules:
+    for pkg in pkgs:
+        install(pkg)
+
+import os
+import ee
+import geemap
+
+Map = geemap.Map() 
+Map
+Map = geemap.Map(center=(40, -100), zoom=4, lite_mode=True) 
+Map
+Map = geemap.Map()
+Map.add_basemap('HYBRID')
+Map
