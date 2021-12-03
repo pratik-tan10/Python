b1ca969dd491175dff3b211e0a5a9c4a4df5ce3d
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-03 17:43:33 -0600

    Update gee.py

--- BigData/gee.py
+++ BigData/gee.py
@@ -60,3 +60,25 @@ Map.addLayer(image, vizParams, 'false color composite');
 
 """
 geemap.js_snippet_to_py(js_snippet, add_new_cell=True, import_ee=True, import_geemap=True, show_map=True)
+
+import ee
+import geemap
+
+Map = geemap.Map()
+
+# Load an image.
+image = ee.Image('LANDSAT/LC08/C01/T1_TOA/LC08_044034_20140318')
+
+# Define the visualization parameters.
+vizParams = {
+  'bands': ['B5', 'B4', 'B3'],
+  'min': 0,
+  'max': 0.5,
+  'gamma': [0.95, 1.1, 1]
+}
+
+# Center the map and display the image.
+Map.setCenter(-122.1899, 37.5010, 10); # San Francisco Bay
+Map.addLayer(image, vizParams, 'False color composite')
+Map
+
