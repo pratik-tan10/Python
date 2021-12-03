a2d623b54db9f9f0ce08dcb7dc5b7b1ffb1cba00
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-03 17:43:05 -0600

    Update gee.py

--- BigData/gee.py
+++ BigData/gee.py
@@ -39,3 +39,23 @@ Map
 naip_url = 'https://services.nationalmap.gov/arcgis/services/USGSNAIPImagery/ImageServer/WMSServer?'
 Map.add_wms_layer(url=naip_url, layers='0', name='NAIP Imagery', format='image/png', shown=True)
 
+Map = geemap.Map()
+Map
+
+js_snippet = """
+// Load an image.
+var image = ee.Image('LANDSAT/LC08/C01/T1_TOA/LC08_044034_20140318');
+
+// Define the visualization parameters.
+var vizParams = {
+  bands: ['B5', 'B4', 'B3'],
+  min: 0,
+  max: 0.5,
+  gamma: [0.95, 1.1, 1]
+};
+
+// Center the map and display the image.
+Map.setCenter(-122.1899, 37.5010, 10); // San Francisco Bay
+Map.addLayer(image, vizParams, 'false color composite');
+
+"""
