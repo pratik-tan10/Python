fbee4211836326dfcac62ec63bee122e4c21321e
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-07 17:16:06 -0600

    Update seabornEx.py

--- BigData/seabornEx.py
+++ BigData/seabornEx.py
@@ -4,3 +4,10 @@ sns.histplot(data=penguins, x="flipper_length_mm", hue="species", multiple="stac
 sns.kdeplot(data=penguins, x="flipper_length_mm", hue="species", multiple="stack")
 sns.displot(data=penguins, x="flipper_length_mm", hue="species", multiple="stack")
 sns.displot(data=penguins, x="flipper_length_mm", hue="species", multiple="stack", kind="kde")
+
+sns.displot(data=penguins, x="flipper_length_mm", hue="species", col="species")
+f, axs = plt.subplots(1, 2, figsize=(8, 4), gridspec_kw=dict(width_ratios=[4, 3]))
+sns.scatterplot(data=penguins, x="flipper_length_mm", y="bill_length_mm", hue="species", ax=axs[0])
+sns.histplot(data=penguins, x="species", hue="species", shrink=.8, alpha=.8, legend=False, ax=axs[1])
+f.tight_layout()
+
