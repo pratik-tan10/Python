607218f6d40544b03bb791044632e32a5c0b278a
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-09 20:13:41 -0600

    Update seabornEx.py

--- BigData/seabornEx.py
+++ BigData/seabornEx.py
@@ -9,3 +9,7 @@ sns.relplot(x="total_bill", y="tip", data=tips);
 sns.relplot(x="total_bill", y="tip", hue="smoker", data=tips);
 sns.relplot(x="total_bill", y="tip", hue="smoker", style="smoker",
             data=tips);
+sns.relplot(x="total_bill", y="tip", hue="smoker", style="time", data=tips);
+sns.relplot(x="total_bill", y="tip", hue="size", data=tips);
+sns.relplot(x="total_bill", y="tip", hue="size", palette="ch:r=-.5,l=.75", data=tips);
+sns.relplot(x="total_bill", y="tip", size="size", data=tips);
