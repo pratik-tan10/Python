9790ee60c3338464fd093a5a160dcecca9b03048
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-11 19:57:18 -0600

    Update folium.py
    choropleth map added

--- BigData/folium.py
+++ BigData/folium.py
@@ -143,3 +143,29 @@ folium.TopoJson(
 
 folium.LayerControl().add_to(m)
 m
+
+import pandas as pd
+
+url = (
+    "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data"
+)
+state_geo = f"{url}/us-states.json"
+state_unemployment = f"{url}/US_Unemployment_Oct2012.csv"
+state_data = pd.read_csv(state_unemployment)
+
+m = folium.Map(location=[48, -102], zoom_start=3)
+
+folium.Choropleth(
+    geo_data=state_geo,
+    name="choropleth",
+    data=state_data,
+    columns=["State", "Unemployment"],
+    key_on="feature.id",
+    fill_color="YlGn",
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name="Unemployment Rate (%)",
+).add_to(m)
+
+folium.LayerControl().add_to(m)
+m
