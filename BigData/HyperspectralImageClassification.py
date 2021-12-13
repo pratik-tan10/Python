0e510e43c5e0a089595420b86ad3f57bb099bcf7
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-12 18:00:45 -0600

    Update HyperspectralImageClassification.py

--- BigData/HyperspectralImageClassification.py
+++ BigData/HyperspectralImageClassification.py
@@ -1,125 +1,17 @@
 #Create CNN
-model = Sequential(name = 'Salinas_CNN')
-
-model.add(Conv1D(filters = 64, kernel_size = 3, activation ='relu', input_shape =(ip_shape[0],1), name = 'Layer1'))
-model.add(Conv1D(filters = 64, kernel_size = 3, activation ='relu' , name = 'Layer2'))
-model.add(Conv1D(filters = 64, kernel_size = 3, activation ='relu' , name = 'Layer3'))
-
-model.add(MaxPooling1D(pool_size = 2, name = 'MaxPooling_Layer1'))
-model.add(Dropout(0.4, name = 'Dropout1'))
-
-model.add(Conv1D(filters = 32, kernel_size = 3, activation ='relu', name = 'Layer4'))
-model.add(Conv1D(filters = 32, kernel_size = 3, activation ='relu', name = 'Layer5'))
-model.add(Conv1D(filters = 32, kernel_size = 3, activation ='relu', name = 'Layer6'))
-
-model.add(MaxPooling1D(pool_size = 2, name = 'MaxPooling_Layer2'))
-model.add(Dropout(0.4, name = 'Dropout2'))
-
-model.add(Flatten(name = 'Flatten'))
-
-model.add(Dense(25, activation='relu', name = 'DenseLayer'))
-
-model.add(Dense(n_outputs, activation='softmax', name = 'OutputLayer'))
-
-
-model.summary()
 
 
 #Prepare data
-data = loadmat('Salinas_corrected.mat')['salinas_corrected']
 
-gt = loadmat('Salinas_gt.mat')['salinas_gt']
-
-print(f'Data Shape: {data.shape[:-1]}\nNumber of Bands: {data.shape[-1]}')
-
-df = pd.DataFrame(data.reshape(data.shape[0]*data.shape[1], -1))
-
-df.columns = [f'band{i}' for i in range(1, df.shape[-1]+1)]
-
-df['class'] = gt.ravel()
 
 #Split train and test data
-t_df = df[df['class']!=0]
 
-ind = ['band'+str(i) for i in range(1, t_df.shape[-1])]
-
-X = t_df.loc[:, ind]
-
-y = to_categorical(t_df.loc[:, 'class'])
-
-X_scaled = minmax_scale(X, axis = 0);
-
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y,
-                                                    stratify=y, 
-                                                    test_size=0.30)
-print(f"X_train: {X_train.shape}\ny_train: {y_train.shape}\nX_test: {X_test.shape}\ny_test: {y_test.shape}")
 
 #Train the CNN model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-early_stop = EarlyStopping(monitor = 'val_loss',
-                            mode = 'min',
-                            min_delta = 0,
-                            patience = 10,
-                            restore_best_weights = True)
-
-checkpoint = ModelCheckpoint(filepath = 'Salinas_Model.h5', 
-                             monitor = 'val_loss', 
-                             mode ='min', 
-                             save_best_only = True)
-
-tensorboard = TensorBoard(log_dir='SA_logs/{}'.format(time()))
-
-hist = model.fit(X_train, 
-                       y_train, 
-                       epochs = 100, 
-                       batch_size = 256 , 
-                       validation_data = (X_test, y_test), 
-                       callbacks=[early_stop,
-                                  checkpoint,
-                                  tensorboard])
 
 #Plot graphs
 
-hist = pd.DataFrame(data= hist.history)
-
-fig = go.Figure()
-
-fig.add_trace(go.Scatter(x = hist.index, y = hist.loss.values,
-                    mode='lines+markers',
-                    name='Train Loss'))
-
-fig.add_trace(go.Scatter(x = hist.index, y = hist.accuracy.values,
-                    mode='lines+markers',
-                    name='Train Accuracy'))
-
-fig.add_trace(go.Scatter(x = hist.index, y = hist.val_loss.values,
-                    mode='lines+markers', name='Test loss'))
-
-fig.add_trace(go.Scatter(x = hist.index, y = hist.val_accuracy.values,
-                    mode='lines+markers', name='Test Accuracy'))
-
-fig.show()
-
 #View Results as confusion matrix
-plt.figure(figsize = (10,7))
-
-classes = ['1.Brocoli_green_weeds_1', '2.Brocoli_green_weeds_2',
-                      '3.Fallow',
-                      '4.Fallow_rough_plow',
-                      '5.Fallow_smooth',
-                      '6.Stubble',
-                      '7.Celery',
-                      '8.Grapes_untrained',
-                      '9.Soil_vinyard_develop',
-                      '10.Corn_senesced_green_weeds',
-                      '11.Lettuce_romaine_4wk',
-                      '12.Lettuce_romaine_5wk',
-                      '13.Lettuce_romaine_6wk',
-                      '14.Lettuce_romaine_7wk',
-                      '15.Vinyard_untrained',
-                      '16.Vinyard_vertical_trellis']
-
 mat = confusion_matrix(np.add(pred, 1), np.add(np.argmax(y_test, 1), 1))
 
 df_cm = pd.DataFrame(mat, index = classes, columns = classes)
@@ -127,24 +19,3 @@ df_cm = pd.DataFrame(mat, index = classes, columns = classes)
 sns.heatmap(df_cm, annot=True, fmt='d')
 
 plt.show()
-
-#Generate Classification report
-pred = np.argmax(model.predict(X_test), axis=1)
-
-# Classification Report
-print(classification_report(pred, np.argmax(y_test, 1),
-      target_names = ['1.Brocoli_green_weeds_1', '2.Brocoli_green_weeds_2',
-                      '3.Fallow',
-                      '4.Fallow_rough_plow',
-                      '5.Fallow_smooth',
-                      '6.Stubble',
-                      '7.Celery',
-                      '8.Grapes_untrained',
-                      '9.Soil_vinyard_develop',
-                      '10.Corn_senesced_green_weeds',
-                      '11.Lettuce_romaine_4wk',
-                      '12.Lettuce_romaine_5wk',
-                      '13.Lettuce_romaine_6wk',
-                      '14.Lettuce_romaine_7wk',
-                      '15.Vinyard_untrained',
-                      '16.Vinyard_vertical_trellis']))
