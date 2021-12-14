b4b8e0997da4d658b0dce5fea481e768846a998e
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-14 15:25:49 -0600

    Update MySQL.py

--- DB/MySQL.py
+++ DB/MySQL.py
@@ -41,88 +41,11 @@ create_users_table = """
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT, 
   name TEXT NOT NULL, 
-  age INT, 
-  gender TEXT, 
-  nationality TEXT, 
+  Age INT, 
+  Sex TEXT, 
+  Country TEXT, 
   PRIMARY KEY (id)
 ) ENGINE = InnoDB
 """
 
 execute_query(connection, create_users_table)
-
-create_posts_table = """
-CREATE TABLE IF NOT EXISTS posts (
-  id INT AUTO_INCREMENT, 
-  title TEXT NOT NULL, 
-  description TEXT NOT NULL, 
-  user_id INTEGER NOT NULL, 
-  FOREIGN KEY fk_user_id (user_id) REFERENCES users(id), 
-  PRIMARY KEY (id)
-) ENGINE = InnoDB
-"""
-execute_query(connection, create_posts_table)
-create_comments_table = """
-CREATE TABLE IF NOT EXISTS comments (
-  id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  text TEXT NOT NULL, 
-  user_id INTEGER NOT NULL, 
-  post_id INTEGER NOT NULL, 
-  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
-);
-"""
-
-create_likes_table = """
-CREATE TABLE IF NOT EXISTS likes (
-  id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  user_id INTEGER NOT NULL, 
-  post_id integer NOT NULL, 
-  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
-);
-"""
-
-execute_query(connection, create_comments_table)  
-execute_query(connection, create_likes_table)
-
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-    except Error as e:
-        print(f"The error '{e}' occurred")
-select_users = "SELECT * FROM users"
-users = execute_read_query(connection, select_users)
-
-for user in users:
-    print(user)
-
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-
-select_users = "SELECT * FROM users"
-users = execute_read_query(connection, select_users)
-
-for user in users:
-    print(user)
-update_post_description = """
-UPDATE
-  posts
-SET
-  description = "The weather has become pleasant now"
-WHERE
-  id = 2
-"""
-
-execute_query(connection,  update_post_description)
-
-delete_comment = "DELETE FROM comments WHERE id = 2"
-execute_query(connection, delete_comment)
