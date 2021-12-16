f2dd0e5e8378802334ad3ae10060afd609862679
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-15 20:14:48 -0600

    Create variableInput.py

---
+++ pands/variableInput.py
@@ -0,0 +1,7 @@
+def test_var_args(f_arg, *argv):
+    print("first normal arg:", f_arg)
+    for arg in argv:
+        print("another arg through *argv:", arg)
+
+test_var_args('yasoob', 'python', 'eggs', 'test')
+
