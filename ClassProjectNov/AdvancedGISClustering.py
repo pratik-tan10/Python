587b2d0501a70378757a4d707907711c93ff0333
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-16 23:50:10 -0600

    Update AdvancedGISClustering.py

--- ClassProjectNov/AdvancedGISClustering.py
+++ ClassProjectNov/AdvancedGISClustering.py
@@ -25,3 +25,17 @@ with arcpy.da.SearchCursor(fc, fields) as rows:
             #i = i+1
             j = row[2]
 
+len(vectorList)
+def prob(a,b,w1=0.7):
+    try:
+        c = a==b
+        N = len(a)
+        p = ((sum(c))/N)*w1 + (1-len(set(b[c]))/N)*(1-w1)
+        #print(f"a:{a},b:{b},c:{c},p:{p}")
+        #p = len(set(b[c]))/N
+        '''c w1 is weight to similarity
+        w2 is weight to variety in crops
+        '''
+        return p
+    except: return 0
+
