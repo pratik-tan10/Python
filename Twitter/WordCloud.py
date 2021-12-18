a7600ce6481e865f40c353ad54266570f9c36095
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-17 21:33:21 -0600

    Update WordCloud.py

--- Twitter/WordCloud.py
+++ Twitter/WordCloud.py
@@ -30,3 +30,13 @@ import matplotlib.pyplot as plt
 # Load in the dataframe
 df = pd.read_csv("data/winemag-data-130k-v2.csv", index_col=0)
 
+# Looking at first 5 rows of the dataset
+df.head()
+
+print("There are {} observations and {} features in this dataset. \n".format(df.shape[0],df.shape[1]))
+
+print("There are {} types of wine in this dataset such as {}... \n".format(len(df.variety.unique()),
+                                                                           ", ".join(df.variety.unique()[0:5])))
+
+print("There are {} countries producing wine in this dataset such as {}... \n".format(len(df.country.unique()),
+                                                                                      ", ".join(df.country.unique()[0:5])))
