50e456823ec6e58c5ee25e67a3e1007253878bd8
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-17 21:34:29 -0600

    Update WordCloud.py

--- Twitter/WordCloud.py
+++ Twitter/WordCloud.py
@@ -48,3 +48,10 @@ country = df.groupby("country")
 # Summary statistic of all countries
 country.describe().head()
 
+country.mean().sort_values(by="points",ascending=False).head()
+plt.figure(figsize=(15,10))
+country.size().sort_values(ascending=False).plot.bar()
+plt.xticks(rotation=50)
+plt.xlabel("Country of Origin")
+plt.ylabel("Number of Wines")
+plt.show()
