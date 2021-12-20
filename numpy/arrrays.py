079c9300a04ebab0c0c0deeeac6d359b66382cec
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-19 23:44:38 -0600

    Update arrrays.py

--- numpy/arrrays.py
+++ numpy/arrrays.py
@@ -129,3 +129,5 @@ C = [[1.0,1.5,-2.0],[2.0,1.0,-1.0],[3.0,-1.0,2.0]]
 [[A[i][j]+B[i][j] for j in range(len(B[0]))] for i in range(len(A))]  # the matrix A+B
 sum(v[i]*v[i] for i in range(len(v)))  # the dot product v.v
 
+[sum(A[i][j]*v[j] for j in range(len(v))) for i in range(len(A))]  # the vector A*v
+
