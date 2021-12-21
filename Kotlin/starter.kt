03cbaf40022f3ce4697c55d697d4124232c60c74
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-20 19:03:19 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -25,3 +25,9 @@ fun printBorder(n: Int, x: String){
         println()
     }
 }
+
+fun main() {
+    val diceRange = 1..6
+     val randomNumber = diceRange.random()
+     println("Random number: ${randomNumber}")     
+}
