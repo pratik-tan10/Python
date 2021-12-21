089337c438f53cc99e14d1931e0be4c8ddefcc2f
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-20 19:06:39 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -28,8 +28,7 @@ fun printBorder(n: Int, x: String){
 
 fun main() {
     val myFirstDice = Dice(6)
-    val diceRoll = myFirstDice.roll()
-    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+    println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}!")
     
     val mySecondDice = Dice(20)
     println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
@@ -38,7 +37,6 @@ fun main() {
 class Dice (val numSides: Int) {
 
     fun roll(): Int {
-        val randomNumber = (1..numSides).random()
-        return randomNumber
+        return (1..numSides).random()
     }
 }
