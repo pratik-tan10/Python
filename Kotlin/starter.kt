d8ff39d95b6bb813a2744ec43d2c7e634363080c
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-21 23:05:39 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -46,17 +46,7 @@ fun main() {
     val rollResult = myFirstDice.roll()
     val luckyNumber = 4
 
-    if (rollResult == luckyNumber) {
-        println("You win!")
-    } else if (rollResult == 1) {
-        println("So sorry! You rolled a 1. Try again!")
-    } else if (rollResult == 2) {
-        println("Sadly, you rolled a 2. Try again!")
-    } else if (rollResult == 3) {
-        println("Unfortunately, you rolled a 3. Try again!")
-    } else if (rollResult == 5) {
-        println("Don't cry! You rolled a 5. Try again!")
-    } else {
-        println("Apologies! You rolled a 6. Try again!")
+    when (rollResult) {
+        luckyNumber -> println("You win!")
     }
 }
