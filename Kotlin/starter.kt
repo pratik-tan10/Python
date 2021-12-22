f3917e766ee4fc396532cf237f052f60a17b1b6d
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-21 23:03:12 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -40,3 +40,10 @@ class Dice (val numSides: Int) {
         return (1..numSides).random()
     }
 }
+#Conditional
+fun main() {
+   val num = 5
+   if (num > 4) {
+       println("The variable is greater than 4")
+   }
+}
