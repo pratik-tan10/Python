db18a7e380d0e7c8c3d56360c80fc51e4545936b
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-21 23:04:12 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -42,9 +42,11 @@ class Dice (val numSides: Int) {
 }
 #Conditional
 fun main() {
-    val num = 3
+    val num = 4
     if (num > 4) {
         println("The variable is greater than 4")
+    } else if (num == 4) {
+        println("The variable is equal to 4")
     } else {
         println("The variable is less than 4")
     }
