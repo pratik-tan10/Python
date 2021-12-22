73c2b8620b4a3dd829daeb4a7a4effc97a172d61
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-21 23:06:08 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -47,6 +47,17 @@ fun main() {
     val luckyNumber = 4
 
     when (rollResult) {
-        luckyNumber -> println("You win!")
+        luckyNumber -> println("You won!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        6 -> println("Apologies! You rolled a 6. Try again!")
+   }
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
