4b549af78f2ace554f07fceece2af0f61339fdb9
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-21 23:04:44 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -42,12 +42,21 @@ class Dice (val numSides: Int) {
 }
 #Conditional
 fun main() {
-    val num = 4
-    if (num > 4) {
-        println("The variable is greater than 4")
-    } else if (num == 4) {
-        println("The variable is equal to 4")
+    val myFirstDice = Dice(6)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 4
+
+    if (rollResult == luckyNumber) {
+        println("You win!")
+    } else if (rollResult == 1) {
+        println("So sorry! You rolled a 1. Try again!")
+    } else if (rollResult == 2) {
+        println("Sadly, you rolled a 2. Try again!")
+    } else if (rollResult == 3) {
+        println("Unfortunately, you rolled a 3. Try again!")
+    } else if (rollResult == 5) {
+        println("Don't cry! You rolled a 5. Try again!")
     } else {
-        println("The variable is less than 4")
+        println("Apologies! You rolled a 6. Try again!")
     }
 }
