84e170c0c53216cc1723c26ade25cf49ed4b437d
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-22 19:56:03 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -68,5 +68,6 @@ import org.junit.Assert.assertTrue
 fun generates_number() {
    val dice = Dice(6)
    val rollResult = dice.roll()
+   assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
 }
 
