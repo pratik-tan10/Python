fd8e5b1532fc812e7b48e03db976c3b600fab630
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-22 19:58:42 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -80,3 +80,10 @@ override fun onCreate(savedInstanceState: Bundle?) {
     setContentView(R.layout.activity_main)
     logging()
 }
+fun logging() {
+    Log.e(TAG, "ERROR: a serious error like an app crash")
+    Log.w(TAG, "WARN: warns about the potential for serious errors")
+    Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+    Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+    Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+}
