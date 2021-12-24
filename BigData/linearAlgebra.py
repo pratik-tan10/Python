3fab745fd0692efa1d9fc373c393daf7c401fe3c
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-23 22:49:40 -0600

    Update linearAlgebra.py

--- BigData/linearAlgebra.py
+++ BigData/linearAlgebra.py
@@ -1,15 +1,23 @@
 import numpy as np
+#2d array
 M = np.array([[1, 2, 3],
  [4, 5, 6],
  [7, 8, 9]])
+
+#1d array
 v = np.array([[1],
  [2],
  [3]])
 
+#Shape of matrix or 2d array
 print M.shape
 
+#Shape of Vector or 1d array
 print v.shape
 
 v_single_dim = np.array([1, 2, 3])
 print v_single_dim.shape
 
+print v + v
+
+print 3*v
