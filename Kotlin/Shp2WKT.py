81517d5538bad508cbbceaef6af559f11e45c290
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-28 19:46:13 -0600

    Update Shp2WKT.py

--- Kotlin/Shp2WKT.py
+++ Kotlin/Shp2WKT.py
@@ -10,3 +10,8 @@ outfname = os.path.join(output_folder,cleanName)
 #If the file already exists, it is deleted
 if os.path.exists(outfname):
     os.remove(outfname)
+
+#writeMessage function takes a message string and write it to the log file
+def fwrite(message):
+    with open(outfname,'a') as of:
+        of.writelines(str(message)+'\n')
