e5b6a9ec4bf16f6843a39086ff7fd4571fc0a8f5
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-28 19:46:30 -0600

    Update Shp2WKT.py

--- Kotlin/Shp2WKT.py
+++ Kotlin/Shp2WKT.py
@@ -15,3 +15,27 @@ if os.path.exists(outfname):
 def fwrite(message):
     with open(outfname,'a') as of:
         of.writelines(str(message)+'\n')
+
+# Enter for loop for each feature
+for row in arcpy.da.SearchCursor(infc, ["OID@", "SHAPE@WKT"]):
+    # Write the current multipoint's ID
+    
+    fwrite("Feature {0}:".format(row[0]))
+    partnum = 0
+
+    # Step through each part of the feature
+    #
+    for part in row[1]:
+
+        # Step through each vertex in the feature
+        #
+        for pnt in part:
+            if pnt:
+                # Write x,y coordinates of current point
+                #
+                fwrite("{0}, {1}".format(pnt.X, pnt.Y))
+            else:
+                # If pnt is None, this represents an interior ring
+                #
+                fwrite("Interior Ring:")
+        partnum += 1
