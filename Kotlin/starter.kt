da0c528062557ef3406af6a4dcff38697ca819bc
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-29 21:02:48 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -1,89 +1,16 @@
+import kotlin.math.PI
+import kotlin.math.sqrt
 fun main() {
-    val name = "Rosh"
-    val amount = 500000
-    val borderLines = 3
-    var BorderChar = '+'
-    println("Congratulaiotns!")
-    println("${name}")
-    //start printing cake
-    printBorder(borderLines, borderChar)
-    println("   ,,,,,   ")
-    println("   |||||   ")
-    println(" =========")
-    println("@@@@@@@@@@@")
-    println("{~@~@~@~@~}")
-    println("@@@@@@@@@@@")
-    println("") //empty line print
-    println("You won the lottery of ${amount}!")
-}
-fun printBorder(n: Int, x: String){
-    println("--------Congratulations---------")
-    repeat(n){
-        repeat(23){
-        print(x)
-        }
-        println()
+	val squareCabin = SquareCabin(6, 50.0)
+	val roundHut = RoundHut(3, 10.0)
+	val roundTower = RoundTower(4, 15.5)
+    with (squareCabin){
+        println("\nSquare Cabin\n============")
+        println("Capacity: ${capacity}")
+        println("Material: ${squareCabin.buildingMaterial}")
+        println("Has room? ${hasRoom()}")
+        getRoom()
+        println("Floor area: %.2f".format(floorArea()))
+        println("Carpet size: %.2f".format(length))
     }
-}
-
-fun main() {
-    val myFirstDice = Dice(6)
-    println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}!")
-    
-    val mySecondDice = Dice(20)
-    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
-}
-
-class Dice (val numSides: Int) {
-
-    fun roll(): Int {
-        return (1..numSides).random()
-    }
-}
-#Conditional
-fun main() {
-    val myFirstDice = Dice(6)
-    val rollResult = myFirstDice.roll()
-    val luckyNumber = 4
-
-    when (rollResult) {
-        luckyNumber -> println("You won!")
-        1 -> println("So sorry! You rolled a 1. Try again!")
-        2 -> println("Sadly, you rolled a 2. Try again!")
-        3 -> println("Unfortunately, you rolled a 3. Try again!")
-        5 -> println("Don't cry! You rolled a 5. Try again!")
-        6 -> println("Apologies! You rolled a 6. Try again!")
-   }
-}
-
-class Dice(val numSides: Int) {
-    fun roll(): Int {
-        return (1..numSides).random()
-    }
-}
-
-#Unit test function is annotated with @Test keyword
-import org.junit.Assert.assertTrue
-@Test
-fun generates_number() {
-   val dice = Dice(6)
-   val rollResult = dice.roll()
-   assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
-}
-
-private const val TAG = "MainActivity"
-fun logging() {
-    Log.v(TAG, "Hello, world!")
-}
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    logging()
-}
-fun logging() {
-    Log.e(TAG, "ERROR: a serious error like an app crash")
-    Log.w(TAG, "WARN: warns about the potential for serious errors")
-    Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
-    Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
-    Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
 }
