87641e2b83589d847217a96cee2a9697bf6a5496
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-29 21:04:07 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -33,3 +33,54 @@ fun main() {
         println("Carpet size: ${calculateMaxCarpetSize()}")
     }
 }
+abstract class Dwelling( private var residents: Int){
+    abstract val buildingMaterial : String
+	abstract val capacity : Int
+    
+    fun hasRoom() : Boolean {
+        return residents < capacity
+    }
+    
+    abstract fun floorArea(): Double
+    
+    fun getRoom(){
+        if (capacity>residents){
+            residents++
+            println("You Get a room.")
+        } else {
+            println("At Capacity, Sorry you dont get a room.")
+        }
+    }
+}
+
+class SquareCabin(residents : Int, val length: Double) : Dwelling(residents){
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
+    
+    override fun floorArea(): Double{
+        return length * length
+    }
+}
+
+open class RoundHut(residents: Int, val radius: Double) : Dwelling(residents){
+    override val buildingMaterial = "Straw"
+    override val capacity = 4
+    
+    override fun floorArea(): Double{
+        return PI * radius * radius
+    }
+    
+    fun calculateMaxCarpetSize(): Double{
+        val diameter = 2 * radius
+        return sqrt(diameter * diameter/2)
+    }
+}
+
+class RoundTower(residents: Int, radius: Double, val floors: Int = 2) : RoundHut(residents, radius){
+    override val buildingMaterial = "Stone"
+    override val capacity = 4 * floors
+    
+    override fun floorArea(): Double {
+    	return super.floorArea() * floors
+    }
+}
