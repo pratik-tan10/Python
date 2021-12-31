ff21a72cd425ba4943c6dce5534ae65b1d633e74
Pratik Dhungana <pratik.tan10@gmail.com>
2021-12-30 22:03:32 -0600

    Update tipCalculator.kt

--- Kotlin/tipCalculator.kt
+++ Kotlin/tipCalculator.kt
@@ -20,26 +20,24 @@ class MainActivity : AppCompatActivity() {
     private fun calculateTip(){
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
-        
-        if (cost == null){
-            binding.tipResult.text = ""
+        fun displayTip(tip: Double){
+            val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+            binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+        }
+        if(cost == null) {
+            displayTip(0.0)
             return
         }
-
-        tipOptionSelected = binding.tipOption.checkedRadioButtonId
-        val tipPercentage = when(tipOptionSelected) {
+        val tipPercentage = when(binding.tipOption.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             R.id.option_fifteen_percent -> 0.15
             else -> 0.15
         }
         var tip = cost *  tipPercentage
-        val roundUp = binding.roundUpSwitch.isChecked
-        if (roundUp){
+        if (binding.roundUpSwitch.isChecked){
             tip = kotlin.math.ceil(tip)
         }
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-
+        displayTip(tip)
     }
 }
