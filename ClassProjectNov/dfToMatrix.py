079c39fec31eaf18285f382cf759f9de14f4fcb0
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-01 17:21:36 -0600

    Update dfToMatrix.py
    Refactoring, changed zero matrix with identity matrix

--- ClassProjectNov/dfToMatrix.py
+++ ClassProjectNov/dfToMatrix.py
@@ -2,13 +2,13 @@
 #Read Neighbour csv table
 import pandas as pd
 df = pd.read_csv(r"C:\Users\Research Lab\Desktop\Pratik\ClassProj\neighbour.csv")
-df
+
 #Get maximum row and colums in csv
-c = r = max(df['OID_'])
+r = max(df['OID_'])
 
 #Initialize a zero square matrix with size of no of polygons
 import numpy as np
-R = np.zeros((r,c))
+R = np.eye(r)
 
 #Get colums names
 df.columns
@@ -16,7 +16,6 @@ df.columns
 #Subset only required source and neighbour colums from database
 #This will be used to change neighbourhood information in matirx
 Ni = df[['src_OBJECTID_1', 'nbr_OBJECTID_1']]
-Ni
 
 #Iterate through each row and create a tuple of each neighbour location in matrix
 l=[]
@@ -25,8 +24,7 @@ for row in Ni.iterrows():
     x = ((row[1][0]), row[1][1])
     l.append(x)
     i=i+1
-len(l)
 
 #For each location tuple, update from zero to 1
-for each in l[:20]:
+for each in l:
     R[each[0],each[1]]=1
