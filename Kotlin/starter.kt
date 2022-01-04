6be92d39163b92579c28ad455c8db0fddcee23be
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-03 21:56:13 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -15,3 +15,44 @@ fun main() {
     order3.addAll(items)
     ordersList.add(order3)
 }
+open class Item(val name: String, val price: Int)
+
+class Noodles : Item("Noodles", 10) {
+    override fun toString(): String {
+        return name
+    }
+}
+
+class Vegetables(vararg val toppings: String) : Item("Vegetables", 5) {
+    override fun toString(): String {
+        if (toppings.isEmpty()) {
+            return "$name Chef's Choice"
+        } else {
+            return name + " " + toppings.joinToString()
+        }
+    }
+}
+
+class Order(val orderNumber: Int) {
+    private val itemList = mutableListOf<Item>()
+
+    fun addItem(newItem: Item): Order {
+        itemList.add(newItem)
+        return this
+    }
+
+    fun addAll(newItems: List<Item>): Order {
+        itemList.addAll(newItems)
+        return this
+    }
+
+    fun print() {
+        println("Order #${orderNumber}")
+        var total = 0
+        for (item in itemList) {
+            println("${item}: $${item.price}")
+            total += item.price
+        }
+        println("Total: $${total}")
+    }
+}
