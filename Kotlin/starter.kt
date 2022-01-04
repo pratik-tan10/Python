9240e3d9be06456e320b7b277edd651e84c64e30
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-03 21:54:53 -0600

    Update starter.kt

--- Kotlin/starter.kt
+++ Kotlin/starter.kt
@@ -1,86 +1,32 @@
-import kotlin.math.PI
-import kotlin.math.sqrt
 fun main() {
-	val squareCabin = SquareCabin(6, 50.0)
-	val roundHut = RoundHut(3, 10.0)
-	val roundTower = RoundTower(4, 15.5)
-    with (squareCabin){
-        println("\nSquare Cabin\n============")
-        println("Capacity: ${capacity}")
-        println("Material: ${squareCabin.buildingMaterial}")
-        println("Has room? ${hasRoom()}")
-        getRoom()
-        println("Floor area: %.2f".format(floorArea()))
-        println("Carpet size: %.2f".format(length))
-    }
-    with (roundHut){
-	println("\nSquare Cabin\n============")
-        println("Capacity: ${capacity}")
-        println("Material: ${squareCabin.buildingMaterial}")
-        println("Has room? ${hasRoom()}")
-        getRoom()
-        println("Floor area: %.2f".format(floorArea()))
-        println("Carpet size: ${calculateMaxCarpetSize()}")
-    }
+    val entrees = mutableListOf<String>()
+    println("Entrees: $entrees")
 
-    with (roundTower){
-        println("\nSquare Cabin\n============")
-        println("Capacity: ${capacity}")
-        println("Material: ${squareCabin.buildingMaterial}")
-        println("Has room? ${hasRoom()}")
-        getRoom()
-        println("Floor area: %.2f".format(floorArea()))
-        println("Carpet size: ${calculateMaxCarpetSize()}")
-    }
-}
-abstract class Dwelling( private var residents: Int){
-    abstract val buildingMaterial : String
-	abstract val capacity : Int
-    
-    fun hasRoom() : Boolean {
-        return residents < capacity
-    }
-    
-    abstract fun floorArea(): Double
-    
-    fun getRoom(){
-        if (capacity>residents){
-            residents++
-            println("You Get a room.")
-        } else {
-            println("At Capacity, Sorry you dont get a room.")
-        }
-    }
-}
+    // Add individual items using add()
+    println("Add noodles: ${entrees.add("noodles")}")
+    println("Entrees: $entrees")
+    println("Add spaghetti: ${entrees.add("spaghetti")}")
+    println("Entrees: $entrees")
 
-class SquareCabin(residents : Int, val length: Double) : Dwelling(residents){
-    override val buildingMaterial = "Wood"
-    override val capacity = 6
-    
-    override fun floorArea(): Double{
-        return length * length
-    }
-}
+    // Add a list of items using addAll()
+    val moreItems = listOf("ravioli", "lasagna", "fettuccine")
+    println("Add list: ${entrees.addAll(moreItems)}")
+    println("Entrees: $entrees")
 
-open class RoundHut(residents: Int, val radius: Double) : Dwelling(residents){
-    override val buildingMaterial = "Straw"
-    override val capacity = 4
-    
-    override fun floorArea(): Double{
-        return PI * radius * radius
-    }
-    
-    fun calculateMaxCarpetSize(): Double{
-        val diameter = 2 * radius
-        return sqrt(diameter * diameter/2)
-    }
-}
+    // Remove an item using remove()
+    println("Remove spaghetti: ${entrees.remove("spaghetti")}")
+    println("Entrees: $entrees")
+    println("Remove item that doesn't exist: ${entrees.remove("rice")}")
+    println("Entrees: $entrees")
 
-class RoundTower(residents: Int, radius: Double, val floors: Int = 2) : RoundHut(residents, radius){
-    override val buildingMaterial = "Stone"
-    override val capacity = 4 * floors
-    
-    override fun floorArea(): Double {
-    	return super.floorArea() * floors
-    }
+    // Remove an item using removeAt() with an index
+    println("Remove first element: ${entrees.removeAt(0)}")
+    println("Entrees: $entrees")
+
+    // Clear out the list
+    entrees.clear()
+    println("Entrees: $entrees")
+
+    // Check if the list is empty
+    println("Empty? ${entrees.isEmpty()}")
 }
