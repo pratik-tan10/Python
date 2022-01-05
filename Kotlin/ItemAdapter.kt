401f13afc1d3654899f866c530ac85ea28cafd06
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-04 20:15:04 -0600

    Update and rename starter.kt to ItemAdapter.kt

---
+++ Kotlin/ItemAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.affirmation.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmation.R
+import com.example.affirmation.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textview: TextView = view.findViewById(R.id.item_title)
+        val imageview: ImageView = view.findViewById(R.id.item_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textview.text = context.resources.getString(item.stringResourceId)
+        holder.imageview.setImageResource(item.imageResourceId)
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
