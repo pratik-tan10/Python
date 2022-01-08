7322c10afd46d466e2b5b9d03b14bf116d77164f
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-07 19:37:30 -0600

    Update listAndMap.kt

--- Kotlin/listAndMap.kt
+++ Kotlin/listAndMap.kt
@@ -24,5 +24,10 @@ val filteredNames = peopleAges.filter { it.key.length < 4 }
 println(filteredNames)
 peopleAges.forEach { print("${it.key} is ${it.value}") }
 
-
+val triple: (Int) -> Int = { a: Int -> a * 3 }
+    println(triple(5))
+peopleAges.filter { it.key.length < 4 }
+val peopleNames = listOf("Fred", "Ann", "Barbara", "Joe")
+println(peopleNames.sorted()
+println(peopleNames.sortedWith { str1: String, str2: String -> str1.length - str2.length })
 }
