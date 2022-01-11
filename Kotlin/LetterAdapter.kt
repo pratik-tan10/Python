e2676b68f53e37b2da091f5726738000fbe8a83c
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-10 23:14:49 -0600

    Update and rename listAndMap.kt to LetterAdapter.kt

---
+++ Kotlin/LetterAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.wordsapp
+
+import android.content.Intent
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+
+class LetterAdapter :
+    RecyclerView.Adapter<LetterAdapter.LetterViewHolder>() {
+
+    private val list = ('A').rangeTo('Z').toList()
+
+    class LetterViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val button = view.findViewById<Button>(R.id.button_item)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterViewHolder {
+        val layout = LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.item_view, parent, false)
+        // Setup custom accessibility delegate to set the text read
+        layout.accessibilityDelegate = Accessibility
+        return LetterViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
+        val item = list.get(position)
+        holder.button.text = item.toString()
+        holder.button.setOnClickListener {
+            val context = holder.view.context
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(DetailActivity.LETTER, holder.button.text.toString())
+            context.startActivity(intent)
+
+        }
+    }
+
+    companion object Accessibility : View.AccessibilityDelegate() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onInitializeAccessibilityNodeInfo(
+            host: View?,
+            info: AccessibilityNodeInfo?
+        ) {
+            super.onInitializeAccessibilityNodeInfo(host, info)
+            val customString = host?.context?.getString(R.string.look_up_words)
+            val customClick =
+                AccessibilityNodeInfo.AccessibilityAction(
+                    AccessibilityNodeInfo.ACTION_CLICK,
+                    customString
+                )
+            info?.addAction(customClick)
+        }
+    }
+}
