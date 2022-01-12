047f8b809b06b3981eb23ee4f246c6a116b38e7c
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-11 19:37:28 -0600

    Update and rename ItemAdapter.kt to starter.kt

---
+++ Kotlin/starter.kt
@@ -0,0 +1,9 @@
+fun main (){
+    val answerString = when {
+    count == 42 -> "I have the answer."
+    count > 35 -> "The answer is close."
+    else -> "The answer eludes me."
+}
+
+println(answerString)
+}
