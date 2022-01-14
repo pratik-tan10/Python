e3817337911a38ea317c16d8314a2440178a7a61
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-13 19:32:31 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -9,3 +9,22 @@ limits
 scored_policies<-merge(x=policies,y=limits,by="State",all.x=TRUE)
 scored_policies
 
+#A query
+which(scored_policies$Limit > scored_policies$regulatory_limit)
+#Natural Join or natural join
+df1 = data.frame(CountryId = c(1:6), Product = c("Netflix","Hulu","HBOMax","DisneyPlus","AppleTV","AmazonPrime"))
+df2 = data.frame(CountryId = c(2, 4, 6, 7, 8), State = c("India","Bangladesh","China","Pakistan","Bhutan")) 
+df = merge(x=df1,y=df2,by="CountryId")
+df
+#Inner Join using dplyr
+library(dplyr)
+df = df1%>%inner_join(df2, by="CountryId")
+#Outer Join
+df = merge(x=df1,y=df2,by="CountryId",all=TRUE)
+#Left outer Join
+df = merge(x=df1,y=df2,by="CountryId",all.x=TRUE)
+#Right outer join
+df = merge(x=df1,y=df2,by="CountryId",all.y=TRUE)
+
+
+
