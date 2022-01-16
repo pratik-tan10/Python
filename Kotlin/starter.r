edccf07be61cbb3a5716a85816e2e3983a63ebb4
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-15 23:10:58 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -81,5 +81,51 @@ set.seed(65)
 # using arguments 1 and 3
 runif(3,,4)
 
+?seq # starting with the seq function
 
+seq() # just using the default settings
 
+# simple sequence from 3 to 5
+seq(3, 5)
+
+seq(from = 3, to = 5)
+
+# using length
+seq(from = 3, length = 3)
+
+# step manipulations
+seq(from = 3, length = 3, by = 0.5)
+
+seq(from = 3, by = 0.5, length = 3) # changed order
+
+?paste # next function
+
+paste(1:4)
+
+class(paste(1:4)) # checking the class
+
+paste("xyz", 1:10)
+
+paste("xyz", c(2,5,7,"test", 4.5)) # random vector with numbers and characters
+
+paste("xyz", 1:10, sep = "") # modifying the seperator
+
+?rep # next function
+
+rep(c(3,4,5), 3)
+
+rep(1:10, times = 3)
+
+x = c(1,2,3) # creating x
+
+rep(x, each = 3) # using each
+
+rep(x, each = 3, times = 3) # combining arguments
+
+## Working with index positions
+
+x = 4:20 # our data
+
+which(x == 10) # double equal sign, logical operation
+
+x[3]
