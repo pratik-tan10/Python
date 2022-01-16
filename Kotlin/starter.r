208d6ba70a831ba5a3e7792b4472a18d11c972f0
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-15 23:10:15 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -50,5 +50,36 @@ montes <-
   regions %>%
   summarise(area = sum(MPIO_NAREA))
 
+# setting a seed to make a random sample reproducible, choose any number you want
+set.seed(65)
+
+# with argument name, exact argument order
+runif(n = 9, min = 3, max = 6)
+
+set.seed(65)
+
+# without argument name, exact argument order
+runif(9, 3, 6)
+
+set.seed(65)
+
+# with argument name, mixed argument order
+runif(min = 3, max = 6, n = 9)
+
+set.seed(65)
+
+# without argument name, mixed argument order
+runif(3, 6, 9) # this means n=3, max=9
+
+set.seed(65)
+
+# using only the first argument
+runif(3)
+
+set.seed(65)
+
+# using arguments 1 and 3
+runif(3,,4)
+
 
 
