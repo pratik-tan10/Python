60a43cff2713249d0e6a8ac46142cbad7a6f4fdd
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-16 23:25:18 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -4,3 +4,18 @@ fruit <- c('Apple', 'Orange', 'Passion fruit', 'Banana')
 for ( i in fruit){ 
  print(i)
 }
+# Create an empty list
+list <- c()
+# Create a for statement to populate the list
+for (i in seq(1, 4, by=1)) {
+  list[[i]] <- i*i
+}
+print(list)
+
+# Create a list with three vectors
+fruit <- list(Basket = c('Apple', 'Orange', 'Passion fruit', 'Banana'), 
+Money = c(10, 12, 15), purchase = FALSE)
+for (p  in fruit) 
+{ 
+	print(p)
+}
