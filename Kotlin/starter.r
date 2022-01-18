7498503a73c1c1d6a4f998d304fe100e06552d4e
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-17 23:10:38 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -1,7 +1,11 @@
 # Create two vectors of different lengths.
-vector1 <- c(5,9,3)
-vector2 <- c(10,11,12,13,14,15)
+v1 <- c(5,9,3)
+v2 <- c(10,11,12,13,14,15)
+column.names <- c("COL1","COL2","COL3")
+row.names <- c("ROW1","ROW2","ROW3")
+matrix.names <- c("Matrix1","Matrix2")
 
 # Take these vectors as input to the array.
-result <- array(c(vector1,vector2),dim = c(3,3,2))
+result <- array(c(v1,v2),dim = c(3,3,2),dimnames = list(row.names,column.names,
+   matrix.names))
 print(result)
