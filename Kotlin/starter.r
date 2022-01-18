d311bf17f59b7697c811afa4bbaf6202d2924254
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-17 23:09:54 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -1,21 +1,7 @@
-# Create fruit vector
-fruit <- c('Apple', 'Orange', 'Passion fruit', 'Banana')
-# Create the for statement
-for ( i in fruit){ 
- print(i)
-}
-# Create an empty list
-list <- c()
-# Create a for statement to populate the list
-for (i in seq(1, 4, by=1)) {
-  list[[i]] <- i*i
-}
-print(list)
+# Create two vectors of different lengths.
+vector1 <- c(5,9,3)
+vector2 <- c(10,11,12,13,14,15)
 
-# Create a list with three vectors
-fruit <- list(Basket = c('Apple', 'Orange', 'Passion fruit', 'Banana'), 
-Money = c(10, 12, 15), purchase = FALSE)
-for (p  in fruit) 
-{ 
-	print(p)
-}
+# Take these vectors as input to the array.
+result <- array(c(vector1,vector2),dim = c(3,3,2))
+print(result)
