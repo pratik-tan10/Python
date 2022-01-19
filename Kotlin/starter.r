00ae657058cc01016f58834209dda497696f92dd
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-18 22:20:34 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -20,3 +20,31 @@ class(my_data)
 cnames <- c("patient", "age", "weight", "bp", "rating", "test")
 colnames(my_data) <- cnames
 my_data
+#Logic
+TRUE == TRUE
+(FALSE == TRUE) == FALSE
+6==7
+6<7
+10<=10
+5 != 7
+!(5==7)
+
+FALSE & FALSE
+TRUE & c(TRUE, FALSE, FALSE)
+TRUE && c(TRUE, FALSE, FALSE)
+
+TRUE || c(TRUE, FALSE, FALSE)
+
+5>8||6!=8&&4>3.9
+
+isTRUE(6>4)
+identical('twins','twins')
+
+xor(5==6, !FALSE)
+ints <- sample(10)
+ints
+ints>5
+which(c(TRUE, FALSE, TRUE))
+which(ints>7)
+any(ints<0)
+all(ints>0)
