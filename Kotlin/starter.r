227f328cbf47aededf1eca4af43dfb28dfe77a4e
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-19 15:52:39 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -1,50 +1,44 @@
-#matrices and data frames
-my_vector <- 1:20
-my_vector
-dim(my_vector)
-length(my_vector)
-dim(my_vector) <- c(4,5)
-dim(my_vector)
-attributes(my_vector)
-my_vector
-class(my_vector)
-my_matrix <- my_vector
-?matrix
-my_matrix2 <- matrix(1:20,4,5)
-identical(my_matrix, my_matrix2)
-patients <- c("Bill", "Gina","Kelly","Sean")
-cbind(patients, my_matrix)
-my_data <-data.frame(patients, my_matrix)
-my_data
-class(my_data)
-cnames <- c("patient", "age", "weight", "bp", "rating", "test")
-colnames(my_data) <- cnames
-my_data
-#Logic
-TRUE == TRUE
-(FALSE == TRUE) == FALSE
-6==7
-6<7
-10<=10
-5 != 7
-!(5==7)
+Sys.Date()
+mean(c(2,4,5))
+submit()
+boring_function("My first function!")
+boring_function
+my_mean(c(4,5,10))
+remainder(5)
+remainder(11,5)
+remainder(divisor = 11, num = 5)
+remainder(4, div=2)
+args(remainder)
+evaluate(sd, c(1.4,3.6,7.9,8.8))
+evaluate(function(x){x+1},6)
+evaluate(function(x){x[length(x)]},c(8,4,0))
+paste("Programming", "is","fun!")
 
-FALSE & FALSE
-TRUE & c(TRUE, FALSE, FALSE)
-TRUE && c(TRUE, FALSE, FALSE)
 
-TRUE || c(TRUE, FALSE, FALSE)
-
-5>8||6!=8&&4>3.9
-
-isTRUE(6>4)
-identical('twins','twins')
-
-xor(5==6, !FALSE)
-ints <- sample(10)
-ints
-ints>5
-which(c(TRUE, FALSE, TRUE))
-which(ints>7)
-any(ints<0)
-all(ints>0)
+head(flags)
+dim(flags)
+viewinfo()
+class(flags)
+cls_list <- lapply(flags, class)
+cls_list
+class(cls_list)
+as.character(cls_list)
+cls_vect<-sapply(flags, class)
+class(cls_vect)
+sum(flags$orange)
+flag_colors<-flags[,11:17]
+head(flag_colors)
+lapply(flag_colors, sum)
+sapply(flag_colors, sum)
+sapply(flag_colors, mean)
+flag_shapes <-flags[,19:23]
+lapply(flag_shapes, range)
+sapply(flag_shapes, range) ->shape_mat
+shape_mat
+class(shape_mat)
+unique(c(3,4,5,5,5,6,6))
+unique_vals<-lapply(flags, unique)
+unique_vals
+sapply(unique_vals,length)
+sapply(flags, unique)
+lapply(unique_vals, function(elem)elem[2])
