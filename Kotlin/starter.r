33d1f7284002f1b517f75482abc235838ca88738
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-20 17:32:22 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -1,86 +1,26 @@
-Sys.Date()
-mean(c(2,4,5))
-submit()
-boring_function("My first function!")
-boring_function
-my_mean(c(4,5,10))
-remainder(5)
-remainder(11,5)
-remainder(divisor = 11, num = 5)
-remainder(4, div=2)
-args(remainder)
-evaluate(sd, c(1.4,3.6,7.9,8.8))
-evaluate(function(x){x+1},6)
-evaluate(function(x){x[length(x)]},c(8,4,0))
-paste("Programming", "is","fun!")
-
-
-head(flags)
-dim(flags)
-viewinfo()
-class(flags)
-cls_list <- lapply(flags, class)
-cls_list
-class(cls_list)
-as.character(cls_list)
-cls_vect<-sapply(flags, class)
-class(cls_vect)
-sum(flags$orange)
-flag_colors<-flags[,11:17]
-head(flag_colors)
-lapply(flag_colors, sum)
-sapply(flag_colors, sum)
-sapply(flag_colors, mean)
-flag_shapes <-flags[,19:23]
-lapply(flag_shapes, range)
-sapply(flag_shapes, range) ->shape_mat
-shape_mat
-class(shape_mat)
-unique(c(3,4,5,5,5,6,6))
-unique_vals<-lapply(flags, unique)
-unique_vals
-sapply(unique_vals,length)
-sapply(flags, unique)
-lapply(unique_vals, function(elem)elem[2])
-
-
-###########################
-vapply(flags, unique, numeric(1))
-sapply(flags, class)
-vapply(flags, class, character(1))
-?tapply
-sapply(flags, class)
-table(flags$animate)
-tapply(flags$animate, flags$landmass,mean)
-tapply(flags$population,flags$red,summary)
-
-###########################
-a <-ls();a
-class(plants)
-dim(plants)
-ncol(plants);nrow(plants)
-ncol(plants)
-object.size(plants)/1024^2
-names(plants)
-head(plants)
-head(plants,10)
-tail(plants, 15)
-summary(plants)
-table(plants$Active_Growth_Period)
-str(plants)
-
-###########################
-sample(1:20,10)
-LETTERS
-sample(LETTERS)
-sample(c(0,1),100,replace = TRUE, prob = c(0.3,0.7)) ->flips
-flips
-mean(flips)
-flips2 <-rbinom(100,size = 1, prob = 0.7)
-flips2
-sum(flips2)
-rnorm(10, mean = 100, sd =25)
-rpois(5,10)
-replicate(100, rpois(5,10)) -> my_pois
-cm <- colMeans(my_pois)
-hist(cm)
+d1 <- Sys.Date()
+class(d1)
+unclass(d1)
+d1
+d2 <- as.Date("1969-01-01")
+unclass(d2)
+t1 <- Sys.time()
+t1
+class(t1)
+unclass(t1)
+t2 <-as.POSIXlt(Sys.time())
+class(t2)
+unclass(t2)
+t2
+str(unclass(t2))
+t2$min
+weekdays(d1)
+months(t1)
+quarters(t2)
+t3 <- "October 17, 1986 08:24"
+strptime(t3, "%B %d, %Y %H:%M") ->t4
+t4
+class(t4)
+Sys.time() > t1
+Sys.time()-t1
+difftime(Sys.time(), t1, units = 'days')
