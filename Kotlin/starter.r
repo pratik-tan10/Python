2ad216f4d5e0513de3cf3589ce8520a92fe9019d
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-22 19:47:18 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -8,3 +8,27 @@ ggplot(data = mpg, mapping = aes(x=displ, y=hwy, color = drv))+
 ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
   geom_point(color = 'white', size=5)+
   geom_point(mapping=aes(color=drv))
+
+dummy <- tribble(
+  ~name,        ~amount,
+  "Rice",       5100,
+  "Corn",       1623,
+  "Soya",       1178,
+  "Bean",       18453,
+  "Wheat",      8181
+)
+#Changing default stat variable
+ggplot(data = dummy) +
+  geom_bar(mapping = aes(x = name, y = amount), stat = "identity")
+
+#plotting summary of individual data subclass
+ggplot(data = diamonds, mapping = aes(x = cut, y = depth)) + 
+  stat_summary(
+    fun.min = min,
+    fun.max = max,
+    fun = median, color = 'red')+
+  stat_summary(fun=mean,color='green')
+
+#colorful bar chart
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = clarity))
