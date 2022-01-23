accdd42b683a5e6db6ca5e2e99af1b41d25d3904
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-22 19:20:51 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -4,4 +4,7 @@ summary(mpg)
 head(mpg)
 ggplot(data = mpg, mapping = aes(x=displ, y=hwy, color = drv))+
   geom_point(mapping = aes(shape = drv, alpha = (1-displ) ))+
-  geom_smooth(mapping = aes(linetype=drv),se=FALSE) 
+  geom_smooth(mapping = aes(linetype=drv),se=FALSE)
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(color = 'white', size=5)+
+  geom_point(mapping=aes(color=drv))
