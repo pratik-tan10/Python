2841eebc0769be4ec696a61d8119c3ddf3077361
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-24 20:58:13 -0600

    Update Qgis.py

--- Esri/SpatialDataScience/Qgis.py
+++ Esri/SpatialDataScience/Qgis.py
@@ -14,3 +14,23 @@ bufferProvider = bufferLayer.dataProvider()
  
 centroidFeatures = []
 bufferFeatures = []
+
+areaPolygon = qgis.core.QgsGeometry.fromWkt('POLYGON ( (6.3 -14, 52 -14, 52 -40, 6.3 -40, 6.3 -14) )')
+for feature in layer.getFeatures(): 
+    if feature.geometry().intersects(areaPolygon): 
+        centroid = qgis.core.QgsFeature() 
+        centroid.setAttributes([feature['NAME']]) 
+        centroid.setGeometry(feature.geometry().centroid()) 
+        centroidFeatures.append(centroid) 
+ 
+        buffer = qgis.core.QgsFeature() 
+        buffer.setAttributes([feature['NAME']]) 
+        buffer.setGeometry(feature.geometry().centroid().buffer(2.0,100)) 
+        bufferFeatures.append(buffer)
+     
+centroidProvider.addFeatures(centroidFeatures) 
+bufferProvider.addFeatures(bufferFeatures)
+qgis.core.QgsVectorFileWriter.writeAsVectorFormat(centroidLayer, r'C:\489\centroids.gpkg', "utf-8", layer.crs(), "GPKG") 
+qgis.core.QgsVectorFileWriter.writeAsVectorFormat(bufferLayer, r'C:\489\buffers.gpkg', "utf-8", layer.crs(), "GPKG")
+qgs.exitQgis()
+
