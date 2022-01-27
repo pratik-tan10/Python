b805c8c7465835ba77129d827aa5fc93aa7897e1
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-26 21:55:41 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -1,63 +1,12 @@
-#plotting with ggplot2
-library(tidyverse)
-summary(mpg)
-head(mpg)
-ggplot(data = mpg, mapping = aes(x=displ, y=hwy, color = drv))+
-  geom_point(mapping = aes(shape = drv, alpha = (1-displ) ))+
-  geom_smooth(mapping = aes(linetype=drv),se=FALSE)
-ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
-  geom_point(color = 'white', size=5)+
-  geom_point(mapping=aes(color=drv))
-
-dummy <- tribble(
-  ~name,        ~amount,
-  "Rice",       5100,
-  "Corn",       1623,
-  "Soya",       1178,
-  "Bean",       18453,
-  "Wheat",      8181
+boxplot(sample(3:14, sample(1:9,1), replace=TRUE),sample(1:8, sample(1:9,1), replace=TRUE), sample(3:100, sample(1:24,1), replace=TRUE), sample(3:100, sample(1:24,1), replace=TRUE),
+main = "Multiple boxplots for comparision",
+at = c(1,2,4,5),
+names = c("L with c", "L without c", "S with c", "S without c"),
+las =1 ,
+col = c("red","yellow","green","lightblue"),
+border = "brown",
+horizontal = FALSE,
+notch = FALSE,
+xlab = 'Group',
+ylab = 'Count'
 )
-#Changing default stat variable
-ggplot(data = dummy) +
-  geom_bar(mapping = aes(x = name, y = amount), stat = "identity")
-
-#plotting summary of individual data subclass
-ggplot(data = diamonds, mapping = aes(x = cut, y = depth)) + 
-  stat_summary(
-    fun.min = min,
-    fun.max = max,
-    fun = median, color = 'red')+
-  stat_summary(fun=mean,color='green')
-
-#colorful bar chart
-ggplot(data = diamonds) + 
-  geom_bar(mapping = aes(x = cut, fill = clarity))
-
-ggplot(data = mpg, mapping = aes(x = class, y = hwy)) + 
-  geom_boxplot() +
-  coord_flip()
-
-ggplot(data = mpg) + 
-  geom_jitter(mapping = aes(x = displ, y = hwy))
-
-nz <- map_data("nz")
-ggplot(nz, aes(long, lat, group = group)) +
-  geom_polygon(fill = "white", colour = "black") +
-  coord_quickmap()
-
-bar <- ggplot(data = diamonds) + 
-  geom_bar(
-    mapping = aes(x = cut, fill = cut), 
-    show.legend = FALSE,
-    width = 1
-  ) + 
-  theme(aspect.ratio = 1) +
-  labs(x = NULL, y = NULL)
-
-bar + coord_flip()
-bar + coord_polar()
-
-ggplot(data = mpg, mapping = aes(x = cty, y = hwy)) +
-  geom_point() + 
-  geom_abline() +
-  coord_fixed()
