7b3c1e82a1de7234fff0c4709bb652dc86c39360
Pratik Dhungana <pratik.tan10@gmail.com>
2022-01-30 20:19:49 -0600

    Update starter.r

--- Kotlin/starter.r
+++ Kotlin/starter.r
@@ -1,12 +1,18 @@
-boxplot(sample(3:14, sample(1:9,1), replace=TRUE),sample(1:8, sample(1:9,1), replace=TRUE), sample(3:100, sample(1:24,1), replace=TRUE), sample(3:100, sample(1:24,1), replace=TRUE),
-main = "Multiple boxplots for comparision",
-at = c(1,2,4,5),
-names = c("L with c", "L without c", "S with c", "S without c"),
-las =1 ,
-col = c("red","yellow","green","lightblue"),
-border = "brown",
-horizontal = FALSE,
-notch = FALSE,
-xlab = 'Group',
-ylab = 'Count'
-)
+rep(c(1,3,6),c(6,6,6))
+rep(c(1,2,3,4,5),5:1)
+rep(c(1,3,6),c(3,1,6))
+#------------------------
+attach(Orange)
+r <- sum((age-mean(age))*(circumference - mean(circumference)))/sqrt(sum((age - mean(age))**2)*sum((circumference - mean(circumference))**2))
+r
+cor(age,circumference)
+
+#----------------------------------------------------
+s <- c( 4.5, 7.3, 0.017, 12, 15, 8.4, 8.9)
+sum(sqrt(abs(s-mean(s))))
+max((s - median(s))**2)
+
+abs(round(sqrt(s),2)**2-s)
+#---------------------------------------------------------
+1.4826*median(abs(chickwts$weight-median(chickwts$weight)))
+mad(chickwts$weight)
