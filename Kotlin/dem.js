0930b4055c8af99ae89e1c1cb3929d9eb8fba575
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-03 22:42:01 -0600

    Update dem.js

--- Kotlin/dem.js
+++ Kotlin/dem.js
@@ -10,3 +10,13 @@ Map.addLayer(mosaic, imageVisParam, 'spatial mosaic');
 var projection = mosaic.projection().getInfo();
 print(projection)
 
+// Export the image, specifying the CRS, transform, and region.
+Export.image.toDrive({
+  image: mosaic,
+  description: 'ALOS30Alabama',
+  crs: projection.crs,
+  crsTransform: projection.transform,
+  region: Alabama,
+  scale: 30,
+  maxPixels: 1e13
+});
