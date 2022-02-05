6e4ebeca731f85b553e0327127a69c07d738b7e6
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-04 22:38:41 -0600

    Create scratch.js

---
+++ Kotlin/scratch.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Objects</h2>
+
+<p id="demo"></p>
+
+<script>
+// Create an object:
+const Car = {
+  owner: "Vicky Bahl",
+  model: "Tesla tx",
+  milage: 24,
+  color: "brown",
+  
+};
+
+// Display some data from the object:
+document.getElementById("demo").innerHTML =
+car.owner + " has " + car.model + " of " + car.color + " color that gives " + car.milage + "milage.";
+</script>
+
+</body>
+</html>
