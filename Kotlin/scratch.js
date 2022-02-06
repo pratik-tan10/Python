c623e06e3a7a74037abb5259b8cca54e27f02caf
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-05 18:36:21 -0600

    Update scratch.js

--- Kotlin/scratch.js
+++ Kotlin/scratch.js
@@ -2,8 +2,16 @@
 <html>
 <body>
 
-<h2>JavaScript HTML Events</h2>
-<button onclick="this.innerHTML=Date()">The time is?</button>
+<h2>JavaScript setDate()</h2>
+<p>The setDate() method sets the day of a date object:</p>
+
+<p id="demo"></p>
+
+<script>
+const d = new Date();
+d.setDate(15);
+document.getElementById("demo").innerHTML = d;
+</script>
 
 </body>
 </html>
