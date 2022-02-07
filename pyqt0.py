0a2c69125dbc66770048c91720cf7b491577e706
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-06 22:03:45 -0600

    Update pyqt0.py

--- pyqt0.py
+++ pyqt0.py
@@ -1,11 +1,20 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QDialog
- 
-app = QApplication(sys.argv)
- 
-window = QDialog()
-window.setGeometry(500, 300, 300, 200)
-window.setWindowTitle('GUI Window')
-window.show()
- 
-sys.exit(app.exec_())
+import arcpy,os
+
+InFolder = arcpy.GetParameterAsText(0)
+Dest=arcpy.GetParameterAsText(1)
+
+arcpy.env.workspace=InFolder
+#The raster datasets in the input workspace
+in_raster_datasets = arcpy.ListRasters()
+
+arcpy.CreateFeatureclass_management(os.path.dirname(Dest),
+                                   os.path.basename(Dest),
+                                   "POLYGON")
+arcpy.AddField_management(Dest,"RasterName", "String","","",250)
+arcpy.AddField_management(Dest,"RasterPath", "String","","",250)
+
+cursor = arcpy.InsertCursor(Dest)
+point = arcpy.Point()
+array = arcpy.Array()
+corners = ["lowerLeft", "lowerRight", "upperRight", "upperLeft"]
+  
