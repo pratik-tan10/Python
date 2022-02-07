086063aea62931c5281706c5603dc6a8e1094bf6
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-06 22:04:12 -0600

    Update pyqt0.py

--- pyqt0.py
+++ pyqt0.py
@@ -18,3 +18,19 @@ point = arcpy.Point()
 array = arcpy.Array()
 corners = ["lowerLeft", "lowerRight", "upperRight", "upperLeft"]
   
+for Ras in in_raster_datasets:
+    feat = cursor.newRow()  
+    r = arcpy.Raster(Ras)
+    for corner in corners:    
+        point.X = getattr(r.extent, "%s" % corner).X
+        point.Y = getattr(r.extent, "%s" % corner).Y
+        array.add(point)
+    array.add(array.getObject(0))
+    polygon = arcpy.Polygon(array)
+    feat.shape = polygon
+    feat.setValue("RasterName", Ras)
+    feat.setValue("RasterPath", InFolder + "\\" + Ras)
+    cursor.insertRow(feat)
+    array.removeAll()
+del feat
+del cursor
