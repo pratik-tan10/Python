01eeb7784c781ff35f717617a73e3d88242761a2
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-07 21:31:36 -0600

    Update scratch.js

--- Kotlin/scratch.js
+++ Kotlin/scratch.js
@@ -2,15 +2,16 @@
 <html>
 <body>
 
-<h2>JavaScript setDate()</h2>
-<p>The setDate() method sets the day of a date object:</p>
+<h2>JavaScript String Methods</h2>
+
+<p>Search a string for "W3Schools", and display the position of the match:</p>
 
 <p id="demo"></p>
 
 <script>
-const d = new Date();
-d.setDate(15);
-document.getElementById("demo").innerHTML = d;
+let text = "Visit W3Schools!"; 
+let n = text.search("W3Schools");
+document.getElementById("demo").innerHTML = n;
 </script>
 
 </body>
