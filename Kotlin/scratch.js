f6372c74d5cb6179b2a66bc40644cb880bfe2df3
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-11 20:37:28 -0600

    Update scratch.js

--- Kotlin/scratch.js
+++ Kotlin/scratch.js
@@ -1,23 +1,29 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h2>JavaScript Sets</h2>
-<p>forEach() calls a function for each element:</p>
-
-<p id="demo"></p>
-
-<script>
-// Create a Set
-const letters = new Set(["a","b","c"]);
-
-// List all Elements
-let text = "";
-letters.forEach (function(value) {
-  text += value + "<br>";
-})
-
-document.getElementById("demo").innerHTML = text;
-</script>
-
-</body>
-</html>
+require(["esri/Map", "esri/views/MapView", "esri/geometry/Point","esri/symbols/SimpleMarkerSymbol","esri/Graphic"], (Map, MapView, Point,SimpleMarkerSymbol,Graphic) => {
+    const map = new Map({
+        basemap: "topo-vector"
+    });
+	
+    const view = new MapView({
+        container: "viewDiv", // Reference to the view div created in step 5
+        map: map, // Reference to the map object created before the view
+        zoom: 10, // Sets zoom level based on level of detail (LOD)
+        center: [84, 27] // Sets center point of view using longitude,latitude
+    });
+    
+	var pt = new Point({
+	  latitude: 27,
+	  longitude: 84
+	});
+	
+	var sym = new SimpleMarkerSymbol({
+	  color: "red",
+	  style: "square",
+	  size: 12
+	});
+	
+	var ptGraphic = new Graphic({
+	  geometry:pt,
+	  symbol:sym
+	});
+	view.graphics.add(ptGraphic);
+});
