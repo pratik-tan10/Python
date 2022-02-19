461b15bd28b2985ed2ff9c4e3b05c80e92c1afee
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-19 10:54:08 -0600

    Update and rename scratch.rmd to scratch.r

---
+++ scratch.r
@@ -0,0 +1,6 @@
+if (!file.exists("Downloads")){
+  dir.create("Downloads")
+}
+dUrl <- "https://www2.census.gov/geo/tiger/TIGER2017/EDGES/tl_2017_01125_edges.zip"
+download.file(dUrl, dfile = "tiger17.zip",method="curl")
+dateDownloaded <- date()
