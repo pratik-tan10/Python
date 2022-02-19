1236fb32abfa61ea2fff6ae5f60bdf2dd64cdeb2
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-19 10:56:16 -0600

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -4,3 +4,5 @@ if (!file.exists("Downloads")){
 dUrl <- "https://www2.census.gov/geo/tiger/TIGER2017/EDGES/tl_2017_01125_edges.zip"
 download.file(dUrl, dfile = "tiger17.zip",method="curl")
 dateDownloaded <- date()
+tTable <- read.table("./data/census.csv", sep =",", header =TRUE)
+head(tTable)
