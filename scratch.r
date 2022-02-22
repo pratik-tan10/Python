7c08c879e4bef85b37e65288ce99c6139f335fbe
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-21 22:04:07 -0600

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -17,3 +17,17 @@ filter(cran, r_version=="3.1.1", country=="US")
 filter(cran, r_version<="3.0.2", country=="IN")
 filter(cran, country == "IN" | country=="US")
 filter(cran, size>100500, r_os == "linux-gnu")
+
+
+filter(cran, !is.na(r_version))
+select(cran,size:ip_id) ->cran2
+arrange(cran2, ip_id)
+arrange(cran2, desc(ip_id))
+arrange(cran2, package, ip_id)
+arrange(cran2, country, desc(r_version), ip_id)
+
+select(cran, ip_id, package, size) -> cran3
+cran3
+mutate(cran3, size_mb = size/2^20, size_gb = size_mb/2^10)
+mutate(cran3, correct_size= size+1000)
+summarize(cran, avg_bytes = mean(size))
