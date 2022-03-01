2f63594ad61edbef7adf4749db7b5362a8efeb3f
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-28 20:10:18 -0600

    Update scratch.js

--- Kotlin/scratch.js
+++ Kotlin/scratch.js
@@ -1,61 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-<style>
-.places {
-  margin: 0;
-  padding: 5px;
-  background-color: lightgray;
-}
-
-.placess > h1, .place {
-  margin: 10px;
-  padding: 5px;
-}
-
-.place {
-  background: white;
-}
-
-.place > h2, p {
-  margin: 4px;
-  font-size: 90%;
-}
-
-footer {
-  text-align: center;
-  padding: 3px;
-  background-color: DarkSalmon;
-  color: white;
-}
-</style>
-</head>
 <body>
 
-<h1>The article and footer elements + CSS</h1>
+<h2>Create Object from JSON String</h2>
 
-<article class="places">
-  <h1>Places to visit</h1>
-  <article class="place">
-    <h2>Hukahm Dehi</h2>
-    <p>Hukahm Dehi is the mountain inhabited by tribes of Endura, who are proficient with harnessing the power of lightening.</p>
-  </article>
-  <article class="place">
-    <h2>Mubakaabaad</h2>
-    <p>Mubakabad is inhabited by fierce ice dragons known as Agasthrumum who are natural enemies of fire breating Agnisherum of Hemiodaar. </p>
-  </article>
-  <article class="place">
-    <h2>Hemiodaar</h2>
-    <p>Hemiodaar is the current nations of Agnisherum after the destruction of Yagnashina from the war with Agasthrumus. </p>
-  </article>
-</article>
+<p id="demo"></p>
 
-<footer>
-  <p>Author: Agabbaram Krichkoff<br>
-  <a href="mailto:akri@example.com">someone@example.com</a>
-  </p>
-  <p>Solmorone, Magnolia</p>
-</footer>
+<script>
+let text = '{"employees":[' +
+'{"firstName":"Aaron","lastName":"Stone" },' +
+'{"firstName":"Lancaster","lastName":"Brimmer" },' +
+'{"firstName":"Michael","lastName":"Morningstar" }]}';
+
+const obj = JSON.parse(text);
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+</script>
 
 </body>
 </html>
