b5166d66a35525efd3be63d42c7c8baf70dcab54
Pratik Dhungana <pratik.tan10@gmail.com>
2022-02-28 20:08:03 -0600

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,8 +1,11 @@
-def printf(lst):
-    for each in lst:
-        print(each)
-gdbName = "myGDB.gdb"
-arcpy.env.workspace=gdbName
-polygons = arcpy.ListFeatureClasses(gdbName, "POLYGON")
-printf(polygons)
-print(len(polygons))
+Str1= "food"
+Str2 = "dorm"
+count1,count2 = 0,0
+for letter in Str1:
+	if letter in Str2:
+		count1+=1
+for letter in Str2:
+	if letter in Str1:
+		count2+=1
+Count = count1+count2
+print(f"{Str1}:{count1},{Str2}:{count2},Total = {Count}")
