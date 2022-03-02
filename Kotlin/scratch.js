405ef4f30b57765703adeab2f33b153fa964c25f
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-01 20:41:07 -0600

    Update scratch.js

--- Kotlin/scratch.js
+++ Kotlin/scratch.js
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>Create Object from JSON String</h2>
-
-<p id="demo"></p>
-
-<script>
-let text = '{"employees":[' +
-'{"firstName":"Aaron","lastName":"Stone" },' +
-'{"firstName":"Lancaster","lastName":"Brimmer" },' +
-'{"firstName":"Michael","lastName":"Morningstar" }]}';
-
-const obj = JSON.parse(text);
-document.getElementById("demo").innerHTML =
-obj.employees[1].firstName + " " + obj.employees[1].lastName;
-</script>
-
-</body>
-</html>
+var dict = {"R1":"1981 to 1991",
+            "R2" : "1991 to 2001",
+            "R3" : "2001 to 2011",
+            "Ra" : "1981 to 2011"
+}
+var a = dict[$feature.WhenTime]
+Concatenate("Rate of population change of ",$feature.DISTRICT, " from ",a," was ",$feature.V2D," per year per 1000 thousand.")
