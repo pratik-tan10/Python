5a3838c7b30311a5caf50eb930614bbde3eb6a73
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-01 20:44:43 -0600

    Update scratch.js

--- Kotlin/scratch.js
+++ Kotlin/scratch.js
@@ -3,5 +3,8 @@ var dict = {"R1":"1981 to 1991",
             "R3" : "2001 to 2011",
             "Ra" : "1981 to 2011"
 }
-var a = dict[$feature.WhenTime]
-Concatenate("Rate of population change of ",$feature.DISTRICT, " from ",a," was ",$feature.V2D," per year per 1000 thousand.")
+var a = dict[$feature.WhenTime];
+var b = $feature.V2D.toFixed(3);
+Concatenate("Rate of population change of ",$feature.DISTRICT, " from ",a," was ",b," per year per 1000 thousand.")
+Concatenate("Year : ",$feature.Years, " Population : ",$feature.Po2D)
+Concatenate("Year : ",$feature.Years, " Population Density : ",$feature.PopDensity, " person per square Kilometers")
