98650ccd75972fc9f3f4e6464751b2b930e879b5
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-04 13:28:56 -0600

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -11,3 +11,10 @@ d <- read.fwf(url, w, header = FALSE, skip = 4, col.names = colNames)
 d <- d[, grep("^[^filler]", names(d))]
 sum(d[, 4])
 ?read.fwf
+connection <- url("https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for")
+lines <- readLines(connection, n = 10)
+w <- c(1, 9, 5, 4, 1, 3, 5, 4, 1, 3, 5, 4, 1, 3, 5, 4, 1, 3)
+colNames <- c("filler", "week", "filler", "sstNino12", "filler", "sstaNino12", "filler", "sstNino3", "filler", "sstaNino3", "filler", "sstNino34", "filler", "sstaNino34", "filler", "sstNino4", "filler", "sstaNino4")
+d <- read.fwf(url, w, header = FALSE, skip = 4, col.names = colNames)
+d <- d[, grep("^[^filler]", names(d))]
+sum(d[, 4])
