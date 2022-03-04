94ea8af6924cb77bc9d5a266c74ccf4b95b53973
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-03 19:50:29 -0600

    Update and rename scratch.rmd to scratch.r

---
+++ scratch.r
@@ -0,0 +1,14 @@
+install.packages("rgdal");install.packages("RSQLite")
+library(rgdal)
+library(RSQLite)
+dta <- src_sqlite("al_tuscaloosa.gpkg/al_tuscaloosa.gpkg") 
+tbldata <- tbl(dta, "al_tuscaloosa") #Create a table from a data source
+tbldf <- as.data.frame (tbldata) #Create a data frame
+colnames(tbldf)
+summary(tbldf[,-2])
+apply(tbldf[,-2],2, function(x2) l-sum(is.na(x2)))->notNull
+apply(tbldf[,-2],2, function(x2) sum(is.na(x2)))->SumNull
+data.frame(notNull) ->notNulldf
+data.frame(SumNull) ->sumNulldf
+cbind(notNulldf,sumNulldf) ->exdf
+save(exdf,file ="exdf.csv")
