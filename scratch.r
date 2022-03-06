a54615550e43c47d36305df257d7ed7b8fb19a22
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-05 19:44:00 -0600

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -27,3 +27,24 @@ murder<-state.x77[,5]
 illit<-state.x77[,3]
 income <- state.x77[,2]
 coplot(murder ~ illit | income)
+
+
+library(MASS)
+parcoord(state.x77)
+parcoord(state.x77[, c(4, 6, 2, 5, 3)])
+
+install.packages("mvtnorm")
+library("mvtnorm")
+Mu <- c(1, 2)
+S <- matrix(c(1, -1, -1, 4), ncol = 2)
+x <- seq(-3, 5, length = 100)
+y <- seq(-5, 9, length = 100)
+z <- matrix(NA, length(x), length(y))
+for (i in 1:length(x)){
+  for (j in 1:length(y)){
+    z[i,j] <- dmvnorm(c(x[i], y[j]), Mu, S)
+  }
+}
+contour(x, y, z)
+persp(x, y, z)
+persp(x, y, z, theta = 70, phi = 50)
