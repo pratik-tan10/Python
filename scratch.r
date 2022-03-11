842ed433bf59535976babb2f9aaa34bc8a275499
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-10 23:41:44 -0600

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,33 +1,24 @@
 install.packages("shiny")
-ibrary(shiny)
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
 
-# Define UI for app that draws a histogram ----
-ui <- fluidPage(
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
 
-  # App title ----
-  titlePanel("Hello Shiny!"),
+    x    <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
+    hist(x, breaks = bins, col = "#75AADB", border = "white",
+         xlab = "Waiting time to next eruption (in mins)",
+         main = "Histogram of waiting times")
 
-    # Sidebar panel for inputs ----
-    sidebarPanel(
+    })
 
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-
-    ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-
-    )
-  )
-)
+}
