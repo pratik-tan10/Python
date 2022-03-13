b54eda72e40e6dea51d4f64f1427b1327eb3c13d
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-13 17:11:06 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -16,3 +16,15 @@ ymd("1920/1/2")
 ymd_hms(dt1)
 hms("03:22:14")
 ymd(dt2)
+
+update(this_moment,hours=8,minutes=34,seconds=55)
+update(this_moment, hours = 10, minutes = 16, seconds = 0)
+this_moment
+now(tzone= "America/New_York")->nyc
+depart<- nyc+days(2)
+depart<- update(depart, hours=17,minutes=34)
+arrive<-depart + hours(15) + minutes(50)
+with_tz(arrive,"Asia/Hong_Kong") ->arrive
+mdy("June 17, 2008",tz="Singapore") -> last_time
+interval(last_time,arrive)->how_long
+as.period(how_long)
