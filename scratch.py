ddd7710b0d00ea720dc4abd04205741ecc4b7d63
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-16 20:25:27 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -21,3 +21,32 @@ wind(circle)
 
 imgN1 = cv2.imread("double.png",1)
 wind(imgN)
+
+imgN = deepcopy(imgN1)
+def click2circle(event,x,y,flags,params):
+    if event == cv2.EVENT_LBUTTONDBCLIK:
+        cv2.circle(imgN,(x,y),50,(225,0,0),4)
+    
+cv2.namedWindow("differences", cv2.WINDOW_NORMAL)
+cv2.setMouseCallback("differences")
+
+while True:
+    cv2.imshow("differences",imgN)
+    a = cv2.waitKey(1)
+    if a==27:
+        break
+cv2.destroyAllWindows()
+
+b,g,r = cv2.split(imgCol)
+imgCol
+imgCol2 = cv2.merge((b,g,r))
+wind(imgCol2)
+
+imgres = cv2.resize(imgCol, dsize = None,fx=0.2,fy=0.2,interpolation = cv2.INTER_CUBIC)
+cv2.imshow("window",imgres)
+cv2.waitKey(10000)
+cv2.destroyAllWindows()
+
+cropped = imgres[210:368,300,570]
+wind(cropped)
+cv2.imwrite("cropped.png",cropped)
