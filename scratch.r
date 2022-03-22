d636922fc7392d39e69abd937f706ad15cd66d90
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-22 17:56:37 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -9,3 +9,10 @@ recoveredraw <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID
 library(tidyr)
 library(dplyr)
 confirmed <- confirmedraw %>% gather(key="date", value="confirmed", -c(Country.Region, Province.State, Lat, Long)) %>% group_by(Country.Region, date) %>% summarize(confirmed=sum(confirmed))
+
+deaths <- deathsraw %>% gather(key="date", value="deaths", -c(Country.Region, Province.State, Lat, Long)) %>% group_by(Country.Region, date) %>% summarize(deaths=sum(deaths))
+recovered <- recoveredraw %>% gather(key="date", value="recovered", -c(Country.Region, Province.State, Lat, Long)) %>% group_by(Country.Region, date) %>% summarize(recovered=sum(recovered))
+summary(confirmed)
+
+# Final data: combine all three
+country <- full_join(confirmed, deaths) %>% full_join(recovered)
