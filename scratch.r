3071b3d956311094663c2956aeeeeeec194b7ec3
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-23 16:31:56 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,18 +1,16 @@
-confirmedraw <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_c…")
-str(confirmedraw) # Check latest date at the end of data
-deathsraw <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_c…")
-recoveredraw <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_c…")
-# Note differences in the number of rows/columns
-
-# DATA CLEANING: To create country level and global combined data
-# Convert each data set from wide to long AND aggregate at country level
-library(tidyr)
-library(dplyr)
-confirmed <- confirmedraw %>% gather(key="date", value="confirmed", -c(Country.Region, Province.State, Lat, Long)) %>% group_by(Country.Region, date) %>% summarize(confirmed=sum(confirmed))
-
-deaths <- deathsraw %>% gather(key="date", value="deaths", -c(Country.Region, Province.State, Lat, Long)) %>% group_by(Country.Region, date) %>% summarize(deaths=sum(deaths))
-recovered <- recoveredraw %>% gather(key="date", value="recovered", -c(Country.Region, Province.State, Lat, Long)) %>% group_by(Country.Region, date) %>% summarize(recovered=sum(recovered))
-summary(confirmed)
-
-# Final data: combine all three
-country <- full_join(confirmed, deaths) %>% full_join(recovered)
+library("mvtnorm")
+Mu <- c(1, 2)
+s <- matrix(c(1, -1, -1, 4), ncol = 2)
+x <- seq(-3, 5, length = 100)
+y <- seq(-5, 9, length = 100)
+z <- matrix(NA, length(x), length(y))
+for (i in 1:length(x)){
+	for (j in 1:length(y)){
+		z[i,j] <- dmvnorm(c(x[i], y[j]), Mu, S)
+	}
+}
+contour(x, y, z)
+persp(x, y, z)
+persp(x, y, z, theta = 70, phi = 50)
+income <- state.x77[,2]
+coplot(murder ~ illit | income)
