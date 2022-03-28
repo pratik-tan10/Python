3f1456567d9bb2eece397de68c3271fc35672f68
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-27 21:21:12 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,30 +1,19 @@
-library(shiny)
-shinyUI(fluidPage(
-    titlePanel("Simple Shiny Calculator"),
-    sidebarLayout(
-        mainPanel(
-        h5("This is a simple calculator. 
-        You put the first and the second number,
-        select the operator and voila!"),
-        numericInput("num1", "Select the first number", 0),
-        numericInput("num2", "Select the second number", 0),
-        selectInput("operator", "Select the operator",
-        choices = c("+","-","x", "÷"))
-        ),
-        mainPanel(
-        h2("The result is:"),
-        textOutput("output")
-        ))))
+setwd(".../Project/R/Workspace/Task1")
+library("EBImage"  , lib.loc="~/R/win-library/3.2")
+library("fftwtools", lib.loc="~/R/win-library/3.2")
+library("fftw", lib.loc="~/R/win-library/3.2")
 
-library(shiny)
-shinyServer(function(input, output) {
-        output$output <- renderText({
-                switch(input$operator,
-                       "+" = input$num1 + input$num2,
-                       "-" = input$num1 - input$num2,
-                       "x" = input$num1 * input$num2,
-                       "÷" = input$num1 / input$num2)
-                
-        })
-        
-})
+# Image Acquisition
+img <- readImage(".../Project/Beispielbilder/drmcircle.jpg")
+display(img, title='Image')
+
+# Grayscaled
+img_gray<-channel(img,"gray")
+
+# FFT
+img_ff <- fft(img_gray)    #fftw2d
+
+magntd <- sqrt(Re(img_ff)^2+Im(img_ff)^2)
+phase  <- atan(Im(img_ff)/Re(img_ff))
+
+plot(log(magntd),main="FFT")
