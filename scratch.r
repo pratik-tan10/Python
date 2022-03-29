d8c7e3d9e8fcf25b780ef8f24ff829658fb1973a
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-29 18:43:34 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,27 +1,11 @@
-library(data.table) df <- fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
-df <- data.frame(df,header=TRUE,stringsAsFactors=FALSE)
-library(dplyr)
-df$Province.State <- sub("^$", NA, df$Province.State)
-df <- transform(df, Province.State = ifelse(Province.State=="" | is.na(Province.State), Country.Region, Province.State))
-#Replace all fields with 0 if the value is NA
-df <- df %>% replace(.=="NA", 0) # replace with 0
-df <- df[with(df, Country.Region %in% c("China","India","Pakistan","Iran")),]
-dfCountryGroupSum <- aggregate(df[,c(5:ncol(df))],by=list(Category=df$Country.Region),FUN=sum)
-head(dfCountryGroupSum[dfCountryGroupSum$Category=="China" ,c(1:5)])
-# Category X1.22.2020 X1.23.2020 X1.24.2020 X1.25.2020
-#28 China 548 643 920 1406
-dfCountryGroupSum[dfCountryGroupSum$Category=="China" | dfCountryGroupSum$Category=="India",c(1:10)]
-Category X1.22.20 X1.23.20 X1.24.20 X1.25.20 X1.26.20 X1.27.20 X1.28.20 X1.29.20 X1.30.20
-37 China 548 643 920 1406 2075 2877 5509 6087 8141
-80 India 0 0 0 0 0 0 0 0 1
-head(dfCountryGroupSum[dfCountryGroupSum$Category %in% c("China","India"),c(1:5)])
+library(datasets)
+with(airquality, plot(Wind,Ozone))
+title(main = "Ozone and Wind in New York City")
 
-dfCountryGroupSum$Total <- apply(dfCountryGroupSum[,c(2:ncol(dfCountryGroupSum))],1,sum,na.omit=T)
-head(dfCountryGroupSum[with(dfCountryGroupSum, Category %in% c("China","India")),c(1,ncol(dfCountryGroupSum))])
+with(airquality,plot(Wind,Ozone, main = "Ozone and Wind in New York City"))
+with(subset(airquality,Month==5),point(Wind,Ozone,col="blue"))
 
-names(dfCountryGroupSum)[1]<-c("Country")
-TotConfirmedCase <- dfCountryGroupSum[,c(1,ncol(dfCountryGroupSum))]
-View(TotConfirmedCase)
-p<-ggplot(TotConfirmedCase, aes(x=Country,y=log(TotCase))) +
-geom_bar(stat="identity")
-p
+with(airquality,plot(Wind,Ozone, main = "Ozone and Wind in New York City", type="n"))
+with(subset(airquality,Month==5),point(Wind,Ozone,col="blue"))
+with(subset(airquality,Month!=5),point(Wind,Ozone,col="red"))
+legend("topright",pch=1,col=c("blue","red"),legend=c("May","Other Months"))
