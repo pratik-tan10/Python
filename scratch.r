1dfd40a55f01153d1c2f10af8031818b6f943fed
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-28 20:54:42 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,84 +1,17 @@
-setwd(".../Project/R/Workspace/Task1")
-library("EBImage"  , lib.loc="~/R/win-library/3.2")
-
-# Image
-img <- readImage(".../Project/Beispielbilder/drmtri.jpg")
-display(img, title='Image')
-
-# Grayscaled
-img_gray<-channel(img,"gray")
-
-# FFT
-img_ff <- fft(img_gray) #fftw2d
-
-
-# FFT SHIFT
-fftshift <- function(img_ff, dim = -1) {
-
-  rows <- dim(img_ff)[1]    
-  cols <- dim(img_ff)[2]    
-
-  swap_up_down <- function(img_ff) {
-    rows_half <- ceiling(rows/2)
-    return(rbind(img_ff[((rows_half+1):rows), (1:cols)], img_ff[(1:rows_half), (1:cols)]))
-  }
-
-  swap_left_right <- function(img_ff) {
-    cols_half <- ceiling(cols/2)
-    return(cbind(img_ff[1:rows, ((cols_half+1):cols)], img_ff[1:rows, 1:cols_half]))
-  }
-
-  if (dim == -1) {
-    img_ff <- swap_up_down(img_ff)
-    return(swap_left_right(img_ff))
-  }
-  else if (dim == 1) {
-    return(swap_up_down(img_ff))
-  }
-  else if (dim == 2) {
-    return(swap_left_right(img_ff))
-  }
-  else {
-    stop("Invalid dimension parameter")
-  }
-}
-
-ifftshift <- function(img_ff, dim = -1) {
-
-  rows <- dim(img_ff)[1]    
-  cols <- dim(img_ff)[2]    
-
-  swap_up_down <- function(img_ff) {
-    rows_half <- floor(rows/2)
-    return(rbind(img_ff[((rows_half+1):rows), (1:cols)], img_ff[(1:rows_half), (1:cols)]))
-  }
-
-  swap_left_right <- function(img_ff) {
-    cols_half <- floor(cols/2)
-    return(cbind(img_ff[1:rows, ((cols_half+1):cols)], img_ff[1:rows, 1:cols_half]))
-  }
-
-  if (dim == -1) {
-    img_ff <- swap_left_right(img_ff)
-    return(swap_up_down(img_ff))
-  }
-  else if (dim == 1) {
-    return(swap_up_down(img_ff))
-  }
-  else if (dim == 2) {
-    return(swap_left_right(img_ff))
-  }
-  else {
-    stop("Invalid dimension parameter")
-  }
-}
-
-# FFT SHIFT
-
-# Magnitude and Phase
-magntd <- sqrt(Re(img_ff)^2+Im(img_ff)^2)
-phase  <- atan(Im(img_ff)/Re(img_ff))
-
-img_fftsh <- fftshift(magntd)
-
-display(log(img_fftsh),title="FFT")
+library(data.table) df <- fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+df <- data.frame(df,header=TRUE,stringsAsFactors=FALSE)
+library(dplyr)
+df$Province.State <- sub("^$", NA, df$Province.State)
+df <- transform(df, Province.State = ifelse(Province.State=="" | is.na(Province.State), Country.Region, Province.State))
+#Replace all fields with 0 if the value is NA
+df <- df %>% replace(.=="NA", 0) # replace with 0
+df <- df[with(df, Country.Region %in% c("China","India","Pakistan","Iran")),]
+dfCountryGroupSum <- aggregate(df[,c(5:ncol(df))],by=list(Category=df$Country.Region),FUN=sum)
+head(dfCountryGroupSum[dfCountryGroupSum$Category=="China" ,c(1:5)])
+# Category X1.22.2020 X1.23.2020 X1.24.2020 X1.25.2020
+#28 China 548 643 920 1406
+dfCountryGroupSum[dfCountryGroupSum$Category=="China" | dfCountryGroupSum$Category=="India",c(1:10)]
+Category X1.22.20 X1.23.20 X1.24.20 X1.25.20 X1.26.20 X1.27.20 X1.28.20 X1.29.20 X1.30.20
+37 China 548 643 920 1406 2075 2877 5509 6087 8141
+80 India 0 0 0 0 0 0 0 0 1
+head(dfCountryGroupSum[dfCountryGroupSum$Category %in% c("China","India"),c(1:5)])
