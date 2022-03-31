173cb57928be3455de325e80ac37cf896b106b11
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-31 14:23:53 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -38,3 +38,11 @@ p <- xyplot(y ~ x | f, panel = function(x, y, ...) {
 })
 print(p)
 invisible()
+
+p2 <- xyplot(y ~ x | f, panel = function(x, y, ...) {
+  panel.xyplot(x, y, ...)  ## First call default panel function
+  panel.lmline(x, y, col = 2)  ## Overlay a simple linear regression line
+})
+print(p2)
+invisible()
+
