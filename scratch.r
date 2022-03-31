0f3a6c74fd983b5841b7912ff3573f016d78052c
Pratik Dhungana <pratik.tan10@gmail.com>
2022-03-30 22:10:20 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,22 +1,33 @@
-library(datasets)
-with(airquality, plot(Wind,Ozone))
-title(main = "Ozone and Wind in New York City")
+fun1<- function(x,a,b,c){
+a*x*exp(-b*x)+c
+}
+fun2<- function(x,a,b,c){
+-a*x*exp(-b*x)+c
+}
+a<-1
+b<-2
+c<- -0.1
+x.seq<-seq(0,5,0.01)
+y.seq<- fun2(x.seq,a,b,c)
+plot(x.seq,y.seq,type="l")
+abline(h=0)
 
-with(airquality,plot(Wind,Ozone, main = "Ozone and Wind in New York City"))
-with(subset(airquality,Month==5),point(Wind,Ozone,col="blue"))
+optimize(f=fun2,interval=c(0,2),a,b,c)
+optimize(f=fun1,interval=c(0,2),maximum=TRUE,a,b,c)
 
-with(airquality,plot(Wind,Ozone, main = "Ozone and Wind in New York City", type="n"))
-with(subset(airquality,Month==5),point(Wind,Ozone,col="blue"))
-with(subset(airquality,Month!=5),point(Wind,Ozone,col="red"))
-legend("topright",pch=1,col=c("blue","red"),legend=c("May","Other Months"))
+x<- c(0.164,0.117,1.880,0.424,0.280,0.353,0.574,0.096,1.081,0.693,1.449,1.181,0.364,0.359,2.173,0.374,0.192,1.617,2.070,1.922)
+hist(x)
+rug(x)
 
-with(airquality,plot(Wind,Ozone, main = "Ozone and Wind in New York City", pch=20))
-model<-lm(Ozone~Wind,airquality)
-abline(model,lwd=2)
+logL<-function(pars,x){
+alpha<-pars[1]
+beta<-pars[2]
 
-par(mfrow=c(1,3),mar=c(4,4,2,1),oma=c(0,0,2,0))
-wigh(airquality,{plot(Wind,Ozone,main="Ozone and Wind")
-                 plot(Solar.R,Ozone,main="Ozone and Solar Radiation")
-                 plot(Temp,Ozone,main="Ozone and Temperature")
-                 mtext("Ozone and Weather in New York City", outer=TRUE
-                 })
+n<- length(x)
+res <- -n*log(gamma(alpha))-n*alpha*log(beta)-sum(x)/beta +(alpha-1)*sum(log(x))
+return(-res)
+}
+alpha0<-1
+beta0<-1
+pars<-c(alpha0,beta0)
+A<-optim(pars,logL,gr=NULL,x=x);A
