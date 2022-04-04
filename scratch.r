e9ee1df0976e54a891a1bc41a862b14df0dc33ba
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-03 20:23:55 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,38 +1,29 @@
-plotOutput("plot1", click = "plot_click")
-library(shiny)
+## app.R ##
+library(shinydashboard)
 
-ui <- basicPage(
-  plotOutput("plot1",
-    click = "plot_click",
-    dblclick = "plot_dblclick",
-    hover = "plot_hover",
-    brush = "plot_brush"
-  ),
-  verbatimTextOutput("info")
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(plotOutput("plot1", height = 250)),
+
+      box(
+        title = "Controls",
+        sliderInput("slider", "Number of observations:", 1, 100, 50)
+      )
+    )
+  )
 )
 
 server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+
   output$plot1 <- renderPlot({
-    plot(mtcars$wt, mtcars$mpg)
-  })
-
-  output$info <- renderText({
-    xy_str <- function(e) {
-      if(is.null(e)) return("NULL\n")
-      paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
-    }
-    xy_range_str <- function(e) {
-      if(is.null(e)) return("NULL\n")
-      paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1), 
-             " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
-    }
-
-    paste0(
-      "click: ", xy_str(input$plot_click),
-      "dblclick: ", xy_str(input$plot_dblclick),
-      "hover: ", xy_str(input$plot_hover),
-      "brush: ", xy_range_str(input$plot_brush)
-    )
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
   })
 }
 
