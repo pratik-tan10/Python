850b0e7bc1fc54fcc101317a9545180cff4641c9
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-05 22:23:52 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,40 +1,16 @@
-# Read this shape file with the rgdal library. 
-library(rgdal)
-my_spdf <- readOGR( 
-  dsn= paste0(getwd(),"/world_shape_file") , 
-  layer="TM_WORLD_BORDERS_SIMPL-0.3",
-  verbose=FALSE
-)
+f11<-function(x)x^3-x+1
+interval1<-seq(-5,5,length=20)
+plot(interval1,f11(interval1),type="l");points(interval1,f11(interval1))
+uniroot(f11,interval=c(-2,1))
 
-# Select Africa only
-africa <- my_spdf[my_spdf@data$REGION==2 , ]
-plot(my_spdf[my_spdf@data$REGION==150,])
+#1b
+f12<-function(x)x*exp(sqrt(x))-1
+interval1<-seq(0,1,length=20)
+plot(interval1,f12(interval1),type="l");points(interval1,f12(interval1))
+uniroot(f12,interval=c(0,1))
 
-# -- > Now you have a Spdf object (spatial polygon data frame). You can start doing maps!
-
-# Plot
-plot(africa , xlim=c(-20,60) , ylim=c(-40,40))
-# library
-library(dplyr)
-library(ggplot2)
-
-# Make sure the variable you are studying is numeric
-africa@data$POP2005 <- as.numeric( africa@data$POP2005 )
-
-# Distribution of the population per country?
-africa@data %>% 
-  ggplot( aes(x=as.numeric(POP2005))) + 
-  geom_histogram(bins=20, fill='#69b3a2', color='white')
-
-
-# Palette of 30 colors
-library(RColorBrewer)
-my_colors <- brewer.pal(9, "Reds") 
-my_colors <- colorRampPalette(my_colors)(30)
-
-# Attribute the appropriate color to each country
-class_of_country <- cut(africa@data$POP2005, 30)
-my_colors <- my_colors[as.numeric(class_of_country)]
-
-# Make the plot
-plot(africa , xlim=c(-20,60) , ylim=c(-40,40), col=my_colors ,  bg = "#A6CAE0")
+#1c
+f13<-function(x)x-(log(x))^2
+interval1<-seq(0.1,2,length=20)
+plot(interval1,f13(interval1),type="l");points(interval1,f13(interval1))
+uniroot(f13,interval=c(0.1,2))
