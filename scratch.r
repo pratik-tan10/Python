8a891c1de7ea643e1eda96d4b0fdebf3bec10489
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-07 14:51:49 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,40 +1,31 @@
 #install.packages(c("spdep","sp"))
-setwd("C:/Users/Research Lab/OneDrive - The University of Alabama/Rupesh/Shapefiles")
+setwd("drive:/some/location/on/disk")
 library(spdep)
 #library(sp)
 library(rgdal)
 
+#Read input data
 nepal <- readOGR("dummyLUNeig.shp", "dummyLUNeig")
 row.names(nepal) <- as.character(nepal$DISTRICT)
+#DISTRICT is the unique identifier
 nb <- poly2nb(nepal)
 names(nb)<-row.names(nepal)
 
-#nepal@data[["DISTRICT"]][nb[[1]]]
-#nepal@data[["Area"]][nb[[1]]]
-#nepal@data[["LUC"]][nb[[1]]]
-#cbind(names(nb)[1],nepal@data[["DISTRICT"]][nb[[1]]],nepal@data[["LUC"]][nb[[1]]],nepal@data[["Area"]][nb[[1]]])
-
-
+#Unlist the neighbor list
 ul<-unlist(nb)
+#This is to remove the last character that is added to the name after unlisting
 names(ul)<-lapply(names(ul),function(x){substr(x,1,nchar(x)-1)})
-cmbd<-cbind(names(ul),ul)
-cmbd2<-data.frame(cmbd)
-colnames(cmbd2)<-c("Name","Neighbor")
-nbrInfo<-data.frame(tapply(cmbd2$Neighbor,cmbd2$Name,function(x2)x2))
-colnames(nbrInfo)<-"Neighbor"
-#nepal@data[["NBR"]]<-nbrInfo
-#head(nepal@data)
-#nbrInfo[paste0("Lu_",1)]<-0
 
+#Create list of all neighbors for each polygon and convert to dataframe
+cmbd<-data.frame(cbind(names(ul),ul))
+colnames(cmbd)<-c("Name","Neighbor")
+nbrInfo<-data.frame(tapply(cmbd$Neighbor,cmbd$Name,function(x2)x2))
+colnames(nbrInfo)<-"Neighbor"
+
+#Add columns indicating lu class to polygons, LUC is the column containing LU class info
 for( i in seq_along(unique(nepal@data[["LUC"]]))){
   nbrInfo[paste0("Lu_",i)]<-0
 }
-
-#nbrInfo
-#as.integer(unlist(nbrInfo[1,1]))
-#nepal@data[["LUC"]]
-
-#nbrInfo$Lu_1[1]<-sum(subset(nepal@data[as.integer(unlist(nbrInfo[1,1])),2:3],LUC==1)$Area)
 
 #Final function to compute area of each LU type
 for(jj in seq_along(nepal@data[["DISTRICT"]])){
