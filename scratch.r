63af3582d3a2b4e29d652f0aec7f928a4c3f8b36
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-09 10:27:17 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -36,3 +36,20 @@ for(jj in seq_along(nepal@data[["DISTRICT"]])){
 
 #Export dataframe to csv
 write.csv(nbrInfo[,-1],"nbrInfo.csv")
+
+## Create data for the graph.
+x <-  c(21, 62, 10,53)
+labels <-  c("London","New York","Singapore","Mumbai")
+
+piepercent<- round(100*x/sum(x), 1)
+
+# Give the chart file a name.
+png(file = "city_percentage_legends.jpg")
+
+# Plot the chart.
+pie(x, labels = piepercent, main = "City pie chart",col = rainbow(length(x)))
+legend("topright", c("London","New York","Singapore","Mumbai"), cex = 0.8,
+   fill = rainbow(length(x)))
+
+# Save the file.
+dev.off()
