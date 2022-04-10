806be5520a8942367f49250f23e3747802392542
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-10 12:36:59 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,12 +1,12 @@
-# First we set the state of the network
-σ = np.tanh
-w1 = -5
-b1 = 5
+# First set up the network.
+sigma = np.tanh
+W = np.array([[-2, 4, -1],[6, 0, -3]])
+b = np.array([0.1, -2.5])
 
-# Then we define the neuron activation.
-def a1(a0) :
-  return σ(w1 * a0 + b1)
-  
-# Finally let's try the network out!
-# Replace x with 0 or 1 below,
-a1(1)
+# Define our input vector
+x = np.array([0.3, 0.4, 0.1])
+
+# Calculate the values by hand,
+# and replace a1_0 and a1_1 here (to 2 decimal places)
+# (Or if you feel adventurous, find the values with code!)
+a1 = np.matmul(W,x)+b
