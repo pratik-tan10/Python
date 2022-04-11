12785285dd0a6ac120fe794ebfacc404bd60a308
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-11 16:48:57 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -64,3 +64,23 @@ summary(mylogit)
 confint(mylogit)
 confint.default(mylogit)
 exp(cbind(OR = coef(mylogit), confint(mylogit)))
+                           
+
+newdata2 <- with(mydata, data.frame(gre = rep(seq(from = 200, to = 800, length.out = 100),
+                                              4), gpa = mean(gpa), rank = factor(rep(1:4, each = 100))))
+
+newdata3 <- cbind(newdata2, predict(mylogit, newdata = newdata2, type = "link",
+                                    se = TRUE))
+newdata3 <- within(newdata3, {
+  PredictedProb <- plogis(fit)
+  LL <- plogis(fit - (1.96 * se.fit))
+  UL <- plogis(fit + (1.96 * se.fit))
+})
+
+## view first few rows of final dataset
+head(newdata3)
+library(ggplot2)
+ggplot(newdata3, aes(x = gre, y = PredictedProb)) + geom_ribbon(aes(ymin = LL,
+                                                                    ymax = UL, fill = rank), alpha = 0.2) + geom_line(aes(colour = rank),
+                                                                                                                      size = 1)
+
