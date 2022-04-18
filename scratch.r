fef2b22ec2245003468ff65356e0e39c903bbf82
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-18 16:26:35 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,68 +1,23 @@
-# install.packages("palmerpenguins")
-library(palmerpenguins)
+# NOT RUN {
+x <- 1:4
+fft(x)
+fft(fft(x), inverse = TRUE)/length(x)
 
-library(tidyverse)
+## Slow Discrete Fourier Transform (DFT) - e.g., for checking the formula
+fft0 <- function(z, inverse=FALSE) {
+  n <- length(z)
+  if(n == 0) return(z)
+  k <- 0:(n-1)
+  ff <- (if(inverse) 1 else -1) * 2*pi * 1i * k/n
+  vapply(1:n, function(h) sum(z * exp(ff*(h-1))), complex(1))
+}
 
-dat <- penguins %>%
-  select(species, flipper_length_mm)
-summary(dat)
-library(ggplot2)
-
-ggplot(dat) +
-  aes(x = species, y = flipper_length_mm, color = species) +
-  geom_jitter() +
-  theme(legend.position = "none")
-res_aov <- aov(flipper_length_mm ~ species,
-  data = dat
-)
-par(mfrow = c(1, 2)) # combine plots
-
-# histogram
-hist(res_aov$residuals)
-
-# QQ-plot
-library(car)
-qqPlot(res_aov$residuals,
-  id = FALSE # id = FALSE to remove point identification
-)
-shapiro.test(res_aov$residuals)
-# Boxplot
-boxplot(flipper_length_mm ~ species,
-  data = dat
-)
-# Dotplot
-library("lattice")
-
-dotplot(flipper_length_mm ~ species,
-  data = dat
-)
-# Levene's test
-library(car)
-
-leveneTest(flipper_length_mm ~ species,
-  data = dat
-)
-
-par(mfrow = c(1, 2)) # combine plots
-
-# 1. Homogeneity of variances
-plot(res_aov, which = 3)
-
-# 2. Normality
-plot(res_aov, which = 2)
-boxplot(flipper_length_mm ~ species,
-  data = dat
-)
-boxplot(flipper_length_mm ~ species,
-  data = dat
-)
-library(ggplot2)
-
-ggplot(dat) +
-  aes(x = species, y = flipper_length_mm) +
-  geom_boxplot()
-
-aggregate(flipper_length_mm ~ species,
-  data = dat,
-  function(x) round(c(mean = mean(x), sd = sd(x)), 2)
-)
+relD <- function(x,y) 2* abs(x - y) / abs(x + y)
+n <- 2^8
+z <- complex(n, rnorm(n), rnorm(n))
+# }
+# NOT RUN {
+## relative differences in the order of 4*10^{-14} :
+summary(relD(fft(z), fft0(z)))
+summary(relD(fft(z, inverse=TRUE), fft0(z, inverse=TRUE)))
+# }
