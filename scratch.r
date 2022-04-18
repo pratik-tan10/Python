4fcd0bfec081f82d5c1591c197473d069867ad5e
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-17 23:19:56 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,21 +1,68 @@
-install.packages('multcomp')
-library(multcomp)
-str(cholesterol)
-attach(cholesterol)
-aov_model <- aov(response ~ trt)
+# install.packages("palmerpenguins")
+library(palmerpenguins)
 
-install.packages('gplots')
-library(gplots)
-plotmeans(response ~ trt, xlab="Treatment", ylab="Response",
-main="Mean Plot\nwith 95% CI")
+library(tidyverse)
 
-update.packages(checkBuilt = TRUE)
-install.packages("car", dependencies = TRUE)
+dat <- penguins %>%
+  select(species, flipper_length_mm)
+summary(dat)
+library(ggplot2)
+
+ggplot(dat) +
+  aes(x = species, y = flipper_length_mm, color = species) +
+  geom_jitter() +
+  theme(legend.position = "none")
+res_aov <- aov(flipper_length_mm ~ species,
+  data = dat
+)
+par(mfrow = c(1, 2)) # combine plots
+
+# histogram
+hist(res_aov$residuals)
+
+# QQ-plot
 library(car)
-outlierTest(aov_model)
+qqPlot(res_aov$residuals,
+  id = FALSE # id = FALSE to remove point identification
+)
+shapiro.test(res_aov$residuals)
+# Boxplot
+boxplot(flipper_length_mm ~ species,
+  data = dat
+)
+# Dotplot
+library("lattice")
 
-df <- read.csv(“file.csv”)
-df
-anova_two_way <- aov(response ~ trt + BP, data = df)
-summary(anova_two_way)
+dotplot(flipper_length_mm ~ species,
+  data = dat
+)
+# Levene's test
+library(car)
 
+leveneTest(flipper_length_mm ~ species,
+  data = dat
+)
+
+par(mfrow = c(1, 2)) # combine plots
+
+# 1. Homogeneity of variances
+plot(res_aov, which = 3)
+
+# 2. Normality
+plot(res_aov, which = 2)
+boxplot(flipper_length_mm ~ species,
+  data = dat
+)
+boxplot(flipper_length_mm ~ species,
+  data = dat
+)
+library(ggplot2)
+
+ggplot(dat) +
+  aes(x = species, y = flipper_length_mm) +
+  geom_boxplot()
+
+aggregate(flipper_length_mm ~ species,
+  data = dat,
+  function(x) round(c(mean = mean(x), sd = sd(x)), 2)
+)
