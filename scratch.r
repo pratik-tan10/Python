d52dec1983ac00bc5cc40ab39c72b62cdb7ac01b
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-21 15:07:30 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,64 +1,44 @@
-dim(pm0)
-head(pm0)
-cnames
-strsplit(cnames,"|",fixed=TRUE)->cnames
-names(pm0)<-make.names(cnames[[1]][wcol])
-head(pm0)
-x0<-pm0$Sample.Value
-str(x0)
-mean(is.na(x0))
-names(pm1)<- make.names(cnames[[1]][wcol])
-dim(pm1)
-x1<-pm1$Sample.Value
-mean(is.na(x1))
-summary(x0)
-summary(x1)
-boxplot(x0,x1)
-boxplot(log10(x0),log10(x1))
+library(tidyverse)
+library(caret)
+# Load the data and remove NAs
+data("PimaIndiansDiabetes2", package = "mlbench")
+PimaIndiansDiabetes2 <- na.omit(PimaIndiansDiabetes2)
+# Inspect the data
+sample_n(PimaIndiansDiabetes2, 3)
+# Split the data into training and test set
+set.seed(123)
+training.samples <- PimaIndiansDiabetes2$diabetes %>% 
+  createDataPartition(p = 0.8, list = FALSE)
+train.data  <- PimaIndiansDiabetes2[training.samples, ]
+test.data <- PimaIndiansDiabetes2[-training.samples, ]
+################################
+library(MASS)
+# Fit the model
+model <- glm(diabetes ~., data = train.data, family = binomial) %>%
+  stepAIC(trace = FALSE)
+# Summarize the final selected model
+summary(model)
+# Make predictions
+probabilities <- model %>% predict(test.data, type = "response")
+predicted.classes <- ifelse(probabilities > 0.5, "pos", "neg")
+# Model accuracy
+mean(predicted.classes==test.data$diabetes)
 
-negative<-x1<0
-sum(negative,na.rm = TRUE)
-mean(negative,na.rm = TRUE)
-dates<-pm1$Date
-str(dates)
-dates<-as.Date(as.character(dates),"%Y%m%d")
-head(dates)
-hist(dates[negative],"month")
+full.model <- glm(diabetes ~., data = train.data, family = binomial)
+coef(full.model)
+step.model <- full.model %>% stepAIC(trace = TRUE)
+coef(step.model)
 
-str(site0)
-str(site1)
-both<-intersect(site0,site1)
-head(pm0)
-cnt0<-subset(pm0,State.Code==36& county.site %in% both)
-cnt1<-subset(pm1,State.Code==36& county.site %in% both)
-sapply(split(cnt0,cnt0$county.site),nrow)
-sapply(split(cnt1,cnt1$county.site),nrow)
-pm0sub<-subset(cnt0,County.Code==63 & Site.ID==2008)
-pm1sub<-subset(cnt1,County.Code==63 & Site.ID==2008)
+# Make predictions
+probabilities <- full.model %>% predict(test.data, type = "response")
+predicted.classes <- ifelse(probabilities > 0.5, "pos", "neg")
+# Prediction accuracy
+observed.classes <- test.data$diabetes
+mean(predicted.classes == observed.classes)
 
-x0sub<-pm0sub$Sample.Value
-x1sub<-pm1sub$Sample.Value
-dates0<-as.Date(as.character(pm0sub$Date),"%Y%m%d")
-dates1<-as.Date(as.character(pm1sub$Date),"%Y%m%d")
-par(mfrow = c(1,2),mar = c(4,4,2,1))
-plot(dates0,x0sub,pch=20)
-abline(h=median(x0sub, na.rm = TRUE),lwd=2)
-plot(dates1,x1sub,pch=20)
-abline(h=median(x1sub,na.rm=TRUE),lwd=2)
-rng<-range(x0sub,x1sub,na.rm=TRUE)
-rng
-mn0<-with(pm0,tapply(Sample.Value,State.Code,mean,na.rm=TRUE))
-str(mn0)
-mn1<-with(pm1,tapply(Sample.Value,State.Code,mean,na.rm=TRUE))
-str(mn1)
-summary(mn0)
-summary(mn1)
-d0<-data.frame(state=names(mn0),mean=mn0)
-d1<-data.frame(state=names(mn1),mean=mn1)
-mrg<-merge(d0,d1,by="state")
-dim(mrg)
-head(mrg)
-with(mrg, plot(rep(1,52),mrg[,2], xlim=c(.5,2.5)))
-with(mrg, points(rep(2,52),mrg[,3]))
-segments(rep(1,52),mrg[,2],rep(2,52),mrg[,3])
-mrg[mrg$mean.x<mrg$mean.y,]
+# Make predictions
+probabilities <- predict(step.model, test.data, type = "response")
+predicted.classes <- ifelse(probabilities > 0.5, "pos", "neg")
+# Prediction accuracy
+observed.classes <- test.data$diabetes
+mean(predicted.classes == observed.classes)
