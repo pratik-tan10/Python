b1d5eade0396c249f98fde2057481c483ad74b29
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-23 23:43:15 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -13,13 +13,26 @@ class Node:
 class SLinkedList:
    def __init__(self):
       self.headval = None
+# Print the linked list
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+   def AtBegining(self,newdata):
+      NewNode = Node(newdata)
 
-list1 = SLinkedList()
-list1.headval = Node("Mon")
+# Update the new nodes next val to existing node
+   NewNode.nextval = self.headval
+   self.headval = NewNode
+
+list = SLinkedList()
+list.headval = Node("Mon")
 e2 = Node("Tue")
 e3 = Node("Wed")
-# Link first Node to second node
-list1.headval.nextval = e2
 
-# Link second Node to third node
+list.headval.nextval = e2
 e2.nextval = e3
+
+list.AtBegining("Sun")
+list.listprint()
