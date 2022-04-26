682f727ca69dba430df25b0364670669192144e0
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-25 23:56:06 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -6,38 +6,41 @@ arcpy.FeatureToPoint_management("main.al_tuscaloosa", "parcels_center.shp",
                                 "INSIDE")
 ###################################
 class Node:
-   def __init__(self, dataval=None):
-      self.dataval = dataval
-      self.nextval = None
+   def __init__(self, data=None):
+      self.data = data
+      self.next = None
 class SLinkedList:
    def __init__(self):
-      self.headval = None
+      self.head = None
 
-# Function to add node
-   def Inbetween(self,middle_node,newdata):
-      if middle_node is None:
-         print("The mentioned node is absent")
+   def Atbegining(self, data_in):
+      NewNode = Node(data_in)
+      NewNode.next = self.head
+      self.head = NewNode
+
+# Function to remove node
+   def RemoveNode(self, Removekey):
+      HeadVal = self.head
+         
+      if (HeadVal is not None):
+         if (HeadVal.data == Removekey):
+            self.head = HeadVal.next
+            HeadVal = None
+            return
+      while (HeadVal is not None):
+         if HeadVal.data == Removekey:
+            break
+         prev = HeadVal
+         HeadVal = HeadVal.next
+
+      if (HeadVal == None):
          return
 
-      NewNode = Node(newdata)
-      NewNode.nextval = middle_node.nextval
-      middle_node.nextval = NewNode
+      prev.next = HeadVal.next
+         HeadVal = None
 
-# Print the linked list
-   def listprint(self):
-      printval = self.headval
-      while printval is not None:
-         print (printval.dataval)
-         printval = printval.nextval
-
-list = SLinkedList()
-list.headval = Node("Mon")
-e2 = Node("Tue")
-e3 = Node("Thu")
-
-list.headval.nextval = e2
-e2.nextval = e3
-
-list.Inbetween(list.headval.nextval,"Fri")
-
-list.listprint()
+   def LListprint(self):
+      printval = self.head
+      while (printval):
+         print(printval.data),
+         printval = printval.next
