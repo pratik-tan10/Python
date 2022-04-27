a97a97d7c122a2b85718aec6c5be37b65d79082c
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-26 22:09:52 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -5,50 +5,34 @@ arcpy.env.workspace = "C:/Users/Research Lab/BoxPratik/Box Sync/Pratik/sem2/Thes
 arcpy.FeatureToPoint_management("main.al_tuscaloosa", "parcels_center.shp", 
                                 "INSIDE")
 ###################################
-class Node:
-   def __init__(self, data=None):
-      self.data = data
-      self.next = None
-class SLinkedList:
-   def __init__(self):
-      self.head = None
+import dash
+from dash import dcc
+from dash import html
+import pandas as pd
+data = pd.read_csv("covid1.csv")
+ 
+data['Date'] = pd.to_datetime(data['Date']).dt.strftime('%d-%m-%Y')
 
-   def Atbegining(self, data_in):
-      NewNode = Node(data_in)
-      NewNode.next = self.head
-      self.head = NewNode
-
-# Function to remove node
-   def RemoveNode(self, Removekey):
-      HeadVal = self.head
-         
-      if (HeadVal is not None):
-         if (HeadVal.data == Removekey):
-            self.head = HeadVal.next
-            HeadVal = None
-            return
-      while (HeadVal is not None):
-         if HeadVal.data == Removekey:
-            break
-         prev = HeadVal
-         HeadVal = HeadVal.next
-
-      if (HeadVal == None):
-         return
-
-      prev.next = HeadVal.next
-         HeadVal = None
-
-   def LListprint(self):
-      printval = self.head
-      while (printval):
-         print(printval.data),
-         printval = printval.next
-
-llist = SLinkedList()
-llist.Atbegining("Mon")
-llist.Atbegining("Tue")
-llist.Atbegining("Wed")
-llist.Atbegining("Thu")
-llist.RemoveNode("Tue")
-llist.LListprint()
+app = dash.Dash(__name__)
+ 
+app.layout=html.Div(
+    children=[
+        html.H1(children="Covid cases and recovery",),
+        html.P(
+            children="First wave of covid.",
+        ),
+        dcc.Graph(
+            figure={
+                "data":[
+                    {
+                        "x":data["Date"],
+                        "y":data["High"],
+                        "type":"lines",
+                    },
+                ],
+                "layout":{"title":"Day-wise highest prices of index"},
+                    },
+        ),
+]
+if __name__ == "__main__":
+    app.run_server(debug=True)
