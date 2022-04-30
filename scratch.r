7179d2581e122fc4d23d561097aba5d393e17389
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-29 23:01:22 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,14 +1,18 @@
-mydata<-data.frame(x=c(0,2,4,15,48,75,122),y=c(123,116,113,100,87,84,77))
-library(ggplot2)
-qplot(x,y,data=mydata,geom="line")
-f <- function(x, a, b) {
-  a * exp(b *-x)
-}
-x <- seq(0:100)
-y <- f(seq(0:100), 1,1)
-qplot(x,y, geom="line")
-fit <- nls(y~f(x,a,b,c), data=data.frame(mydata), start=list(a=1, b=30, c=-0.3))
-x <- seq(0,120)
-fitted.data <- data.frame(x=x, y=predict(fit, list(x=x)))
-                          ggplot(mydata, aes(x, y)) + geom_point(color="red", alpha=.5) + geom_line(alpha=.5) + geom_line(data=fitted.data)
-                          
+# Importing your data
+dataset <- read.table(text='
+    x   y
+1   0 123
+2   2 116
+3   4 113
+4  15 100
+5  48  87
+6  75  84
+7 122  77', header=T)
+
+
+y.hat <- predict(lm(y~x+I(x^2)+I(x^3), data=dataset)) 
+
+qplot(x, y, data=dataset, geom="line") 
+last_plot() + geom_line(aes(x=x, y=y.hat), col=2) 
+lm(y~I+I(x^2)+I(x^3),data=dataset)
+
