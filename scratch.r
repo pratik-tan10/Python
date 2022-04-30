5e1f99889b053e45b734a89740b990a290f2becf
Pratik Dhungana <pratik.tan10@gmail.com>
2022-04-29 23:00:43 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,12 +1,14 @@
-a<-"C:/Users/12058/Desktop/dv.txt"
-heading <- read.table("dv.txt",skip=36 header = TRUE, sep ="\t", stringsAsFactors = FALSE)
-data<- read.table("dv.txt",header = TRUE, sep = "\t", stringsAsFactors = FALSE, skip = 37)
-heading<- read.table("dv.txt",header = TRUE, sep = "\t", stringsAsFactors = FALSE, skip = 35, nrows=1)
-nam<-c("agency_cd","site_np","dateime",	"gage_max",	"gage_max_cd",	"gage_mean",	"gage_mean_cd",	"tid_max",	"tid_max_cd",	"tid_min",	"tid_min_cd",	"tid_mean",	"tid_mean_cd",	"gage_min",	"gage_min_cd",	"vel_mean",	"vel_mean_cd",	"vel_min",	"vel_min_cd",	"vel_max",	"vel_max_cd")
-colnames(data)<-nam
-lm(vel_mean~gage_mean, data= data)->lmx
-summary(lmx)
-plot(lmx,pch=19,col="blue",chx = 2)
-plot(tid_mean~gage_mean,pch=19,col="blue",chx = 2)
-abline(lmx,col="green")
-
+mydata<-data.frame(x=c(0,2,4,15,48,75,122),y=c(123,116,113,100,87,84,77))
+library(ggplot2)
+qplot(x,y,data=mydata,geom="line")
+f <- function(x, a, b) {
+  a * exp(b *-x)
+}
+x <- seq(0:100)
+y <- f(seq(0:100), 1,1)
+qplot(x,y, geom="line")
+fit <- nls(y~f(x,a,b,c), data=data.frame(mydata), start=list(a=1, b=30, c=-0.3))
+x <- seq(0,120)
+fitted.data <- data.frame(x=x, y=predict(fit, list(x=x)))
+                          ggplot(mydata, aes(x, y)) + geom_point(color="red", alpha=.5) + geom_line(alpha=.5) + geom_line(data=fitted.data)
+                          
