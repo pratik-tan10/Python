07a6a5d618ef9da3122a0c7ee480fb0537c8aa3e
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-02 18:41:47 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -5,25 +5,121 @@ arcpy.env.workspace = "C:/Users/Research Lab/BoxPratik/Box Sync/Pratik/sem2/Thes
 arcpy.FeatureToPoint_management("main.al_tuscaloosa", "parcels_center.shp", 
                                 "INSIDE")
 ###################################
-# Load libraries
-from sklearn.linear_model import LogisticRegression
-from sklearn import datasets
-from sklearn.preprocessing import StandardScaler
-# Load data
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-# Standarize features
-scaler = StandardScaler()
-X_std = scaler.fit_transform(X)
-# Create one-vs-rest logistic regression object
-clf = LogisticRegression(random_state=0, multi_class='ovr')
+# IMPORTING DATA
 
-# Train model
-model = clf.fit(X_std, y)
-# Create new observation
-new_observation = [[.5, .5, .5, .5]]
-# Predict class
-model.predict(new_observation)
-# View predicted probabilities
-model.predict_proba(new_observation)
+df = pd.read_csv('house.csv')
+df.set_index('Id', inplace = True)
+
+df.head(5)
+df.dropna(inplace = True)
+print(cl(df.isnull().sum(), attrs = ['bold']))
+df.describe()
+print(cl(df.dtypes, attrs = ['bold']))
+
+df['MasVnrArea'] = pd.to_numeric(df['MasVnrArea'], errors = 'coerce')
+df['MasVnrArea'] = df['MasVnrArea'].astype('int64')
+print(cl(df.dtypes, attrs = ['bold']))
+
+# 1. Heatmap
+sb.heatmap(df.corr(), annot = True, cmap = 'magma')
+plt.savefig('heatmap.png')
+plt.show()
+
+# 2. Scatter plot
+
+def scatter_df(y_var):
+    scatter_df = df.drop(y_var, axis = 1)
+    i = df.columns
+    
+    plot1 = sb.scatterplot(i[0], y_var, data = df, color = 'orange', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[0]), fontsize = 16)
+    plt.xlabel('{}'.format(i[0]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter1.png')
+    plt.show()
+    
+    plot2 = sb.scatterplot(i[1], y_var, data = df, color = 'yellow', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[1]), fontsize = 16)
+    plt.xlabel('{}'.format(i[1]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter2.png')
+    plt.show()
+    
+    plot3 = sb.scatterplot(i[2], y_var, data = df, color = 'aquamarine', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[2]), fontsize = 16)
+    plt.xlabel('{}'.format(i[2]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter3.png')
+    plt.show()
+    
+    plot4 = sb.scatterplot(i[3], y_var, data = df, color = 'deepskyblue', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[3]), fontsize = 16)
+    plt.xlabel('{}'.format(i[3]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter4.png')
+    plt.show()
+    
+    plot5 = sb.scatterplot(i[4], y_var, data = df, color = 'crimson', edgecolor = 'white', s = 150)
+    plt.title('{} / Sale Price'.format(i[4]), fontsize = 16)
+    plt.xlabel('{}'.format(i[4]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter5.png')
+    plt.show()
+    
+    plot6 = sb.scatterplot(i[5], y_var, data = df, color = 'darkviolet', edgecolor = 'white', s = 150)
+    plt.title('{} / Sale Price'.format(i[5]), fontsize = 16)
+    plt.xlabel('{}'.format(i[5]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter6.png')
+    plt.show()
+    
+    plot7 = sb.scatterplot(i[6], y_var, data = df, color = 'khaki', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[6]), fontsize = 16)
+    plt.xlabel('{}'.format(i[6]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter7.png')
+    plt.show()
+    
+    plot8 = sb.scatterplot(i[7], y_var, data = df, color = 'gold', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[7]), fontsize = 16)
+    plt.xlabel('{}'.format(i[7]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter8.png')
+    plt.show()
+    
+    plot9 = sb.scatterplot(i[8], y_var, data = df, color = 'r', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[8]), fontsize = 16)
+    plt.xlabel('{}'.format(i[8]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter9.png')
+    plt.show()
+    
+    plot10 = sb.scatterplot(i[9], y_var, data = df, color = 'deeppink', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[9]), fontsize = 16)
+    plt.xlabel('{}'.format(i[9]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter10.png')
+    plt.show()
+    
+scatter_df('SalePrice')
+
