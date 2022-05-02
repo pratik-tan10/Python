29e78d3b60a876ab25ef959f10c2f898f9b7d83c
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-02 18:42:28 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -123,3 +123,58 @@ def scatter_df(y_var):
     
 scatter_df('SalePrice')
 
+# 3. Distribution plot
+
+sb.distplot(df['SalePrice'], color = 'r')
+plt.title('Sale Price Distribution', fontsize = 16)
+plt.xlabel('Sale Price', fontsize = 14)
+plt.ylabel('Frequency', fontsize = 14)
+plt.xticks(fontsize = 12)
+plt.yticks(fontsize = 12)
+
+plt.savefig('distplot.png')
+plt.show()
+
+# FEATURE SELECTION & DATA SPLIT
+
+X_var = df[['LotArea', 'MasVnrArea', 'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF']].values
+y_var = df['SalePrice'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X_var, y_var, test_size = 0.2, random_state = 0)
+
+print(cl('X_train samples : ', attrs = ['bold']), X_train[0:5])
+print(cl('X_test samples : ', attrs = ['bold']), X_test[0:5])
+print(cl('y_train samples : ', attrs = ['bold']), y_train[0:5])
+print(cl('y_test samples : ', attrs = ['bold']), y_test[0:5])
+# MODELING
+
+# 1. OLS
+
+ols = LinearRegression()
+ols.fit(X_train, y_train)
+ols_yhat = ols.predict(X_test)
+
+# 2. Ridge
+
+ridge = Ridge(alpha = 0.5)
+ridge.fit(X_train, y_train)
+ridge_yhat = ridge.predict(X_test)
+
+# 3. Lasso
+
+lasso = Lasso(alpha = 0.01)
+lasso.fit(X_train, y_train)
+lasso_yhat = lasso.predict(X_test)
+
+# 4. Bayesian
+
+bayesian = BayesianRidge()
+bayesian.fit(X_train, y_train)
+bayesian_yhat = bayesian.predict(X_test)
+
+# 5. ElasticNet
+
+en = ElasticNet(alpha = 0.01)
+en.fit(X_train, y_train)
+en_yhat = en.predict(X_test)
+
