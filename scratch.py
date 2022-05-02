6352671b1e66df1fcbc6c200a3686082a170e0ae
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-01 19:57:38 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -5,36 +5,16 @@ arcpy.env.workspace = "C:/Users/Research Lab/BoxPratik/Box Sync/Pratik/sem2/Thes
 arcpy.FeatureToPoint_management("main.al_tuscaloosa", "parcels_center.shp", 
                                 "INSIDE")
 ###################################
-import pandas as pd
-# load data file
-df = pd.read_csv("C/anova/test.txt", sep="\t")
-# reshape the d dataframe suitable for statsmodels package 
-df_melt = pd.melt(df.reset_index(), id_vars=['index'], value_vars=['A', 'B', 'C', 'D'])
-# replace column names
-df_melt.columns = ['index', 'treatments', 'value']
-
-# generate a boxplot to see the data distribution by treatments. Using boxplot, we can 
-# easily detect the differences between different treatments
-import matplotlib.pyplot as plt
-import seaborn as sns
-ax = sns.boxplot(x='treatments', y='value', data=df_melt, color='#99c2a2')
-ax = sns.swarmplot(x="treatments", y="value", data=df_melt, color='#7d0013')
-
-import scipy.stats as stats
-# stats f_oneway functions takes the groups as input and returns ANOVA F and p value
-fvalue, pvalue = stats.f_oneway(df['A'], df['B'], df['C'], df['D'])
-print(fvalue, pvalue)
-# 17.492810457516338 2.639241146210922e-05
-
-# get ANOVA table as R like output
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-
-# Ordinary Least Squares (OLS) model
-model = ols('value ~ C(treatments)', data=df_melt).fit()
-anova_table = sm.stats.anova_lm(model, typ=2)
-anova_table
-from bioinfokit.analys import stat
-res = stat()
-res.anova_stat(df=df_melt, res_var='value', anova_model='value ~ C(treatments)')
-res.anova_summary
+# Load libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+# Load data
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+# Standarize features
+scaler = StandardScaler()
+X_std = scaler.fit_transform(X)
+# Create one-vs-rest logistic regression object
+clf = LogisticRegression(random_state=0, multi_class='ovr')
