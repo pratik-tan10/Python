2255046ece5a813074a24422e5a78b8b14cd99f2
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-01 19:58:14 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -18,3 +18,12 @@ scaler = StandardScaler()
 X_std = scaler.fit_transform(X)
 # Create one-vs-rest logistic regression object
 clf = LogisticRegression(random_state=0, multi_class='ovr')
+
+# Train model
+model = clf.fit(X_std, y)
+# Create new observation
+new_observation = [[.5, .5, .5, .5]]
+# Predict class
+model.predict(new_observation)
+# View predicted probabilities
+model.predict_proba(new_observation)
