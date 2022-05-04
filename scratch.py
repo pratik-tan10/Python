d85a28312b3e7df1cae3759afa0efeca604424d8
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-03 22:47:55 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -7,174 +7,18 @@ arcpy.FeatureToPoint_management("main.al_tuscaloosa", "parcels_center.shp",
 ###################################
 # IMPORTING DATA
 
-df = pd.read_csv('house.csv')
-df.set_index('Id', inplace = True)
+import numpy as np
+from scipy import misc
+import matplotlib.pyplot as plt
 
-df.head(5)
-df.dropna(inplace = True)
-print(cl(df.isnull().sum(), attrs = ['bold']))
-df.describe()
-print(cl(df.dtypes, attrs = ['bold']))
+pic=misc.face() #reading an image from misc in scipy
+plt.imshow(pic) #displaying the image using imshow() function in matplot
+pic=misc.imread(location_of_image) 
+misc.imsave(‘picture_name_to_be_stored’,pic) #here pic is the name of the variable holding the image
 
-df['MasVnrArea'] = pd.to_numeric(df['MasVnrArea'], errors = 'coerce')
-df['MasVnrArea'] = df['MasVnrArea'].astype('int64')
-print(cl(df.dtypes, attrs = ['bold']))
-
-# 1. Heatmap
-sb.heatmap(df.corr(), annot = True, cmap = 'magma')
-plt.savefig('heatmap.png')
-plt.show()
-
-# 2. Scatter plot
-
-def scatter_df(y_var):
-    scatter_df = df.drop(y_var, axis = 1)
-    i = df.columns
+path = glob.glob("D:/New folder/*.png") #storing the location of all the images in variable path
+imgs = [] #creating an empty list
+for img in path: #running a loop to iterate through every image in the file
+    pic = plt.imread(img) #reading the image using matplotlib
+    imgs.append(pic) #adding the image to the list
     
-    plot1 = sb.scatterplot(i[0], y_var, data = df, color = 'orange', edgecolor = 'b', s = 150)
-    plt.title('{} / Sale Price'.format(i[0]), fontsize = 16)
-    plt.xlabel('{}'.format(i[0]), fontsize = 14)
-    plt.ylabel('Sale Price', fontsize = 14)
-    plt.xticks(fontsize = 12)
-    plt.yticks(fontsize = 12)
-    plt.savefig('scatter1.png')
-    plt.show()
-    
-    plot2 = sb.scatterplot(i[1], y_var, data = df, color = 'yellow', edgecolor = 'b', s = 150)
-    plt.title('{} / Sale Price'.format(i[1]), fontsize = 16)
-    plt.xlabel('{}'.format(i[1]), fontsize = 14)
-    plt.ylabel('Sale Price', fontsize = 14)
-    plt.xticks(fontsize = 12)
-    plt.yticks(fontsize = 12)
-    plt.savefig('scatter2.png')
-    plt.show()
-    
-    plot3 = sb.scatterplot(i[2], y_var, data = df, color = 'aquamarine', edgecolor = 'b', s = 150)
-    plt.title('{} / Sale Price'.format(i[2]), fontsize = 16)
-    plt.xlabel('{}'.format(i[2]), fontsize = 14)
-    plt.ylabel('Sale Price', fontsize = 14)
-    plt.xticks(fontsize = 12)
-    plt.yticks(fontsize = 12)
-    plt.savefig('scatter3.png')
-    plt.show()
-    
-    plot4 = sb.scatterplot(i[3], y_var, data = df, color = 'deepskyblue', edgecolor = 'b', s = 150)
-    plt.title('{} / Sale Price'.format(i[3]), fontsize = 16)
-    plt.xlabel('{}'.format(i[3]), fontsize = 14)
-    plt.ylabel('Sale Price', fontsize = 14)
-    plt.xticks(fontsize = 12)
-    plt.yticks(fontsize = 12)
-    plt.savefig('scatter4.png')
-    plt.show()
-    
-    plot5 = sb.scatterplot(i[4], y_var, data = df, color = 'crimson', edgecolor = 'white', s = 150)
-    plt.title('{} / Sale Price'.format(i[4]), fontsize = 16)
-    plt.xlabel('{}'.format(i[4]), fontsize = 14)
-    plt.ylabel('Sale Price', fontsize = 14)
-    plt.xticks(fontsize = 12)
-    plt.yticks(fontsize = 12)
-    plt.savefig('scatter5.png')
-    plt.show()
-    
-    plot6 = sb.scatterplot(i[5], y_var, data = df, color = 'darkviolet', edgecolor = 'white', s = 150)
-    plt.title('{} / Sale Price'.format(i[5]), fontsize = 16)
-    plt.xlabel('{}'.format(i[5]), fontsize = 14)
-    plt.ylabel('Sale Price', fontsize = 14)
-    plt.xticks(fontsize = 12)
-    plt.yticks(fontsize = 12)
-    plt.savefig('scatter6.png')
-    plt.show()
-    
-    plot7 = sb.scatterplot(i[6], y_var, data = df, color = 'khaki', edgecolor = 'b', s = 150)
-    plt.title('{} / Sale Price'.format(i[6]), fontsize = 16)
-    plt.xlabel('{}'.format(i[6]), fontsize = 14)
-    plt.ylabel('Sale Price', fontsize = 14)
-    plt.xticks(fontsize = 12)
-    plt.yticks(fontsize = 12)
-    plt.savefig('scatter7.png')
-    plt.show()
-    
-    plot8 = sb.scatterplot(i[7], y_var, data = df, color = 'gold', edgecolor = 'b', s = 150)
-    plt.title('{} / Sale Price'.format(i[7]), fontsize = 16)
-    plt.xlabel('{}'.format(i[7]), fontsize = 14)
-    plt.ylabel('Sale Price', fontsize = 14)
-    plt.xticks(fontsize = 12)
-    plt.yticks(fontsize = 12)
-    plt.savefig('scatter8.png')
-    plt.show()
-    
-    plot9 = sb.scatterplot(i[8], y_var, data = df, color = 'r', edgecolor = 'b', s = 150)
-    plt.title('{} / Sale Price'.format(i[8]), fontsize = 16)
-    plt.xlabel('{}'.format(i[8]), fontsize = 14)
-    plt.ylabel('Sale Price', fontsize = 14)
-    plt.xticks(fontsize = 12)
-    plt.yticks(fontsize = 12)
-    plt.savefig('scatter9.png')
-    plt.show()
-    
-    plot10 = sb.scatterplot(i[9], y_var, data = df, color = 'deeppink', edgecolor = 'b', s = 150)
-    plt.title('{} / Sale Price'.format(i[9]), fontsize = 16)
-    plt.xlabel('{}'.format(i[9]), fontsize = 14)
-    plt.ylabel('Sale Price', fontsize = 14)
-    plt.xticks(fontsize = 12)
-    plt.yticks(fontsize = 12)
-    plt.savefig('scatter10.png')
-    plt.show()
-    
-scatter_df('SalePrice')
-
-# 3. Distribution plot
-
-sb.distplot(df['SalePrice'], color = 'r')
-plt.title('Sale Price Distribution', fontsize = 16)
-plt.xlabel('Sale Price', fontsize = 14)
-plt.ylabel('Frequency', fontsize = 14)
-plt.xticks(fontsize = 12)
-plt.yticks(fontsize = 12)
-
-plt.savefig('distplot.png')
-plt.show()
-
-# FEATURE SELECTION & DATA SPLIT
-
-X_var = df[['LotArea', 'MasVnrArea', 'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF']].values
-y_var = df['SalePrice'].values
-
-X_train, X_test, y_train, y_test = train_test_split(X_var, y_var, test_size = 0.2, random_state = 0)
-
-print(cl('X_train samples : ', attrs = ['bold']), X_train[0:5])
-print(cl('X_test samples : ', attrs = ['bold']), X_test[0:5])
-print(cl('y_train samples : ', attrs = ['bold']), y_train[0:5])
-print(cl('y_test samples : ', attrs = ['bold']), y_test[0:5])
-# MODELING
-
-# 1. OLS
-
-ols = LinearRegression()
-ols.fit(X_train, y_train)
-ols_yhat = ols.predict(X_test)
-
-# 2. Ridge
-
-ridge = Ridge(alpha = 0.5)
-ridge.fit(X_train, y_train)
-ridge_yhat = ridge.predict(X_test)
-
-# 3. Lasso
-
-lasso = Lasso(alpha = 0.01)
-lasso.fit(X_train, y_train)
-lasso_yhat = lasso.predict(X_test)
-
-# 4. Bayesian
-
-bayesian = BayesianRidge()
-bayesian.fit(X_train, y_train)
-bayesian_yhat = bayesian.predict(X_test)
-
-# 5. ElasticNet
-
-en = ElasticNet(alpha = 0.01)
-en.fit(X_train, y_train)
-en_yhat = en.predict(X_test)
-
