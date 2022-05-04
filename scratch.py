ec4abf77713d88a0ce66ca1a4538b85a2c2314cc
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-04 18:47:29 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -7,31 +7,18 @@ arcpy.FeatureToPoint_management("main.al_tuscaloosa", "parcels_center.shp",
 ###################################
 # IMPORTING DATA
 
-import numpy as np
-from scipy import misc
-import matplotlib.pyplot as plt
+uri = '/Users/ep9k/Desktop/TestZipCodes/TestZipCodes.shp'
+join_layer = iface.addVectorLayer(uri, 'Patients by Zip Code', 'ogr')	#adds layer to map
+target_field = 'PatCNT'
 
-pic=misc.face() #reading an image from misc in scipy
-plt.imshow(pic) #displaying the image using imshow() function in matplot
-pic=misc.imread(location_of_image) 
-misc.imsave(‘picture_name_to_be_stored’,pic) #here pic is the name of the variable holding the image
+def calculate_attributes():
+    """Calculates values for 'PatCNT' by copying attributes from Patient_Da column
+    and adds them to 'PatCNT' column in US Zip Codes table"""
 
-path = glob.glob("D:/New folder/*.png") #storing the location of all the images in variable path
-imgs = [] #creating an empty list
-for img in path: #running a loop to iterate through every image in the file
-    pic = plt.imread(img) #reading the image using matplotlib
-    imgs.append(pic) #adding the image to the list
-    
-pic=misc.face(gray=True) # getting the image in grayscale format
-type(pic)
-l=int(len(pic)/4)
-crop_pic=pic[l:3*l,2*l:4*l] #taking oly some values of the matrix pic
-plt.imshow(crop_pic,cmap='gray')
-pic=misc.face(gray=True)
-print("Mean:",pic.mean())
-print("Max:",pic.max())
-print("Min:",pic.min())
-from scipy import ndimage
+    with edit(join_layer):
+        for feature in join_layer.getFeatures():
+            feature.setAttribute(feature.fieldNameIndex('PatCNT'), feature['Patient_Da'])
+            join_layer.updateFeature(feature)
+    print(f"Attribute calculated for {target_field} field")
 
-rot_pic=ndimage.rotate(pic,45)
-plt.imshow(rot_pic,cmap='gray')
+calculate_attributes()
