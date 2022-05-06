f1a12a1ea661641927616c86fa676db3fcd6dec3
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-06 12:52:51 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -1,21 +1,101 @@
-library(ggplot2)
-set.seed(2021)
-coffee <- data.frame(grams = c(rnorm(12500, 12.5, 2), runif(7500, 10, 15)))
-coffee$grams <- ifelse(coffee$grams < 8, 8, coffee$grams)
-ggplot(coffee, aes(x = grams)) +
-  geom_histogram(fill = 'cadetblue', bins = 50) +
-  geom_vline(xintercept = mean(coffee$grams), color = 'gold') +
-  labs(title = 'Distribution of Grams of Coffee Used Across All 20,000 Forum Members',
-       y = 'Number of People', x = 'Grams of Coffee')
-# Manually calculate population SD, as sd() function divides by n - 1 instead of n
-pop_sd <- sqrt(sum((coffee$grams - mean(coffee$grams))^2) / nrow(coffee))
-# Note that 12.52 and 0.18 are the rounded mean and SE
-se <- pop_sd / sqrt(100)
-lo <- mean(coffee$grams) - (1.96 * se)
-hi <- mean(coffee$grams) + (1.96 * se)
-interval <- c(lo, hi)
-interval
-## [1] 12.16489 12.87077
+shift <- function(x, lag) {
+  n <- length(x)
+  xnew <- rep(NA, n)
+  if (lag < 0) {
+    xnew[1:(n-abs(lag))] <- x[(abs(lag)+1):n]
+  } else if (lag > 0) {
+    xnew[(lag+1):n] <- x[1:(n-lag)]
+  } else {
+    xnew <- x
+  }
+  return(xnew)
+}
 
-# Or, shortcut the steps above with:
-# interval <- qnorm(c(.025, .975), mean = mean(coffee$grams), sd = (pop_sd / sqrt(100)))
+
+f<-function(df,d=7){
+  b<-0*seq_along(df$day)
+  for (i in seq_along(df$day)){
+    b[i]<-mean(df$cases[max(i-d+1,1):i])
+  }
+  plot(df$day,df$cases,type="l",col="blue",xlab = "date",ylab="Number of New Cases")
+  points(df$day,b,type="l",col="red")
+}
+
+
+#33333333333333333
+download.file("https://covid19.who.int/WHO-COVID-19-global-data.csv", destfile="WHO-COVID-19-global-data.csv")
+mcsv<-read.csv("WHO-COVID-19-global-data.csv")
+head(mcsv)
+library(tidyr)
+
+gather(mcsv,key="cases",value="number",-c(1:4))->mcsv2
+spread(mcsv2,ï..Date_reported,number)->mcsv3
+
+for (jj in (4+1:(dim(mcsv3)[2]-4))){
+  if(jj==5) mm<-tapply(mcsv3[,jj],mcsv3$cases,sum)
+  else mm<- cbind(mm,tapply(mcsv3[,jj],mcsv3$cases,sum))
+}
+
+cumulative_cases<-subset(mcsv3, cases=="Cumulative_cases")
+cumulative_deaths<-subset(mcsv3, cases=="Cumulative_deaths")
+New_cases<-subset(mcsv3, cases=="New_cases")
+nco<-New_cases[1,5:dim(New_cases)[2]]
+apply(nco,2,sum)->ncw
+New_cases[nrow(New_cases) + 1,1:4] <- c("WD","World","All","New_cases")
+New_cases[nrow(New_cases),5:dim(New_cases)[2]] <- ncw
+New_deaths<-subset(mcsv3, cases=="New_deaths")
+
+
+# Download the shapefile. (note that I store it in a folder called DATA. You have to change that if needed.)
+download.file("http://thematicmapping.org/downloads/TM_WORLD_BORDERS_SIMPL-0.3.zip" , destfile="world_shape_file.zip")
+# You now have it in your current working directory, have a look!
+
+# Unzip this file. You can do it with R (as below), or clicking on the object you downloaded.
+system("unzip world_shape_file.zip")
+#  -- > You now have 4 files. One of these files is a .shp file! (TM_WORLD_BORDERS_SIMPL-0.3.shp)
+
+# Read this shape file with the rgdal library. 
+library(rgdal)
+world_spdf <- readOGR( 
+  dsn= paste0(getwd(),"/world_shape_file") , 
+  layer="TM_WORLD_BORDERS_SIMPL-0.3",
+  verbose=FALSE
+)
+#Get dataframe
+world_spdf@data->x
+#Change colnames
+colnames(x)[2]<-colnames(New_cases)[1]
+world_spdf@data= x %>% merge(New_cases, by="Country_code")
+colnames(world_spdf@data)<-make.names(colnames(world_spdf@data))
+
+# Library for map
+library(leaflet)
+#Color Brewing
+library(RColorBrewer)
+partdata<-world_spdf@data$X2021.02.07
+mybins<-quantile(partdata,seq(0,1,length=7),na.rm=TRUE)
+y<-mybins[duplicated(mybins)]
+mybins<-c(y, mybins[!mybins %in% y])
+mypalette <- colorBin( palette="YlOrBr", domain=world_spdf@data$X2021.02.07, na.color="transparent", bins=mybins)
+# Basic choropleth with leaflet?
+
+leaflet(world_spdf) %>% 
+  addTiles()  %>% 
+  setView( lat=10, lng=0 , zoom=2) %>%
+  addPolygons( 
+    fillColor = ~mypalette(partdata), 
+    stroke=TRUE, 
+    fillOpacity = 0.9, 
+    color="white", 
+    weight=0.3,
+  ) %>%
+  addLegend( pal=mypalette, values=~partdata, opacity=0.9, title = "New Covid Cases", position = "bottomleft" )
+
+
+#To convert valid colnames to date
+as.Date(substring("X2020.01.02",2,11),"%Y.%m.%d")
+t(mcsv3[5,5:dim(mcsv3)[2]])->ax3t
+data.frame(day = as.Date(rownames(ax3t),"%Y-%m-%d"),cases=ax3t[,1])->ax3t
+f(ax3t)
+
+colnames(mcsv3)<-cn
