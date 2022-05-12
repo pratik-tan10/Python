a859d7a12876c06c6a967ff180f7774837b53295
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-12 16:15:35 -0500

    Update scratch.sql

--- DB/scratch.sql
+++ DB/scratch.sql
@@ -9,22 +9,22 @@ with home as (
 -- Set up the away team CTE
 away as (
   SELECT m.id, t.team_long_name,
-	  CASE WHEN m.home_goal > m.away_goal THEN 'MU Win'
-		   WHEN m.home_goal < m.away_goal THEN 'MU Loss' 
+	  CASE WHEN m.home_goal > m.away_goal THEN 'MU Loss'
+		   WHEN m.home_goal < m.away_goal THEN 'MU Win' 
   		   ELSE 'Tie' END AS outcome
   FROM match AS m
   LEFT JOIN team AS t ON m.awayteam_id = t.team_api_id)
--- Select team names, the date and goals
+-- Select columns and and rank the matches by goal difference
 SELECT DISTINCT
-    m.date,
+    date,
     home.team_long_name AS home_team,
     away.team_long_name AS away_team,
-    m.home_goal,
-    m.away_goal
+    m.home_goal, m.away_goal,
+    rank() over(order by ABS(home_goal - away_goal) desc) as match_rank
 -- Join the CTEs onto the match table
 FROM match AS m
 LEFT JOIN home ON m.id = home.id
 LEFT JOIN away ON m.id = away.id
 WHERE m.season = '2014/2015'
-      AND (home.team_long_name = 'Manchester United' 
-           OR away.team_long_name = 'Manchester United');
+      AND ((home.team_long_name = 'Manchester United' AND home.outcome = 'MU Loss')
+      OR (away.team_long_name = 'Manchester United' AND away.outcome = 'MU Loss'));
