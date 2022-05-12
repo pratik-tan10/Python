f38849034a2ffdb8ba9667c7c388ac39c934f220
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-11 20:58:33 -0500

    Update scratch.sql

--- DB/scratch.sql
+++ DB/scratch.sql
@@ -11,3 +11,36 @@ CREATE TABLE Persons (
 );
 INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
 VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+
+CREATE TABLE sales.promotions (
+    promotion_id INT PRIMARY KEY IDENTITY (1, 1),
+    promotion_name VARCHAR (255) NOT NULL,
+    discount NUMERIC (3, 2) DEFAULT 0,
+    start_date DATE NOT NULL,
+    expired_date DATE NOT NULL
+); 
+INSERT INTO sales.promotions (
+    promotion_name,
+    discount,
+    start_date,
+    expired_date
+)
+VALUES
+    (
+        '2019 Summer Promotion',
+        0.15,
+        '20190601',
+        '20190901'
+    ),
+    (
+        '2019 Fall Promotion',
+        0.20,
+        '20191001',
+        '20191101'
+    ),
+    (
+        '2019 Winter Promotion',
+        0.25,
+        '20191201',
+        '20200101'
+    );
