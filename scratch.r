bca4f734f06af05a6e64558e68d28475c7dbe7a5
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-14 21:49:02 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -132,18 +132,33 @@ colnames(mcsv3)<-cn
 mysqlconnection = dbConnect(MySQL(), user = 'root', password = '', dbname = 'sakila',
    host = 'localhost')
 
-# List the tables available in this database.
+# Get list of available tables
 dbListTables(mysqlconnection)
 # Query the "actor" tables to get all the rows.
 result = dbSendQuery(mysqlconnection, "select * from actor")
 
-# Store the result in a R data frame object. n = 5 is used to fetch first 5 rows.
+# Fetch first 5 rows
 data.frame = fetch(result, n = 5)
 print(data.fame)
 
 result = dbSendQuery(mysqlconnection, "select * from actor where last_name = 'TORN'")
 
-# Fetch all the records(with n = -1) and store it as a data frame.
+#Fetch records
 data.frame = fetch(result, n = -1)
 print(data)
+
+dbSendQuery(mysqlconnection, "update mtcars set disp = 168.5 where hp = 110")
+dbSendQuery(mysqlconnection,
+   "insert into mtcars(row_names, mpg, cyl, disp, hp, drat, wt, qsec, vs, am, gear, carb)
+   values('New Mazda RX4 Wag', 21, 6, 168.5, 110, 3.9, 2.875, 17.02, 0, 1, 4, 4)"
+)
+
+#create table
+mysqlconnection = dbConnect(MySQL(), user = 'root', password = '', dbname = 'sakila', 
+   host = 'localhost')
+
+dbWriteTable(mysqlconnection, "mtcars", mtcars[, ], overwrite = TRUE)
+
+#droping tables
+dbSendQuery(mysqlconnection, 'drop table if exists mtcars')
 
