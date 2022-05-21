1d0b9636ce7d681d2e23ec174606f10e8422bbad
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-20 19:15:59 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -129,16 +129,22 @@ colnames(mcsv3)<-cn
 
 
 ##############################
-# From previous step
+# Update the data frame
 prediction_data <- explanatory_data %>% 
   mutate(   
     has_churned = predict(mdl_churn_vs_relationship, explanatory_data, type = "response"),
-    odds_ratio = has_churned / (1 - has_churned)
+    odds_ratio = has_churned / (1 - has_churned),
+    # Add the log odds ratio from odds_ratio
+    log_odds_ratio = log(odds_ratio),
+    # Add the log odds ratio using predict()
+    log_odds_ratio2 = predict(mdl_churn_vs_relationship, explanatory_data)
   )
 
-# Using prediction_data, plot odds_ratio vs. time_since_first_purchase
-prediction_data%>%ggplot(aes(time_since_first_purchase ,odds_ratio)) +
-  # Make it a line plot
+# See the result
+prediction_data
+#plot
+ggplot(prediction_data, aes(time_since_first_purchase, odds_ratio)) +
   geom_line() +
-  # Add a dotted horizontal line at y = 1
-  geom_hline(yintercept=1,linetype="dotted")
+  geom_hline(yintercept = 1, linetype = "dotted") +
+  # Use a logarithmic y-scale
+  scale_y_log10()
