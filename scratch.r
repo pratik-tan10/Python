fee4b9e5b0b33aae9af5f5e66f53e65cfe7f9da0
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-25 22:29:55 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -173,3 +173,36 @@ sort(cut_oes)
 # Plot the relationship between mean_salary and year and color the lines by the assigned cluster
 ggplot(gathered_oes, aes(x = year, y = mean_salary, color = factor(cluster))) + 
     geom_line(aes(group = occupation))
+# Use map_dbl to run many models with varying value of k (centers)
+tot_withinss <- map_dbl(1:10,  function(k){
+  model <- kmeans(x = oes, centers = k)
+  model$tot.withinss
+})
+
+# Generate a data frame containing both k and tot_withinss
+elbow_df <- data.frame(
+  k = 1:10,
+  tot_withinss = tot_withinss
+)
+
+# Plot the elbow plot
+ggplot(elbow_df, aes(x = k, y = tot_withinss)) +
+  geom_line() +
+  scale_x_continuous(breaks = 1:10)
+# Use map_dbl to run many models with varying value of k
+sil_width <- map_dbl(2:10,  function(k){
+  model <- pam(oes, k = k)
+  model$silinfo$avg.width
+})
+
+# Generate a data frame containing both k and sil_width
+sil_df <- data.frame(
+  k = 2:10,
+  sil_width = sil_width
+)
+
+# Plot the relationship between k and sil_width
+ggplot(sil_df, aes(x = k, y = sil_width)) +
+  geom_line() +
+  scale_x_continuous(breaks = 2:10)
+
