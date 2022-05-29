2a5c6a2535b406964b80da049aa9ce62daac0695
Pratik Dhungana <pratik.tan10@gmail.com>
2022-05-28 20:27:46 -0500

    Update scratch.r

--- scratch.r
+++ scratch.r
@@ -129,115 +129,37 @@ colnames(mcsv3)<-cn
 
 
 ##############################
-# ca_geo has been pre-defined
-str(ca_geo, 1)
+library(leaflet.extras)
 
-# See what measurements are at each location
-names(ca_geo)
+leaflet() %>%
+  addTiles() %>% 
+  addSearchOSM() %>% 
+  addReverseSearchOSM() 
+m2 <- ipeds %>% 
+        leaflet() %>% 
+            # use the CartoDB provider tile
+            addProviderTiles("CartoDB") %>% 
+            # center on the middle of the US with zoom of 3
+            setView(lat = 39.8282, lng = -98.5795, zoom= 3)
 
-# Get a summary of the acidity (pH) values
-summary(ca_geo$pH)
+# Map all American colleges 
+m2 %>% 
+    addCircleMarkers() 
+pal <- colorFactor(palette = c("red", "blue", "#9b4a11"), 
+                   levels = c("Public", "Private", "For-Profit"))
 
-# Look at the distribution
-hist(ca_geo$pH)
+m2 %>% 
+    addCircleMarkers(radius = 2, label = ~name, color = ~pal(sector_label))
+# Load the htmltools package
+library(htmltools)
 
-# Make a vector that is TRUE for the missing data
-miss <- is.na(ca_geo$pH)
-table(miss)
+# Create data frame called public with only public colleges
+public <- filter(ipeds, sector_label == "Public")  
 
-# Plot a map of acidity
-spplot(ca_geo[!miss, ], "pH")
-# ca_geo has been pre-defined
-str(ca_geo, 1)
+# Create a leaflet map of public colleges called m3 
+m3 <- leaflet() %>% 
+        addProviderTiles("CartoDB") %>% 
+        addCircleMarkers(data = public, radius = 2, label = ~htmlEscape(name),
+                         color = ~pal(sector_label), group = "Public")
 
-# Are they called lat-long, up-down, or what?
-coordnames(ca_geo)
-
-# Complete the formula
-m_trend <- lm(pH ~ x + y, as.data.frame(ca_geo))
-
-# Check the coefficients
-summary(m_trend)
-# ca_geo, miss, m_trend have been pre-defined
-ls.str()
-
-# Make a vector that is TRUE for the missing data
-miss <- is.na(ca_geo$pH)
-
-# Create a data frame of missing data
-ca_geo_miss <- as.data.frame(ca_geo)[miss, ]
-
-# Predict pH for the missing data
-predictions <- predict(m_trend, newdata = ca_geo_miss, se.fit = TRUE)
-
-# Compute the exceedance probability
-pAlkaline <- 1 - pnorm(7, mean = predictions$fit, sd = predictions$se.fit)
-hist(pAlkaline)
-# ca_geo, miss have been pre-defined
-ls.str()
-
-# Make a cloud from the non-missing data up to 10km
-plot(variogram(pH ~ 1, ca_geo[!miss, ], cloud = TRUE, cutoff = 10000))
-
-# Make a variogram of the non-missing data
-plot(variogram(pH ~ 1, ca_geo[!miss, ]))
-var(ca_geo$pH, na.rm=T)
-# ca_geo, miss have been pre-defined
-ls.str()
-
-# See what coordinates are called
-coordnames(ca_geo)
-# ca_geo, miss, v_model have been pre-defined
-ls.str()
-
-# Set the trend formula and the new data
-km <- krige(pH ~ x + y, ca_geo[!miss, ], newdata = ca_geo[miss, ], model = v_model)
-names(km)
-
-# Plot the predicted values
-spplot(km, "var1.pred")
-
-# Compute the probability of alkaline samples, and map
-km$pAlkaline <- 1 - pnorm(7, mean = km$var1.pred, sd = sqrt(km$var1.var))
-spplot(km, "pAlkaline")
-# ca_geo, geo_bounds have been pre-defined
-ls.str()
-
-# Plot the polygon and points
-plot(geo_bounds); points(ca_geo)
-
-# Find the corners of the boundary
-bbox(geo_bounds)
-
-# Define a 2.5km square grid over the polygon extent. The first parameter is
-# the bottom left corner.
-grid <- GridTopology(c(537853,5536290), c(2500, 2500), c(72, 48))
-
-# Create points with the same coordinate system as the boundary
-gridpoints <- SpatialPoints(grid, proj4string = CRS(projection(geo_bounds)))
-plot(gridpoints)
-
-# Crop out the points outside the boundary
-cropped_gridpoints <- crop(gridpoints, geo_bounds)
-plot(cropped_gridpoints)
-
-# Convert to SpatialPixels
-spgrid <- SpatialPixels(cropped_gridpoints)
-coordnames(spgrid) <- c("x", "y")
-plot(spgrid)
-# spgrid, v_model have been pre-defined
-ls.str()
-
-# Do kriging predictions over the grid
-ph_grid <- krige(pH ~ x + y, ca_geo[!miss, ], newdata = spgrid, model = v_model)
-
-# Calc the probability of pH exceeding 7
-ph_grid$pAlkaline <- 1 - pnorm(7, mean = ph_grid$var1.pred, sd = sqrt(ph_grid$var1.var))
-
-# Map the probability of alkaline samples
-spplot(ph_grid, zcol = "pAlkaline")
-# The pH depends on the coordinates
-ph_vgm <- variogram(pH ~ x + y, ca_geo[!miss, ])
-plot(ph_vgm)
-nugget <- 0.16
-
+m3
