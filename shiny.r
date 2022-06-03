abb8abb737d696a58c8534d952feace210b661ff
Pratik Dhungana <pratik.tan10@gmail.com>
2022-06-03 18:56:35 -0500

    Update shiny.r

--- shiny.r
+++ shiny.r
@@ -1,30 +1,54 @@
-# MODIFY this app (built in the previous exercise)
 ui <- fluidPage(
-  titlePanel("Most Popular Names"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput('sex', 'Select Sex', c("M", "F")),
-      sliderInput('year', 'Select Year', min = 1880, max = 2017, value = 1900)
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Plot", plotOutput('plot')),
-        tabPanel("Table", tableOutput("table"))
-      )
+  titlePanel("UFO Sightings"),
+  sidebarPanel(
+    selectInput("state", "Choose a U.S. state:", choices = unique(usa_ufo_sightings$state)),
+    dateRangeInput("dates", "Choose a date range:",
+      start = "1920-01-01",
+      end = "1950-01-01"
     )
+  ),
+  # MODIFY CODE BELOW: Create a tab layout for the dashboard
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Plot", plotOutput("shapes")),
+      tabPanel("Table", tableOutput("duration_table"))
+    )
+    
   )
 )
 
-server <- function(input, output, session) {
-  output$plot <- renderPlot({
-    top_names_by_sex_year <- get_top_names(input$year, input$sex) 
-    ggplot(top_names_by_sex_year, aes(x = name, y = prop)) +
-      geom_col()
+server <- function(input, output) {
+  output$shapes <- renderPlot({
+    usa_ufo_sightings %>%
+      filter(
+        state == input$state,
+        date_sighted >= input$dates[1],
+        date_sighted <= input$dates[2]
+      ) %>%
+      ggplot(aes(shape)) +
+      geom_bar() +
+      labs(
+        x = "Shape",
+        y = "# Sighted"
+      )
   })
-  
-  output$table<-renderTable({
-    get_top_names(input$year, input$sex)
+
+  output$duration_table <- renderTable({
+    usa_ufo_sightings %>%
+      filter(
+        state == input$state,
+        date_sighted >= input$dates[1],
+        date_sighted <= input$dates[2]
+      ) %>%
+      group_by(shape) %>%
+      summarize(
+        nb_sighted = n(),
+        avg_duration_min = mean(duration_sec) / 60,
+        median_duration_min = median(duration_sec) / 60,
+        min_duration_min = min(duration_sec) / 60,
+        max_duration_min = max(duration_sec) / 60
+      )
   })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
