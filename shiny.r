10079290c3594e230c176b1ae235826629b556f6
Pratik Dhungana <pratik.tan10@gmail.com>
2022-06-04 16:21:59 -0500

    Update shiny.r

--- shiny.r
+++ shiny.r
@@ -1,54 +1,42 @@
 ui <- fluidPage(
-  titlePanel("UFO Sightings"),
-  sidebarPanel(
-    selectInput("state", "Choose a U.S. state:", choices = unique(usa_ufo_sightings$state)),
-    dateRangeInput("dates", "Choose a date range:",
-      start = "1920-01-01",
-      end = "1950-01-01"
+  sidebarLayout(
+    sidebarPanel(
+      textInput("title", "Title", "GDP vs life exp"),
+      numericInput("size", "Point size", 1, 1),
+      checkboxInput("fit", "Add line of best fit", FALSE),
+      radioButtons("color", "Point color",
+                   choices = c("blue", "red", "green", "black")),
+      selectInput("continents", "Continents",
+                  choices = levels(gapminder$continent),
+                  multiple = TRUE,
+                  selected = "Europe"),
+      # Add a slider selector for years to filter
+      sliderInput("years", "Years", min = min(gapminder$year), max = max(gapminder$year), value = c(1977, 2002))
+    ),
+    mainPanel(
+      plotOutput("plot")
     )
-  ),
-  # MODIFY CODE BELOW: Create a tab layout for the dashboard
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Plot", plotOutput("shapes")),
-      tabPanel("Table", tableOutput("duration_table"))
-    )
-    
   )
 )
 
+# Define the server logic
 server <- function(input, output) {
-  output$shapes <- renderPlot({
-    usa_ufo_sightings %>%
-      filter(
-        state == input$state,
-        date_sighted >= input$dates[1],
-        date_sighted <= input$dates[2]
-      ) %>%
-      ggplot(aes(shape)) +
-      geom_bar() +
-      labs(
-        x = "Shape",
-        y = "# Sighted"
-      )
-  })
-
-  output$duration_table <- renderTable({
-    usa_ufo_sightings %>%
-      filter(
-        state == input$state,
-        date_sighted >= input$dates[1],
-        date_sighted <= input$dates[2]
-      ) %>%
-      group_by(shape) %>%
-      summarize(
-        nb_sighted = n(),
-        avg_duration_min = mean(duration_sec) / 60,
-        median_duration_min = median(duration_sec) / 60,
-        min_duration_min = min(duration_sec) / 60,
-        max_duration_min = max(duration_sec) / 60
-      )
+  output$plot <- renderPlot({
+    # Subset the gapminder data by the chosen years
+    data <- subset(gapminder,
+                   continent %in% input$continents &
+                   year >= input$years[1] & year <= input$years[2])
+    
+    p <- ggplot(data, aes(gdpPercap, lifeExp)) +
+      geom_point(size = input$size, col = input$color) +
+      scale_x_log10() +
+      ggtitle(input$title)
+    
+    if (input$fit) {
+      p <- p + geom_smooth(method = "lm")
+    }
+    p
   })
 }
 
-shinyApp(ui, server)
+shinyApp(ui = ui, server = server)
