2221f96dc42b4cfd4b6bd15a8b95acb6a74014df
Pratik Dhungana <pratik.tan10@gmail.com>
2022-06-04 19:00:25 -0500

    Update shiny.r

--- shiny.r
+++ shiny.r
@@ -1,41 +1,47 @@
+# Load the plotly package
+library(plotly)
+
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       textInput("title", "Title", "GDP vs life exp"),
       numericInput("size", "Point size", 1, 1),
       checkboxInput("fit", "Add line of best fit", FALSE),
-      radioButtons("color", "Point color",
-                   choices = c("blue", "red", "green", "black")),
+      colourInput("color", "Point color", value = "blue"),
       selectInput("continents", "Continents",
                   choices = levels(gapminder$continent),
                   multiple = TRUE,
                   selected = "Europe"),
-      # Add a slider selector for years to filter
-      sliderInput("years", "Years", min = min(gapminder$year), max = max(gapminder$year), value = c(1977, 2002))
+      sliderInput("years", "Years",
+                  min(gapminder$year), max(gapminder$year),
+                  value = c(1977, 2002))
     ),
     mainPanel(
-      plotOutput("plot")
+      plotly::plotlyOutput("plot")
     )
   )
 )
 
 # Define the server logic
 server <- function(input, output) {
-  output$plot <- renderPlot({
-    # Subset the gapminder data by the chosen years
-    data <- subset(gapminder,
-                   continent %in% input$continents &
-                   year >= input$years[1] & year <= input$years[2])
-    
-    p <- ggplot(data, aes(gdpPercap, lifeExp)) +
-      geom_point(size = input$size, col = input$color) +
-      scale_x_log10() +
-      ggtitle(input$title)
-    
-    if (input$fit) {
-      p <- p + geom_smooth(method = "lm")
-    }
-    p
+  # Replace the `renderPlot()` with the plotly version
+  output$plot <- renderPlotly({
+    # Convert the existing ggplot2 to a plotly plot
+    ggplotly({
+      data <- subset(gapminder,
+                     continent %in% input$continents &
+                       year >= input$years[1] & year <= input$years[2])
+      
+      p <- ggplot(data, aes(gdpPercap, lifeExp)) +
+        geom_point(size = input$size, col = input$color) +
+        scale_x_log10() +
+        ggtitle(input$title)
+      
+      if (input$fit) {
+        p <- p + geom_smooth(method = "lm")
+      }
+      p
+    })
   })
 }
 
