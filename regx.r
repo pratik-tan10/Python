ef4fd2b4afc1f22ada3f7adeac3a7f91a8671aae
Pratik Dhungana <pratik.tan10@gmail.com>
2022-06-07 19:53:06 -0500

    Update regx.r

--- regx.r
+++ regx.r
@@ -13,3 +13,43 @@ str_extract(contact, phone_pattern)
 
 # Extract ALL phone numbers
 str_extract_all(contact, phone_pattern)
+
+# View text containing phone numbers
+contact
+
+# Add capture() to get digit parts
+phone_pattern <- capture(three_digits) %R% zero_or_more(separator) %R% 
+           capture(three_digits) %R% zero_or_more(separator) %R%
+           capture(four_digits)
+           
+# Pull out the parts with str_match()
+phone_numbers <- str_match(contact, phone_pattern)
+
+# Put them back together
+str_c(
+  "(",
+  phone_numbers[,2],
+  ") ",
+  phone_numbers[,3],
+  "-",
+  phone_numbers[,4])
+
+# Use these patterns
+age <- DGT %R% optional(DGT)
+unit <- optional(SPC) %R% or("YO", "YR", "MO")
+gender <- optional(SPC) %R% or("M", "F")
+
+# Extract age, unit, gender
+str_extract(narratives, pattern =  age%R% unit%R% gender)
+
+# Numeric ages, from previous step
+ages_numeric <- as.numeric(str_extract(age_gender, age))
+
+# Extract units 
+time_units <- str_extract(age_gender,  unit)
+
+# Extract first word character
+time_units_clean <- str_extract(time_units, WRD)
+
+# Turn ages in months to years
+ifelse(time_units_clean == "Y", ages_numeric, ages_numeric/12)
