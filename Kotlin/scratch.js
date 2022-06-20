da0a80dc4e4a7729673662fd5e5cb6dec29b8ef0
Pratik Dhungana <pratik.tan10@gmail.com>
2022-06-19 22:34:31 -0500

    Update scratch.js

--- Kotlin/scratch.js
+++ Kotlin/scratch.js
@@ -44,3 +44,27 @@ Export.table.toDrive(newft,
 "anyDescription",
 "anyFolder",
 "anyNameYouWant")
+
+//--------------------------------------------------------
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Window Resizing</h1>
+    <h2>Press a key on keyboard</h2>
+<script>
+    window.addEventListener("resize", function(){
+        console.log(`The window width is ${window.innerWidth}`);
+        console.log(`The window height is ${window.innerHeight}`);
+    });
+    document.addEventListener("keydown", function(event){
+        alert(`the ${event.key} key was pressed`);
+    });
+</script>
+</body>
+</html>
